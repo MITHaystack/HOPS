9efc8b318b662e2aad666b3d0d1fd419a26f30f7
John Barrett <barrettj@mit.edu>
2020-10-23 12:02:01 -0400

    importing fft math classes from Haystack Hose project

---
+++ source/cpp_src/Math/include/MHOFastFourierTransform.hh
@@ -0,0 +1,56 @@
+#ifndef MHOFastFourierTransform_HH__
+#define MHOFastFourierTransform_HH__
+
+#include <complex>
+
+#include "MHOArrayWrapper.hh"
+#include "MHOUnaryArrayOperator.hh"
+
+#include "MHOBitReversalPermutation.hh"
+#include "MHOFastFourierTransformUtilities.hh"
+
+namespace hops
+{
+
+class MHOFastFourierTransform: public MHOUnaryArrayOperator< std::complex<double>, 1 >
+{
+    public:
+
+        MHOFastFourierTransform();
+        virtual ~MHOFastFourierTransform();
+
+        virtual void SetSize(unsigned int N);
+
+        virtual void SetForward();
+        virtual void SetBackward();
+
+        virtual void Initialize();
+
+        virtual void ExecuteOperation();
+
+    private:
+
+        virtual void AllocateWorkspace();
+        virtual void DealocateWorkspace();
+
+        bool fIsValid;
+        bool fForward;
+        bool fInitialized;
+        bool fSizeIsPowerOfTwo;
+
+        //auxilliary workspace needed for basic 1D transform
+        unsigned int fN;
+        unsigned int fM;
+        unsigned int* fPermutation;
+        std::complex<double>* fTwiddle;
+        std::complex<double>* fConjugateTwiddle;
+        std::complex<double>* fScale;
+        std::complex<double>* fCirculant;
+        std::complex<double>* fWorkspace;
+
+};
+
+}
+
+
+#endif /* MHOFastFourierTransform_H__ */
