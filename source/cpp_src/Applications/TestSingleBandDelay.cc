21f2ac0a3b6c085173962de089d7d9ee3a559f00
John Barrett <barrettj@mit.edu>
2020-10-30 16:33:12 -0400

    starting work on sbd test

---
+++ source/cpp_src/Applications/TestSingleBandDelay.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <utility>
+#include <map>
+#include <getopt.h>
+
+#ifdef USE_ROOT
+#include "TCanvas.h"
+#include "TApplication.h"
+#include "TStyle.h"
+#include "TColor.h"
+#include "TGraph.h"
+#include "TGraph2D.h"
+#include "TH2D.h"
+#include "TMath.h"
+#include "TMultiGraph.h"
+#endif
+
+#include "MHOTokenizer.hh"
+#include "MHOMK4VexInterface.hh"
+#include "MHOMK4CorelInterface.hh"
+
+#include "MHOMultidimensionalFastFourierTransform.hh"
+
+
+using namespace hops;
+
+
+int main(int argc, char** argv)
+{
+    std::string usage = "TestSingleBandDelay -r <root_filename> -f <corel_filename>";
+
+    std::string root_filename;
+    std::string corel_filename;
+
+    static struct option longOptions[] = {{"help", no_argument, 0, 'h'},
+                                          {"root (vex) file", required_argument, 0, 'r'},
+                                          {"corel file", required_argument, 0, 'f'}};
+
+    static const char* optString = "hr:f:";
+
+    while(true)
+    {
+        char optId = getopt_long(argc, argv, optString, longOptions, NULL);
+        if (optId == -1)
+            break;
+        switch(optId)
+        {
+            case ('h'):  // help
+                std::cout << usage << std::endl;
+                return 0;
+            case ('r'):
+                root_filename = std::string(optarg);
+                break;
+            case ('f'):
+                corel_filename = std::string(optarg);
+                break;
+            default:
+                std::cout << usage << std::endl;
+                return 1;
+        }
+    }
+
+    MHOMessage::GetInstance().AcceptAllKeys();
+
+    MHOMK4CorelInterface mk4inter;
+
+    mk4inter.SetCorelFile(corel_filename);
+    mk4inter.SetVexFile(root_filename);
+    baseline_data_type* bl_data = mk4inter.ExtractCorelFile();
+
+
+
+    return 0;
+}
