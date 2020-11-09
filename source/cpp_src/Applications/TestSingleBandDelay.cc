8d3e84894493ef5f2204850abaee67c6229440eb
John Barrett <barrettj@mit.edu>
2020-11-09 12:09:44 -0500

    some plots, need axis transform

--- source/cpp_src/Applications/TestSingleBandDelay.cc
+++ source/cpp_src/Applications/TestSingleBandDelay.cc
@@ -115,97 +115,87 @@ int main(int argc, char** argv)
 
     std::cout<<"done with the FFT's"<<std::endl;
 
+    std::vector< std::vector< std::complex<double> > > sbd;
+    sbd.resize(data_dims[CH_POLPROD_AXIS]);
+
+    //now collapse the time and channel axis
+    for(std::size_t pp=0; pp<data_dims[CH_POLPROD_AXIS]; pp++)
+    {
+        sbd[pp].resize(data_dims[CH_FREQ_AXIS], std::complex<double>(0.0, 0.0) );
+        for(std::size_t ch=0; ch<data_dims[CH_CHANNEL_AXIS]; ch++)
+        {
+            for(std::size_t t=0; t<data_dims[CH_TIME_AXIS]; t++)
+            {
+                for(std::size_t f=0; f<data_dims[CH_FREQ_AXIS]; f++)
+                {
+                    sbd[pp][f] += ch_bl_data->at(pp,ch,t,f);
+                }
+            }
+        }
+    }
+
+
+
+    #ifdef USE_ROOT
+    
+    std::cout<<"starting root plotting"<<std::endl;
+    //ROOT stuff for plots
+    TApplication* App = new TApplication("PowerPlot",&argc,argv);
+    TStyle* myStyle = new TStyle("Plain", "Plain");
+    myStyle->SetCanvasBorderMode(0);
+    myStyle->SetPadBorderMode(0);
+    myStyle->SetPadColor(0);
+    myStyle->SetCanvasColor(0);
+    myStyle->SetTitleColor(1);
+    myStyle->SetPalette(1,0);   // nice color scale for z-axis
+    myStyle->SetCanvasBorderMode(0); // gets rid of the stupid raised edge around the canvas
+    myStyle->SetTitleFillColor(0); //turns the default dove-grey background to white
+    myStyle->SetCanvasColor(0);
+    myStyle->SetPadColor(0);
+    myStyle->SetTitleFillColor(0);
+    myStyle->SetStatColor(0); //this one may not work
+    const int NRGBs = 5;
+    const int NCont = 48;
+    double stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+    double red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+    double green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+    double blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+    TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+    myStyle->SetNumberContours(NCont);
+    myStyle->cd();
     
 
+    TGraph* g_amp[ data_dims[CH_POLPROD_AXIS] ];
+    TCanvas* c[data_dims[CH_POLPROD_AXIS] ];
 
 
-    // #ifdef USE_ROOT
-    //
-    // std::cout<<"starting root plotting"<<std::endl;
-    // //ROOT stuff for plots
-    // TApplication* App = new TApplication("PowerPlot",&argc,argv);
-    // TStyle* myStyle = new TStyle("Plain", "Plain");
-    // myStyle->SetCanvasBorderMode(0);
-    // myStyle->SetPadBorderMode(0);
-    // myStyle->SetPadColor(0);
-    // myStyle->SetCanvasColor(0);
-    // myStyle->SetTitleColor(1);
-    // myStyle->SetPalette(1,0);   // nice color scale for z-axis
-    // myStyle->SetCanvasBorderMode(0); // gets rid of the stupid raised edge around the canvas
-    // myStyle->SetTitleFillColor(0); //turns the default dove-grey background to white
-    // myStyle->SetCanvasColor(0);
-    // myStyle->SetPadColor(0);
-    // myStyle->SetTitleFillColor(0);
-    // myStyle->SetStatColor(0); //this one may not work
-    // const int NRGBs = 5;
-    // const int NCont = 48;
-    // double stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
-    // double red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
-    // double green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
-    // double blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
-    // TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
-    // myStyle->SetNumberContours(NCont);
-    // myStyle->cd();
-    //
-    // auto* x_axis = &(std::get<TIME_AXIS>(*bl_data));
-    // auto* y_axis = &(std::get<FREQ_AXIS>(*bl_data));
-    //
-    // size_t x_axis_size = x_axis->GetSize();
-    // size_t y_axis_size = y_axis->GetSize();
-    //
-    // //just plot phase/amp for a single channel
-    // // TGraph2D *gr = new TGraph2D(x_axis_size*y_axis_size);
-    // // TGraph2D *gb = new TGraph2D(x_axis_size*y_axis_size);
-    // TGraph* g_amp = new TGraph();
-    // TGraph* g_ph = new TGraph();
-    //
-    //
-    // size_t count = 0;
-    // auto ch = y_axis->GetFirstIntervalWithKeyValue(std::string("channel"), 12);
-    //
-    // if(ch != nullptr)
-    // {
-    //     size_t low = ch->GetLowerBound();
-    //     size_t up = ch->GetUpperBound();
-    //     for(size_t j=low; j<up; j++) //freq axis
-    //     {
-    //         std::cout<<j<<", "<<y_axis->at(j)<<std::endl;
-    //         std::complex<double> sum = 0.0;
-    //         for(size_t i=0; i<x_axis_size; i++) //time axis
-    //         {
-    //             std::complex<double> vis = bl_data->at(0,i,j); //only first pol-product
-    //             sum += vis;
-    //             //g->SetPoint(j,y_axis->at(j),std::arg(vis));
-    //             //gr->SetPoint(count, x_axis->at(i), y_axis->at(j), std::arg(vis) );
-    //             //gb->SetPoint(count, x_axis->at(i), y_axis->at(j), std::abs(vis) );
-    //         }
-    //         g_amp->SetPoint(j, y_axis->at(j), std::abs(sum));
-    //         g_ph->SetPoint(j, y_axis->at(j), std::arg(sum));
-    //         count++;
-    //     }
-    // }
-    //
-    //
-    //
-    // std::string name = "test";
-    // TCanvas* c = new TCanvas(name.c_str(),name.c_str(), 50, 50, 950, 850);
-    // c->SetFillColor(0);
-    // c->SetRightMargin(0.2);
-    // c->Divide(1,2);
-    // c->cd(1);
-    // g_amp->SetMarkerStyle(20);
-    // g_amp->SetMarkerSize(1);
-    // g_amp->Draw("APL");
-    // c->Update();
-    // c->cd(2);
-    // g_ph->SetMarkerStyle(20);
-    // g_ph->SetMarkerSize(1);
-    // g_ph->Draw("APL");
-    // c->Update();
-    //
-    // App->Run();
-    //
-    // #endif //USE_ROOT
+    for(std::size_t pp=0; pp<data_dims[CH_POLPROD_AXIS]; pp++)
+    {
+        std::stringstream ss; 
+        ss << pp;
+        c[pp] = new TCanvas(ss.str().c_str(),ss.str().c_str(), 50, 50, 950, 850);
+        c[pp]->SetFillColor(0);
+        c[pp]->SetRightMargin(0.2);
+        g_amp[pp] = new TGraph();
+        for(std::size_t f=0; f<data_dims[CH_FREQ_AXIS]; f++)
+        {
+            g_amp[pp]->SetPoint(f, f, std::abs( sbd[pp][f]) );
+        }
+    }
+
+
+    for(std::size_t pp=0; pp<data_dims[CH_POLPROD_AXIS]; pp++)
+    {
+        c[pp]->cd();
+        g_amp[pp]->SetMarkerStyle(20);
+        g_amp[pp]->SetMarkerSize(1);
+        g_amp[pp]->Draw("APL");
+        c[pp]->Update();
+    }
+    
+    App->Run();
+    
+    #endif //USE_ROOT
 
 
 
