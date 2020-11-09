f9235c9d173d5194b32839047b269fd0a2f575aa
gcrew <gbc@haystack.mit.edu>
2020-11-09 12:58:48 -0500

    created a pair of trivial functions

--- source/python_src/Bindings/include/numpy_test.h
+++ source/python_src/Bindings/include/numpy_test.h
@@ -4,7 +4,13 @@
  * The contents of the package Copyright statement apply here.
  */
 
-/* nothing yet */
+#include <numpy/npy_common.h>
+
+/* void functions are somewhat easier to work with */
+extern void get_np_amps(
+    npy_cdouble *vis, int nvis, npy_double *amp, int namp);
+extern void get_np_phases(
+    npy_cdouble *vis, int nvis, npy_double *phs, int nphs);
 
 /*
  * eof
--- source/python_src/Bindings/src/numpy_test.cc
+++ source/python_src/Bindings/src/numpy_test.cc
@@ -3,12 +3,33 @@
 // (c) Massachusetts Institute of Technology, 2020
 // The contents of the package Copyright statement apply here.
 //
-// this is for initial testing...
+// import numpy as np
+// vis = np.ones(16, dtype=np.complex128)
+//
+// FIXME: there is probably some more clever way to launder
+//        a npy_complex into a std::complex, but this works.
 //
 
-extern "C" void numpy_nothing(void)
+extern "C" {
+#include "numpy_test.h"
+#include <complex.h>
+}
+
+extern "C" void get_np_amps(
+    npy_cdouble *vis, int nvis, npy_double *amp, int namp)
 {
-    ;
+    for (int ii = 0; ii < nvis && ii < namp; ii++) {
+        amp[ii] = cabs((vis[ii].real + I * vis[ii].imag));
+    }
+}
+
+extern "C" void get_np_phases(
+    npy_cdouble *vis, int nvis, npy_double *phs, int nphs)
+{
+    std::complex<double> temp;
+    for (int ii = 0; ii < nvis && ii < nphs; ii++) {
+        phs[ii] = carg((vis[ii].real + I * vis[ii].imag));
+    }
 }
 
 //
