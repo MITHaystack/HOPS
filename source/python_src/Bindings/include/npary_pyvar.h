850d544936887a9b90d35f541ff7f9c9c8e9703d
gcrew <gbc@haystack.mit.edu>
2020-11-13 12:44:00 -0500

    Now the swig/numpy code functions correctly, and added doc.

--- source/python_src/Bindings/include/npary_pyvar.h
+++ source/python_src/Bindings/include/npary_pyvar.h
@@ -5,12 +5,20 @@
  *
  * This example is fully contrived, but you want such a file when
  * the functions you have (from lower levels) have an interface that
- * is somewhat awkward to pass through the SWIG numpy rules.
+ * is somewhat awkward to pass through the SWIG/numpy rules and it
+ * is just easiest to build something that plays better in C/C++.
+ *
+ * The notion here is that MyVis can function as an object.
  */
 #ifndef npary_pyvar_h
 #define npary_pyvar_h
 
-/* #warning "nothing here yet" */
+//typedef struct myvis {
+//    char    *name;          /* name of something */
+//    double  *amps;          /* pointer to some amplitudes */
+//    double  *phase;         /* pointer to some phases */
+//    int     nvis;           /* length of both arrays */
+//} MyVis;
 
 #endif /* npary_pyvar_h */
 /*
