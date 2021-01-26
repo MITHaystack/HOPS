ac61fea7c6364ce29f788c86597a066f99c7edc9
gcrew <gbc@haystack.mit.edu>
2021-01-26 15:15:17 -0500

    Set up data heirarchy for imported tests.
    With this checkin, only the top-level README.txt contains useful
    information and the directories contain temporary stubs until there
    is something there.

--- data/bootstrap/import_check.sh
+++ data/bootstrap/import_check.sh
@@ -7,9 +7,11 @@
 [ -n "$testverb" -a "$testverb" -gt 0 ] && verb=true || verb=false
 $verb && [ "$testverb" -gt 1 ] && vrb1=true || vrb1=false
 # ...
+$vrb1 && echo being really verbose about commentary here.
 
 [ -n "$srcdir" ] && $verb && echo srcdir is $srcdir
-[ -d "$srcdir" -a -f "$srcdir/import_check.sh" ] && status=0 || status=1
+[ -d "$srcdir" -a -f "$srcdir/bootstrap/import_check.sh" ] &&
+    status=0 || status=1
 
 exit $status
 #
