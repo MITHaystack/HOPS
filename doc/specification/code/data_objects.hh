47b12da98880e9597206cb4707c7c9489b5c970f
John Barrett <barrettj@mit.edu>
2021-01-29 01:09:59 -0500

    more updates to the spec document

--- doc/specification/code/data_objects.hh
+++ doc/specification/code/data_objects.hh
@@ -1,37 +1,31 @@
-template< typename XValueType, typename XUnitType >
-class ScalarContainer {
+//implemenation omitted between {}
+template< typename XValueType, std::size_t RANK>
+class MHO_NDArrayWrapper
+{
+    //...impl omitted...
+    protected:
+        XValueType* fDataPtr; //raw pointer to memory
+        bool fExternallyManaged; //flag for externally managed data
+        std::vector< XValueType > fData; //used for internally managed data
+        std::size_t fDimensions[RANK]; //size of each dimension
+        std::size_t fTotalArraySize; //total size of array
     public:
-        ScalarContainer();
-        virtual ~ScalarContainer();
-        //...TBD impl...
-    private:
-        std::string fName;
-        XUnitType fUnit;
-        XValueType fData;
+        //STL style iterator
+        class iterator {};
 };
 
-template< typename XValueType, typename XUnitType >
-class VectorContainer {
-    public:
-        std::string fName;
-        VectorContainer();
-        virtual ~VectorContainer();
-        //...TBD impl...
-    private:
-        std::string fName;
-        XUnitType fUnit;
-        std::vector< XValueType > fData;
-};
+template< typename XValueType >
+class MHO_ScalarContainer: public MHO_NDArrayWrapper< XValueType, 0>, public MHO_Named {};
 
-template< typename XValueType, typename XUnitType, size_t RANK, typename... XAxisTypes >
-class TensorContainer {
-    public:
-        TensorContainer();
-        virtual ~TensorContainer();
-        //...TBD impl...
-    private:
-        std::string fName;
-        XUnitType fUnit; //units of the data
-        std::vector< XValueType > fData; //row-indexed block of data
-        std::tuple< XAxisTypes > fAxes; //tuple of length RANK of VectorContainers
-};
+template< typename XValueType >
+class MHO_VectorContainer: public MHO_NDArrayWrapper< XValueType, 1>, public MHO_Named{};
+
+template< typename XValueType >
+class MHO_Axis: public MHO_VectorContainer< XValueType >, public MHO_IntervalLabelTree {};
+
+template< typename...XAxisTypeS >
+class MHO_AxisPack:  public std::tuple< XAxisTypeS... > {};
+
+template< typename XValueType, typename XAxisPackType >
+class MHO_TableContainer: public MHO_NDArrayWrapper< XValueType, XAxisPackType::NAXES::value>,
+                          public XAxisPackType, public MHO_Named {};
