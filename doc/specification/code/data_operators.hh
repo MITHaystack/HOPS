fd3342d99073da7e1a983f89ae2f7a0d14a73b19
John Barrett <barrettj@mit.edu>
2021-01-29 11:24:58 -0500

    edits to spec

--- doc/specification/code/data_operators.hh
+++ doc/specification/code/data_operators.hh
@@ -1,20 +1,30 @@
 //template parameters must inherit from MHO_NDArrayWrapper
+class MHO_DataOperator
+{
+    public:
+       virtual bool Initialize() = 0;
+       virtual bool ExecuteOperation() = 0;
+    //...impl...
+};
+
+
+
 template<class XInputArrayType, class XOutputArrayType>
-class MHO_NDArrayOperator
+class MHO_NDArrayOperator: public MHO_DataOperator
 {
     public:
        virtual void SetInput(XInputArrayType* in){fInput = in;};
        virtual void SetOutput(XOutputArrayType* out){fOutput = out;};
        virtual XInputArrayType* GetInput(){return fInput;};
        virtual XOutputArrayType* GetOutput(){return fOutput;};
-       virtual bool Initialize() = 0;
-       virtual bool ExecuteOperation() = 0;
+       virtual bool Initialize() override;
+       virtual bool ExecuteOperation() override;
     //...impl...
 };
 
  //template parameters must inherit from MHO_NDArrayWrapper
 template<class XInputArrayType1, class XInputArrayType2, class XOutputArrayType>
-class MHO_BinaryNDArrayOperator
+class MHO_BinaryNDArrayOperator:  public MHO_DataOperator
 {
     public:
         virtual void SetFirstInput(XInputArrayType1* in){fInput1 = in;};
@@ -23,7 +33,7 @@ class MHO_BinaryNDArrayOperator
         virtual XInputArrayType1* GetFirstInput(){return fInput1;};
         virtual XInputArrayType2* GetSecondInput(){return fInput2;};
         virtual XOutputArrayType* GetOutput(){return fOutput;};
-        virtual bool Initialize() = 0;
-        virtual bool ExecuteOperation() = 0;
+        virtual bool Initialize() override;
+        virtual bool ExecuteOperation() override;
         //...impl...
 };
