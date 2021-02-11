7a81c1ed4b5d24ecb937ba9e9ebf95d95a042389
gcrew <gbc@haystack.mit.edu>
2021-02-11 17:31:17 -0500

    shovelled in scripts and APP example

---
+++ doc/tasking/taskscripts/sw_task_parser.pl
@@ -0,0 +1,381 @@
+#!/usr/bin/perl
+#
+use warnings;           # turns on optional warnings
+use diagnostics;        # and makes them not terse
+use strict;             # makes unsafe constructs illegal
+#
+# Support script for parsing the sw task data file
+#
+
+# hashes of domains, things, and tasks
+our %domains;
+our %things;
+our %tasks;
+# the work breakdown structure
+our %wbs;
+our $sep = '%%';
+# verbose for feedback, veryverb for debugging
+our $verb;
+our $veryverb;
+
+#
+# first mention of domain/thing/task defines it
+# next mention merely establishes parentage
+# ^@* = value       to define abbreviations (substituted thereafter)
+#                   Note: abbreviations automatically made for nick's.
+# ^tdefs = name     allows @name later to fill out task entries
+#                   definition ends with blank or comment line
+#
+# the keywords define the input language
+# and must be initialized in taskage.
+#
+our @keywords = (
+    'type', 'domain', 'thing', 'task',
+    'desc', 'nick', 'parent', 'kids', 'line',
+    'path', ,'code', 'notes', 'level', 'issues', 'orient',
+    'who', 'pri', 'sec', 'men', 'fte',
+    'uid', 'needs', 'start', 'begin', 'days', 'derate',
+    'done', 'mjds', 'flex', 'end', 'stop', 'allows',
+    'preps', 'leads'
+);
+
+my %taskage = (
+    # what it is
+    'type'   => 'derived: one of domain, thing, task',
+    'domain' => 'derived: some domain of activity',
+    'thing'  => 'derived: some thing in domain to be done',
+    'task'   => 'derived: tasks to required complete thing',
+
+    # basic detail about the task
+    'desc'   => 'input: short description of task/thing/domain',
+    'nick'   => 'input: nickname for task/thing/domain',
+    'parent' => 'derived: wbs | thing of task | or domain of thing',
+    'kids'   => 'derived: domains or things',
+    'line'   => 'derived: line number in original file',
+
+    # yet more detail about the task
+    'path'   => 'input: CVS:where',
+    'code'   => 'input: C++/Java/Python',
+    'notes'  => 'input: SVN:local file with more details',
+    'level'  => 'input: easy | moderate | difficult',
+    'issues' => 'input: open issues relevant to planning',
+    'orient' => 'input: portrait/landscape',
+
+    # workers associated with task
+    'who'    => 'input: list of workers',
+    'pri'    => 'derived: primary (1st in who)',
+    'sec'    => 'derived: secondary (2nd in who)',
+    'men'    => 'derived: everyone else',
+    'fte'    => 'derived: total fte (.66pri,.33sec,0else)',
+
+    # dependencies and timeline
+    'uid'    => 'derived: ordinal identifier',
+    'needs'  => 'input: ,-sep list of predecessor nicks',
+    'start'  => 'either: start date if days=0, else derived',
+    'days'   => 'input: estimate of real man-days of work',
+    'derate' => 'input: multiplier on ftes to get calendar days',
+    'begin'  => 'derived: MJD of start date',
+    'done'   => 'input: percentage done',
+    'mjds'   => 'derived: calendar days required',
+    'flex'   => 'derived: calendar days of margin',
+    'end'    => 'derived: MJD of finish date',
+    'stop'   => 'either: finish date if days=0, else derived',
+    'allows' => 'input: ,-sep list of successor nicks',
+    'preps'  => 'derived: nick that defines start of this one',
+    'leads'  => 'derived: nick that defines stop of this one',
+);
+
+# a hash of task defaults
+my %taskdefs = ();
+# a hash of abbreviations
+my %abbrevs = ();
+
+# private parsing variables
+my $current_taskdef = '';
+my $current_domain = '';
+my $current_thing = '';
+my $current_task = '';
+my $current_item = '';
+my ($domcntr,$thgcntr,$tskcntr) = (1,1,1);
+
+#
+# Undo abbreviations -- replace @foo by foo's value.
+# Note that spaces around the abbreviation are not required.
+#
+sub deabbr {
+    #my ($_) = @_;
+    local ($_) = @_;
+    for my $key (keys(%abbrevs)) { s($key)($abbrevs{$key}); }
+    for my $key (keys(%abbrevs)) { s($key)($abbrevs{$key}); }
+    return $_;
+}
+
+#
+# Make an abbreviation by stripping off wrapper spaces
+#
+sub makeabbr {
+    #my ($_,$ln,@bits) = @_;
+    local $_; my ($ln,@bits); ($_,$ln,@bits) = @_;
+    @bits = split(/=/);
+    $bits[0] =~ s{([@]\S+)\s*}{$1};
+    $bits[1] =~ s{\s*(.*)\s*}{$1};
+    $abbrevs{$bits[0]} = &deabbr($bits[1]);
+    print DEBUG "# abbrev  " . $bits[0] . "->" .
+            $abbrevs{$bits[0]} . "<\n" if ($veryverb);
+    return $_ . "\n";
+};
+
+
+#
+# some functions to create a new object with defaults for
+# everything and to then hook it up to what is already known.
+#
+# canonicalize_items() will eventually overwrite things left undefined
+#
+sub create_object {
+    my $kv = $_[0];
+    $wbs{$kv} = ();
+    for my $key (keys(%taskage)) {
+        $wbs{$kv}{$key} = $taskage{$key};
+    }
+}
+sub set_default_object {
+    my ($kv,$ln,$type,$parent,@parts) = @_;
+    &create_object($kv);
+    $wbs{$kv}{'type'} = $type;
+    $wbs{$kv}{'parent'} = $parent;
+    $wbs{$kv}{'kids'} = '';
+    $wbs{$kv}{'issues'} = '';
+    $wbs{$kv}{'orient'} = 'portrait';
+    $wbs{$kv}{'line'} = $ln;
+    $wbs{$kv}{'nick'} = $kv;
+#   $wbs{$kv}{'uid'} = $uidcntr++;
+#   for my $ii ('domain','thing','task') {
+#       $wbs{$kv}{$ii} = ($ii eq $type) ? 'true' : 'false';
+#   }
+    @parts = split(/$sep/,$kv);
+    if ($type eq 'domain') {
+        $wbs{$kv}{'domain'} = $parts[0];
+        $wbs{$kv}{'thing'} = 'none';
+        $wbs{$kv}{'task'} = 'none';
+        $wbs{$kv}{'uid'} = ($domcntr++) * 100000;
+    } elsif ($type eq 'thing') {
+        $wbs{$kv}{'domain'} = $parts[0];
+        $wbs{$kv}{'thing'} = $parts[1];
+        $wbs{$kv}{'task'} = 'none';
+        $wbs{$kv}{'uid'} = ($thgcntr++) * 1000;
+        $wbs{$kv}{'uid'} += $wbs{$parent}{'uid'};
+    } elsif ($type eq 'task') {
+        $wbs{$kv}{'domain'} = $parts[0];
+        $wbs{$kv}{'thing'} = $parts[1];
+        $wbs{$kv}{'task'} = $parts[2];
+        $wbs{$kv}{'uid'} = $tskcntr++;
+        $wbs{$kv}{'uid'} += $wbs{$parent}{'uid'};
+    }
+}
+
+#
+# Parse one line of the input
+#
+sub really_parse_one_line {
+    #my ($_,$ln,$kv,@bits) = @_;
+    local ($_); my ($ln,$kv,@bits); ($_,$ln,$kv,@bits) = @_;
+    @bits = split(/=/);
+    push @bits,'';
+    $bits[1] =~ s{\s*(.*)\s*}{$1};
+    $kv = &deabbr($bits[1]);
+    print DEBUG '## line ' . $ln . ':' . $_ if ($veryverb);
+
+    # $current_item = '';
+    $current_item = $bits[0];
+    $current_item =~ s{^\s*}{};
+    $current_item =~ s{\s*$}{};
+
+    # tdefs handling
+    if ( /^tdefs/ ) {
+        $current_taskdef = $kv = '@' . $kv;
+        $taskdefs{$kv} = '';
+    } elsif ( $current_taskdef ne '' ) {
+        $taskdefs{$current_taskdef} .= &deabbr($_) . "%%%";
+
+    # what it is
+    } elsif ( /^domain/ ) {
+        if (!defined($domains{$kv})) {
+            $domains{$kv} = $ln;
+            &set_default_object($kv,$ln,'domain','wbs');
+        }
+        $current_task = $current_thing = $current_domain = $kv;
+    } elsif ( /^thing/  ) {
+        $kv = $current_domain . $sep . $kv;
+        if (!defined($things{$kv})) {
+            $things{$kv} = $ln;
+            &set_default_object($kv,$ln,'thing',$current_domain);
+            if (defined($wbs{$current_domain}{'nick'})) {
+                ($wbs{$kv}{'nick'} = $wbs{$current_domain}{'nick'} .
+                    ':' . $bits[1]) =~ s/[@ ]//g;
+            }
+        }
+        $current_task = $current_thing = $kv;
+    } elsif ( /^task/  ) {
+        $kv = $current_thing . $sep . $kv;
+        if (!defined($tasks{$kv})) {
+            $tasks{$kv} = $ln;
+            &set_default_object($kv,$ln,'task',$current_thing);
+            if (defined($wbs{$current_thing}{'nick'})) {
+                ($wbs{$kv}{'nick'} = $wbs{$current_thing}{'nick'} .
+                    ':' . $bits[1]) =~ s/[@ ]//g;
+            }
+        }
+        $current_task = $kv;
+    } elsif ( /^line/ or /^parent/ or /^type/ or /^kids/) {
+        ; # derived
+
+    } elsif ( /^desc/ ) {
+        $wbs{$current_task}{'desc'} = $kv;
+        # $current_item = 'desc';
+    } elsif ( /^nick/ ) {
+        #&makeabbr('@' . $kv . ' = ' .  $wbs{$current_task}{'nick'},$ln);
+        @bits = split(/$sep/,$current_task);
+        &makeabbr('@' . $kv . ' = ' . $bits[$#bits],$ln);
+        $wbs{$current_task}{'nick'} = $kv;
+
+    } elsif ( /^path/ ) {
+        $wbs{$current_task}{'path'} = $kv;
+    } elsif ( /^code/ ) {
+        $wbs{$current_task}{'code'} = $kv;
+    } elsif ( /^notes/ ) {
+        $wbs{$current_task}{'notes'} = $kv;
+    } elsif ( /^days/ ) {
+        $wbs{$current_task}{'days'} = $kv;
+    } elsif ( /^derate/ ) {
+        $wbs{$current_task}{'derate'} = $kv;
+    } elsif ( /^level/ ) {
+        $wbs{$current_task}{'level'} = $kv;
+    } elsif ( /^issues/ ) {
+        $wbs{$current_task}{'issues'} = $kv;
+    } elsif ( /^orient/ ) {
+        $wbs{$current_task}{'orient'} = $kv;
+
+    } elsif ( /^who/ ) {
+        $wbs{$current_task}{'who'} = $kv;
+        my @people = split(/,/,$kv,3);
+        $wbs{$current_task}{'pri'} = $people[0];    # first is primary
+        $wbs{$current_task}{'sec'} = $people[1];    # second is secondary
+        $wbs{$current_task}{'men'} = $people[2];    # everyone else
+
+    } elsif ( /^needs/ ) {
+        $wbs{$current_task}{'needs'} = $kv;
+    } elsif ( /^start/ ) {
+        $wbs{$current_task}{'start'} = $kv;
+    } elsif ( /^done/ ) {
+        $wbs{$current_task}{'done'} = $kv;
+    } elsif ( /^stop/ ) {
+        $wbs{$current_task}{'stop'} = $kv;
+    } elsif ( /^allows/ ) {
+        $wbs{$current_task}{'allows'} = $kv;
+    } elsif ( /^uid/ or /^begin/ or /^mjds/ or /^flex/ or /^end/ ) {
+        ; # derived
+
+    }
+    return $_ . "\n";
+}
+
+#
+# most things can be continued with whitespace in the first column
+#
+sub continued_item {
+    #my ($_,$ln,$strip) = @_;
+    local ($_); my ($ln,$strip); ($_,$ln,$strip) = @_;
+    $strip = $_;
+    $strip =~ s/\s*(.*)\s*/$1/;
+    $strip = &deabbr($strip);
+    if (defined($wbs{$current_task}{$current_item})) {
+        if ($wbs{$current_task}{$current_item} =~ m/^.*-$/) {
+            $wbs{$current_task}{$current_item} .= $strip;
+        } else {
+            $wbs{$current_task}{$current_item} .= ' ' . $strip;
+        }
+    }
+    return $_ . "\n";
+}
+
+#
+# If this task is defined, then step through its definition, expanding it.
+#
+sub insert_taskdef {
+    #my ($_,$ln,@bits) = @_;
+    local ($_); my ($ln,@bits); ($_,$ln,@bits) = @_;
+    print DEBUG "# taskdef " . $_ . "\n" if ($veryverb);
+    if (defined($taskdefs{$_})) {
+        @bits = split(/%%%/,$taskdefs{$_});
+        $_ = '# ' . $_ . ' at line ' . $ln . ":\n";
+        for my $b (@bits) { $_ .= &really_parse_one_line($b,$ln); }
+    }
+    return $_;
+}
+
+#
+# taskdefs end with a blank line or comment
+#
+sub finish_taskdef {
+    $current_taskdef = '';
+    return $_ . "\n";
+}
+
+#
+# switch to handle @definedtask
+#
+sub parse_one_line {
+    #my ($_,$ln,@bits) = @_;
+    local ($_); my ($ln,@bits); ($_,$ln,@bits) = @_;
+    if ( /^@.*=/ )  {       return(&makeabbr($_,$.)); }
+    elsif ( /^@/ )  { return(&insert_taskdef($_,$.)); }
+    elsif ( /^$/ )  { return(&finish_taskdef($_,$.)); }
+    elsif ( /^#/ )  { return(&finish_taskdef($_,$.)); }
+    elsif ( /^\s/ ) { return(&continued_item($_,$.)); }
+    else {    return(&really_parse_one_line($_,$.)); }
+    return "#never get here\n";
+}
+
+#
+# Parse the provided file and generate the data structures
+# Returns the three pieces of the scanned file.
+#
+sub parse_sw_task_file {
+    my ($file) = @_;
+    my ($ok,$hdr,$guts,$ftr) = (0,'','','');
+    open(DATA,"<",$file);
+    while (<DATA>) {
+        chomp;
+        if    ( /^HEAD/ ) { $ok = 1; $hdr .= $_ . "\n"; }
+        elsif ( /^TAIL/ ) { $ok = 2; $ftr .= $_ . "\n"; }
+        elsif ( $ok == 0 ) { $hdr .= $_ . "\n"; }
+        elsif ( $ok == 2 ) { $ftr .= $_ . "\n"; }
+        else  { $guts .= &parse_one_line($_,$.); }
+    }
+    close(DATA);
+    return($hdr,$guts,$ftr);
+}
+
+#
+# For debugging, dump the task defs
+#
+sub dump_taskdefs {
+    for my $td (sort(keys(%taskdefs))) {
+        print DEBUG "TDEF: $td = >" . $taskdefs{$td} . "<\n";
+    }
+}
+sub dump_abbrevs {
+    for my $td (sort(keys(%abbrevs))) {
+        print DEBUG "ABBR: $td = >" . $abbrevs{$td} . "<\n";
+    }
+}
+
+#
+# this is a library module, so it needs to return TRUE.
+#
+1;
+
+#
+# eof
+#
---
+++ doc/tasking/taskscripts/sw_task_wbs.pl
@@ -0,0 +1,280 @@
+#!/usr/bin/perl
+#
+use warnings;           # turns on optional warnings
+use diagnostics;        # and makes them not terse
+use strict;             # makes unsafe constructs illegal
+#
+# Support script for walking through the wbs
+#
+
+our @keywords;
+our %domains;
+our %things;
+our %tasks;
+our %wbs;
+our $sep;
+
+our $sort_key;
+our $verb;
+our $veryverb;
+
+# some subsets
+my @jelly   = ( 'nick', 'start', 'stop', 'days', 'who',
+                'task', 'thing', 'domain', 'desc' );
+my @summary = ( 'desc', 'nick', 'who', 'stop' );
+my @simple  = ( 'desc' );
+my @inputs  = ( 'desc', 'nick', 'path', ,'code', 'notes', 'level',
+                'issues', 'orient',
+                'who', 'needs', 'start', 'days', 'derate', 'done',
+                'stop', 'allows' );
+
+# trying to use forms
+our ($fkey,$fval);
+format WBS =
+  @>>>>>>>>  ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  $fkey,     $fval
+~~           ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+             $fval
+.
+format NEW =
+@<<<<<<<<  = ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+  $fkey,     $fval
+~~           ^<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+             $fval
+.
+
+#
+# convert a mnemonic into a list
+#
+sub report_on {
+    my $what = $_[0];
+    if ($what eq 'all') { return(@keywords); }
+    elsif ($what eq 'sum') { return(@summary); }
+    elsif ($what eq 'bean') { return(@jelly); }
+    elsif ($what eq 'what') { return(@simple); }
+    # else
+    print "Undefined report $what so you just get a simple list.\n";
+    return(@simple);
+}
+
+#
+# a comparison on %wbs for sort
+#
+sub by_uid { return( $wbs{$a}{'uid'} <=> $wbs{$b}{'uid'} ); }
+sub by_begin { return( $wbs{$a}{'begin'} <=> $wbs{$b}{'begin'} ); }
+sub by_key {
+    return &by_uid   if ($sort_key eq 'uid');
+    return &by_begin if ($sort_key eq 'begin');
+    if (defined($wbs{$a}{$sort_key}) and
+        defined($wbs{$b}{$sort_key})) {
+        return( $wbs{$a}{$sort_key} cmp $wbs{$b}{$sort_key} );
+    }
+    if (!defined($wbs{$a}{'uid'})) { print "Undef uid on $a $sort_key\n"; }
+    if (!defined($wbs{$b}{'uid'})) { print "Undef uid on $b $sort_key\n"; }
+    return( $wbs{$a}{'uid'} <=> $wbs{$b}{'uid'} );
+}
+
+#
+# helper function; remember that keys are
+#   domain . sep . thing . sep . task
+#
+sub write_new_rest {
+    my ($key,$k,$kv,@p) = @_;
+    @p = split(/$sep/,$key);
+    $kv = $p[0];
+    while ($#p >= 0) {
+        $fval = shift(@p);
+        $fkey = $wbs{$kv}{'type'};
+        $kv .= $sep . $p[0] if ($#p >= 0);
+        write NEW;
+    }
+    # dump the defined remainder of it
+    for $k (@inputs) {
+        $fkey = $k;
+        $fval = $wbs{$key}{$k};
+        write NEW if ($fval ne '');
+    }
+    print NEW "\n";
+}
+
+#
+# dump the entire structure in the input format
+#
+sub dump_new_input {
+    my ($out,$in,$t) = @_;
+    open(NEW,$out);
+    print NEW "#\n# Generated from $in\n#\n\n";
+    print NEW "HEAD = start reading here\n\n";
+    for $t (keys(%domains)) { &write_new_rest($t); }
+    for $t (keys(%things)) { &write_new_rest($t); }
+    for $t (keys(%tasks)) { &write_new_rest($t); }
+    print NEW "\nTAIL = stop reading here\n\n";
+    print NEW "#\n# eof\n#\n";
+    close(NEW);
+}
+
+#
+# dump out the entire structure
+#
+sub dump_the_wbs {
+    my ($out,$orderef,@wbskeys) = @_;
+    my ($task,$key);
+    open(WBS,$out);
+    if ($veryverb) {
+        print "# Reporting on: ";
+        for my $k (@wbskeys) { print "$k, "; } print "\n";
+    }
+    for $task (sort($orderef keys(%wbs))) {
+        print WBS $wbs{$task}{'type'} . " $task\n";
+        for $key (@wbskeys) {
+            if (defined($wbs{$task}{$key})) {
+                print WBS ' ' . $key . ' -> ' . $wbs{$task}{$key} . "\n";
+            }
+        }
+        print WBS "\n";
+    }
+    close(WBS);
+}
+
+#
+# same thing, but just the domain/thing/task
+#
+sub dump_items {
+    my ($what,$out,$orderef,@wbskeys) = @_;
+    my ($task,$key);
+    open(WBS,$out);
+    for $task (sort($orderef keys(%wbs))) {
+        next if ($wbs{$task}{'type'} ne $what);
+        print WBS $wbs{$task}{'type'} . " $task\n";
+        for $key (@wbskeys) {
+            if (defined($wbs{$task}{$key})) {
+                $fkey = $key;
+                $fval = $wbs{$task}{$key};
+                write WBS;
+            }
+        }
+        print WBS "\n";
+    }
+    close(WBS);
+}
+
+sub dump_the_domains { &dump_items('domain',@_); }
+sub dump_the_things { &dump_items('thing',@_); }
+sub dump_the_tasks { &dump_items('task',@_); }
+
+sub dump_tsv_tasks {
+    my ($out,$orderef,@wbskeys) = @_;
+    my ($aa,$bb,$cc,$preds);
+    my ($task,$key);
+    open(TSV,$out);
+    for $task (sort($orderef keys(%wbs))) {
+        next if ($wbs{$task}{'type'} ne 'task');
+        print TSV '#' . " $task\n" if ($verb);
+        $aa = int($wbs{$task}{'uid'} / 100000 + 0.1);
+        $bb = int($wbs{$task}{'uid'} /   1000 + 0.1) - 100 * $aa;
+        $cc = $wbs{$task}{'uid'} - $aa * 100000 - $bb * 1000;
+        print TSV $aa . "\t" . $bb . "\t" . $cc . "\t";
+        print TSV $wbs{$task}{'uid'};
+        for $key (@wbskeys) {
+            if (defined($wbs{$task}{$key})) {
+                $_ = $wbs{$task}{$key};
+                s/\n/ /;
+                print TSV "\t" . $wbs{$task}{$key};
+            }
+        }
+        $preds = &pred_uids("\t",$wbs{$task}{'needs'});
+        print TSV $preds . "\n";
+    }
+    close(TSV);
+}
+
+#
+# something to generate a list of predecessors by uid
+#
+sub pred_uids {
+    my ($delim,$list,$reply,@parts) = @_;
+    @parts = split(/,/,$list);
+    $reply = '';
+    for my $need (@parts) {
+        $reply .= "\t" . $wbs{&task_by_nick($need)}{'uid'};
+    }
+    return($reply);
+}
+
+#
+# go through and nuke entries with any of input: derived: either:
+#
+sub canonicalize_items {
+    for my $task (keys(%wbs)) {
+        for my $key (@keywords) {
+            if (not defined($wbs{$task}{$key}) or
+                $wbs{$task}{$key} =~ m/input:.*/ or
+                $wbs{$task}{$key} =~ m/derived:.*/ or
+                $wbs{$task}{$key} =~ m/either:.*/) {
+                # a few require numerical values
+                if ($key eq 'days') {
+                    $wbs{$task}{$key} = 0;
+                } elsif ($key eq 'done') {
+                    $wbs{$task}{$key} = 0;
+                } elsif ($key eq 'fte') {
+                    $wbs{$task}{$key} = 1;
+                } elsif ($key eq 'derate') {
+                    $wbs{$task}{$key} = 3;
+                } else {
+                    $wbs{$task}{$key} = '';
+                }
+            }
+        }
+    }
+}
+
+#
+# a simple lookup function for use as needed
+#
+sub task_by_nick {
+    my ($nick,$kv) = @_;
+    for $kv (keys(%wbs)) {
+        next if (!defined($wbs{$kv}{'nick'}));
+        if ($wbs{$kv}{'nick'} eq $nick) {
+            return($kv);
+        }
+    }
+    print "No task for '$nick', returning 'none'.\n" if ($verb);
+    return('none');
+}
+
+#
+# assign kids to every thing and domain
+#
+sub make_kids_of_things {
+    my ($kv,$nick,$pop);
+    for $kv (keys(%tasks)) {
+        $nick = $wbs{$kv}{'nick'};
+        $pop = $wbs{$kv}{'parent'};
+        if ($wbs{$pop}{'kids'} eq '') {
+            $wbs{$pop}{'kids'} = $nick;
+        } else {
+            $wbs{$pop}{'kids'} .= ',' . $nick;
+        }
+    }
+}
+sub make_kids_of_domains {
+    my ($kv,$nick,$pop);
+    for $kv (keys(%things)) {
+        $nick = $wbs{$kv}{'nick'};
+        $pop = $wbs{$kv}{'parent'};
+        if ($wbs{$pop}{'kids'} eq '') {
+            $wbs{$pop}{'kids'} = $nick;
+        } else {
+            $wbs{$pop}{'kids'} .= ',' . $nick;
+        }
+    }
+}
+
+#
+# this is a library module, so it needs to return TRUE.
+#
+1;
+#
+# eof
+#
