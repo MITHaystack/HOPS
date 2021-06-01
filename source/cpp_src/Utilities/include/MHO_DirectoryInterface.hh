390e8318c35ce25adf708d29c81dca780f35bfb4
John Barrett <barrettj@mit.edu>
2021-06-01 16:14:04 -0400

    adding directory interface

---
+++ source/cpp_src/Utilities/include/MHO_DirectoryInterface.hh
@@ -0,0 +1,54 @@
+#ifndef MHO_DirectoryInterface_HH__
+#define MHO_DirectoryInterface_HH__
+
+/*
+*File: MHO_DirectoryInterface.hh
+*Class: MHO_DirectoryInterface
+*Author: J. Barrett
+*Email: barrettj@mit.edu
+*Date:
+*Description:
+*/
+
+#include <vector>
+#include <string>
+
+namespace hops
+{
+
+class MHO_DirectoryInterface
+{
+    public:
+        MHO_DirectoryInterface();
+        virtual ~MHO_DirectoryInterface();
+
+        std::string GetDirectoryFullPath(const std::string& dirname) const;
+        bool DoesDirectoryExist(const std::string& dirname);
+        bool CreateDirectory(const std::string& dirname);
+
+        void SetCurrentDirectory(const std::string& dirname);
+        std::string GetCurrentDirectory() const;
+
+        void ReadCurrentDirectory();
+        void GetFileList(std::vector< std::string >& aFileList) const;
+        void GetSubDirectoryList(std::vector< std::string >& aSubDirList) const;
+
+    private:
+
+
+
+        std::string fCurrentDirectoryFullPath;
+
+        bool fDirectoryIsSet;
+        bool fHaveReadDirectory;
+
+        std::vector<std::string> fCurrentFileList;
+        std::vector<std::string> fCurrentSubDirectoryList;
+
+
+};
+
+
+}
+
+#endif /* end of include guard: MHO_DirectoryInterface */
