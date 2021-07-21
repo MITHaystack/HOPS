f251b888942b3e645bcdcdfd99ef4c28a0344ef0
John Barrett <barrettj@mit.edu>
2021-07-21 12:11:39 -0400

    change signature of norm_fx to pass structs and avoid global extern references

--- source/c_src/fourfit_libs/ffsearch/src/search.c
+++ source/c_src/fourfit_libs/ffsearch/src/search.c
@@ -17,8 +17,8 @@
 #define MBD_GRID_MAX 8192
 int search (struct type_pass *pass)
     {
-    fftw_complex *data = NULL; 
-    double mb_delay[MBD_GRID_MAX]; 
+    fftw_complex *data = NULL;
+    double mb_delay[MBD_GRID_MAX];
     static hops_complex rate_spectrum[MAXFREQ][MAXAP];
     static double amps[MBD_GRID_MAX][MAXAP], drtemp[MAXAP];
     int cnt, fr, i, j, station, lag, dr_index, mbd_index, ap, newmax;
@@ -50,7 +50,7 @@ int search (struct type_pass *pass)
         msg ("fftw_malloc() failed to allocate memory");
         return (-1);
         }
-    
+
                                         /* Make sure data will fit (note that auto
                                          * correlations use up twice as many lags */
     if (param.nlags*param.num_ap > MAX_APXLAG)
@@ -71,16 +71,16 @@ int search (struct type_pass *pass)
 
     for (i = 0; i <= pass->nfreq; i++)
         status.epoch_err[i]=param.frt_offset;
-   
+
         /* This section calls norm() which performs calculations that must         */
         /* be done on each frequency for each accumulation period. For every       */
         /* frequency, it also calculates an average phasecal & dominant sideband.  */
-    
+
     msg ("nfreq=%d, num_ap=%d",-1,pass->nfreq, pass->num_ap);
-    for (fr = 0; fr < pass->nfreq; fr++) 
-        {      
+    for (fr = 0; fr < pass->nfreq; fr++)
+        {
         status.pcals_accum[0] = status.pcals_accum[1] = 0.0;
-        
+
                                         // extract phase calibration for this channel
         pcalibrate (pass, fr);
         }
@@ -90,22 +90,22 @@ int search (struct type_pass *pass)
 
                                         // calculate phase cal corrections
     rotate_pcal (pass);
-    if (do_accounting) 
+    if (do_accounting)
         account ("Phase cal calculations");
 
-    for (fr = 0; fr < pass->nfreq; fr++) 
-        {      
+    for (fr = 0; fr < pass->nfreq; fr++)
+        {
         status.ap_num[0][fr] = 0;
         status.ap_num[1][fr] = 0;
         status.ap_frac[0][fr] = 0.0;
         status.ap_frac[1][fr] = 0.0;
                                         // convert the digital results to analog equivalents
-        for (ap = pass->ap_off; ap < pass->ap_off + pass->num_ap; ap++)  
+        for (ap = pass->ap_off; ap < pass->ap_off + pass->num_ap; ap++)
             if (param.corr_type == MK4HDW)
                 norm_xf (pass, fr, ap);
             else                        // use spectral version
-                norm_fx (pass, fr, ap);
-        
+                norm_fx (pass, &param, &status, fr, ap);
+
         msg ("Freq %d, ap's by sideband through norm = %d, %d", -1,
                 fr, status.ap_num[0][fr], status.ap_num[1][fr]);
         }
@@ -119,7 +119,7 @@ int search (struct type_pass *pass)
         }
     status.epoch_off_cent = -(status.epoch_off_cent / status.total_ap + 0.5)
                 * param.acc_period - param.frt_offset;
-        
+
     freq_spacing (pass);                /* Calculate spacing of freq channels */
                                         /* trap for mbd grid having too many pts */
     if (status.grid_points > MBD_GRID_MAX)
@@ -130,16 +130,16 @@ int search (struct type_pass *pass)
         return (-1);
         }
 
-    if (do_accounting) 
+    if (do_accounting)
         account ("Normalization");
 
         /* This section generates the delay resolution function, and searches for   */
         /* the maximum. At each SB delay it generates a delay rate function for each */
         /* frequency. Then, for each delay rate, it calculates the delay resolution */
         /* function. It then runs through all the delay res. values to find the     */
-        /* maximum.                                                                 */          
-        
-    search_windows (pass);  
+        /* maximum.                                                                 */
+
+    search_windows (pass);
                                         /* One of the plot arrays gets filled up */
                                         /* in the next loop, so we need to initialize */
                                         /* the arrays here, rather than later on */
@@ -153,16 +153,16 @@ int search (struct type_pass *pass)
         status.lag = lag;
                                         /* drate spectrum for each freq */
                                         /* This weighted by fractional AP */
-        for (fr = 0; fr < pass->nfreq; fr++) 
+        for (fr = 0; fr < pass->nfreq; fr++)
             delay_rate (pass, fr, rate_spectrum[fr]);
 
-        for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++) 
+        for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++)
             {
             status.dr = dr_index;                       /* Clear data array and */
                                                 /* Fill with delay rate data */
             for (i = 0; i < status.grid_points; i++)
                 data[i] = 0.0;
-                
+
             for (fr = 0; fr < pass->nfreq; fr++)
                 data[status.mb_index[fr]] = rate_spectrum[fr][dr_index];
 
@@ -176,7 +176,7 @@ int search (struct type_pass *pass)
                 i = (i+1) % status.grid_points;
                 j = i-status.grid_points/2;
                 if (j < 0) j += status.grid_points;
-                mb_delay[i] = cabs (data[j]); 
+                mb_delay[i] = cabs (data[j]);
                 }
             while (i != status.win_mb[1]);
                                                 /* Normalize delay res. value and store */
@@ -187,7 +187,7 @@ int search (struct type_pass *pass)
                 status.mbd = mbd_index;
                                         // mean amplitude
                 amps[mbd_index][dr_index] = mb_delay[mbd_index] / status.total_ap_frac;
-                }  
+                }
             while (mbd_index != status.win_mb[1]);
             }
                                         // smooth amplitudes over delay rates, iff incoherent
@@ -196,19 +196,19 @@ int search (struct type_pass *pass)
             {
             n = status.drsp_size * param.acc_period / (2.0 * pass->control.t_cohere) + 0.5;
             msg ("convolving rate spectrum over %d resolution elements", -3, n);
-        
+
             mbd_index = status.win_mb[0] - 1;
             do                          // smooth over each of the mbd channels
                 {
                 mbd_index = (mbd_index+1) % status.grid_points;
                                         // create temporary copy of this delay rate row
-                for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++) 
+                for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++)
                     {
                     drtemp[dr_index] = amps[mbd_index][dr_index];
                     amps[mbd_index][dr_index] = 0.0;
                     }
                                         // average together the surrounding 2n+1 dr cells
-                for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++) 
+                for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++)
                     {                   // ensure that array bounds are not exceeded
                     jlo = dr_index - n;
                     jlo = (jlo < 0) ? 0 : jlo;
@@ -220,14 +220,14 @@ int search (struct type_pass *pass)
                         amps[mbd_index][dr_index] += drtemp[j];
                     amps[mbd_index][dr_index] /= (jhi - jlo + 1.0);
                     }
-                }  
+                }
             while (mbd_index != status.win_mb[1]);
             }
-            
+
 
                                         // search over mbd and dr for maximum amplitude
         max_amp[lag] = -1.0;
-        for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++) 
+        for (dr_index = status.win_dr[0]; dr_index <= status.win_dr[1]; dr_index++)
             {
             status.dr = dr_index;
 
@@ -240,16 +240,16 @@ int search (struct type_pass *pass)
                 if (amps[mbd_index][dr_index] > max_amp[lag])
                     {
                     max_amp[lag] = amps[mbd_index][dr_index];
-                    mbdmax[lag] = mbd_index;                            
+                    mbdmax[lag] = mbd_index;
                     drmax[lag] = dr_index;
                     msg ("search: lag %d dr_index %d mbd_index %d amp %f",-3,
                               lag,   dr_index,   mbd_index,   max_amp[lag]);
                     }
 
-                }  
+                }
             while (mbd_index != status.win_mb[1]);
             }
-            
+
                                         /* Store mbdmax for this lag */
         update (pass, mbdmax[lag], max_amp[lag], lag, drmax[lag], LAG);
         }
@@ -279,11 +279,11 @@ int search (struct type_pass *pass)
 
     msg ("from search: global_max %f max_lag %d drcell %d mbdcell %d",
           0, global_max, max_lag, max_dr_cell, max_mbd_cell);
-        
+
     msg ("finished fringe search ", 1);
     if (do_accounting) account ("Grid search");
 
     fftw_free (data);   // clean up the malloc'ed data array
     return (0);         /* This return should be modified to give some indication */
                         /* of whether the search was successful.                  */
-    }   
+    }
