53cb6ee860652e2095190ac10d4a4be9004387e4
John Barrett <barrettj@mit.edu>
2021-07-21 15:19:06 -0400

    rework vex and corel test import

--- source/cpp_src/Applications/TestNormFX.cc
+++ source/cpp_src/Applications/TestNormFX.cc
@@ -21,50 +21,176 @@
 using namespace hops;
 
 
-//convert a corel file
-void ConvertCorel(const std::string root_file,
-                  const std::string& input_file,
-                  ch_baseline_data_type*& ch_bl_data,
-                  ch_baseline_weight_type*& ch_bl_wdata
+// //get list of all the files (and directories) in directory
+// std::vector< std::string > allFiles;
+// std::vector< std::string > allDirs;
+// dirInterface.GetFileList(allFiles);
+// dirInterface.GetSubDirectoryList(allDirs);
+//
+// //sort files, locate root, corel and station files
+// std::vector< std::string > corelFiles;
+// std::vector< std::string > stationFiles;
+// std::string root_file;
+// dirInterface.GetRootFile(allFiles, root_file);
+// std::cout<<"root file = "<<root_file<<std::endl;
+//
+// //convert root file ovex data to JSON
+// MHO_MK4VexInterface vexInter;
+// vexInter.OpenVexFile(root_file);
+// struct vex* root = vexInter.GetVex();
+// json ovex;
+// bool ovex_ok = vexInter.ExportVexFileToJSON(ovex);
+
+
+//read and fill-in the vex data as json and vex struct objects
+bool GetVex(MHO_DirectoryInterface& dirInterface,
+            MHO_MK4VexInterface& vexInterface,
+            json& json_vex,
+            struct vex*& root)
+{
+    //get list of all the files (and directories) in directory
+    std::vector< std::string > allFiles;
+    dirInterface.GetFileList(allFiles);
+    std::string root_file;
+    dirInterface.GetRootFile(allFiles, root_file);
+
+    //convert root file ovex data to JSON, and export root/ovex ptr
+    vexInterface.OpenVexFile(root_file);
+    bool ovex_ok = vexInterface.ExportVexFileToJSON(json_vex);
+    root = vexInterface.GetVex();
+    return ovex_ok;
+}
+
+
+// read a corel file and fill in old style and new style data containers
+bool GetCorel(MHO_DirectoryInterface& dirInterface,
+              MHO_MK4CorelInterface& corelInterface,
+              const std::string& baseline,
+              struct mk4_corel*& cdata,
+              ch_baseline_data_type*& ch_bl_data,
+              ch_baseline_weight_type*& ch_bl_wdata
 )
 {
-    MHO_MK4CorelInterface mk4inter;
+    bool corel_ok = false;
+    // ch_baseline_data_type* ch_bl_data = nullptr;
+    // ch_baseline_weight_type* ch_bl_wdata = nullptr;
+    std::string root_file;
+    std::vector< std::string > allFiles, corelFiles;
+    dirInterface.GetFileList(allFiles);
+    dirInterface.GetRootFile(allFiles, root_file);
+    dirInterface.GetCorelFiles(allFiles, corelFiles);
 
-    std::cout<<"input_file = "<<input_file<<std::endl;
-    mk4inter.SetCorelFile(input_file);
-    mk4inter.SetVexFile(root_file);
-    mk4inter.ExtractCorelFile();
-    baseline_data_type* bl_data = mk4inter.GetExtractedVisibilities();
-    baseline_weight_type* bl_wdata = mk4inter.GetExtractedWeights();
-
-    MHO_VisibilityChannelizer channelizer;
-    channelizer.SetInput(bl_data);
-    ch_bl_data = new ch_baseline_data_type();
-    channelizer.SetOutput(ch_bl_data);
-    bool init = channelizer.Initialize();
-    if(init)
+    for(auto it = corelFiles.begin(); it != corelFiles.end(); it++)
     {
-        std::cout<<"initialization done"<<std::endl;
-        bool exe = channelizer.ExecuteOperation();
-        if(exe){std::cout<<"vis channelizer done"<<std::endl;}
+        std::string st_pair, root_code;
+        std::string input_basename = dirInterface.GetBasename(*it);
+        dirInterface.SplitCorelFileBasename(input_basename, st_pair, root_code);
+        if(st_pair == baseline)
+        {
+            std::cout<<"found corel file: "<< *it <<std::endl;
+            corelInterface.SetCorelFile(*it);
+            corelInterface.SetVexFile(root_file);
+            corelInterface.ExtractCorelFile();
+            baseline_data_type* bl_data = corelInterface.GetExtractedVisibilities();
+            baseline_weight_type* bl_wdata = corelInterface.GetExtractedWeights();
+
+            MHO_VisibilityChannelizer channelizer;
+            channelizer.SetInput(bl_data);
+            ch_bl_data = new ch_baseline_data_type();
+            channelizer.SetOutput(ch_bl_data);
+            bool init = channelizer.Initialize();
+            bool exe = false;
+            if(init)
+            {
+                std::cout<<"initialization done"<<std::endl;
+                exe = channelizer.ExecuteOperation();
+                if(exe){std::cout<<"vis channelizer done"<<std::endl;}
+            }
+
+            MHO_WeightChannelizer wchannelizer;
+            wchannelizer.SetInput(bl_wdata);
+            ch_bl_wdata = new ch_baseline_weight_type();
+            wchannelizer.SetOutput(ch_bl_wdata);
+            bool winit = wchannelizer.Initialize();
+            bool wexe = false;
+            if(winit)
+            {
+                std::cout<<"initialization done"<<std::endl;
+                wexe = wchannelizer.ExecuteOperation();
+                if(wexe){std::cout<<"weight channelizer done"<<std::endl;}
+            }
+
+            //get the raw mk4 type data
+            cdata = corelInterface.GetCorelData();
+
+            if(exe && wexe){corel_ok = true;}
+            break;
+        }
     }
-
-    MHO_WeightChannelizer wchannelizer;
-    wchannelizer.SetInput(bl_wdata);
-    ch_bl_wdata = new ch_baseline_weight_type();
-    wchannelizer.SetOutput(ch_bl_wdata);
-    bool winit = wchannelizer.Initialize();
-    if(winit)
-    {
-        std::cout<<"initialization done"<<std::endl;
-        bool wexe = wchannelizer.ExecuteOperation();
-        if(wexe){std::cout<<"weight channelizer done"<<std::endl;}
-    }
-
-    delete bl_data;
-    delete bl_wdata;
-
+    return corel_ok;
 }
+
+
+// bool GetStationData(MHO_DirectoryInterface& dirInterface,
+//                     MHO_MK4CorelInterface& corelInterface,
+//                     const std::string& baseline,
+//                     struct mk4_sda*& cdata,
+//                     ch_baseline_data_type*& ch_bl_data,
+//                     ch_baseline_weight_type*& ch_bl_wdata
+//
+//
+
+// ////////////////////////////////////////////////////////////////////////////////
+//
+// //convert a corel file
+// void ConvertCorel(const std::string root_file,
+//                   const std::string& input_file,
+//                   ch_baseline_data_type*& ch_bl_data,
+//                   ch_baseline_weight_type*& ch_bl_wdata
+// )
+// {
+
+// }
+//
+//
+
+// //convert a station data  file
+// void ConvertStation(const std::string root_file, const std::string& input_file, station_coord_data_type*& st_data)
+// {
+//     MHO_MK4StationInterface mk4inter;
+//
+//     std::cout<<"input_file = "<<input_file<<std::endl;
+//     mk4inter.SetStationFile(input_file);
+//     mk4inter.SetVexFile(root_file);
+//     st_data = mk4inter.ExtractStationFile();
+// }
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -127,6 +253,28 @@ int main(int argc, char** argv)
 
     //directory interface
     MHO_DirectoryInterface dirInterface;
+    dirInterface.SetCurrentDirectory(input_dir);
+    dirInterface.ReadCurrentDirectory();
+
+    //get the root (ovex) file information
+    MHO_MK4VexInterface vexInterface;
+    json json_vex;
+    struct vex* root = nullptr;
+    bool ovex_ok = GetVex(dirInterface, vexInterface, json_vex, root);
+
+    MHO_MK4CorelInterface corelInterface;
+    struct mk4_corel* cdata = nullptr;
+    ch_baseline_data_type* ch_bl_data = nullptr;
+    ch_baseline_weight_type* ch_bl_wdata = nullptr;
+    bool corel_ok = GetCorel(dirInterface, corelInterface, baseline, cdata, ch_bl_data, ch_bl_wdata);
+    std::cout<<"data ptrs = "<<cdata<<", "<<ch_bl_data<<", "<<ch_bl_wdata<<std::endl;
+
+    MHO_MK4StationInterface stationInterface;
+    struct mk4_sdata sdata[2];
+
+
+
+/*
 
     //get list of all the files (and directories) in directory
     std::vector< std::string > allFiles;
