b647aa0f106c09fd9009d059b139776069f9c83b
gcrew <gbc@haystack.mit.edu>
2021-08-10 16:05:30 -0400

    ok, 2836 works in the new tree

--- source/c_src/applications/test/chk_2836.sh
+++ source/c_src/applications/test/chk_2836.sh
@@ -32,12 +32,42 @@ source "$MHO_REGRESSION_DATA/switches/test_config.sh"
 [ -n "$MHO_REGRESSION_REQ" ] || { echo requirement not set ; exit 99; }
 
 # first check that everything needed is actually present
-# FIXME # whatever
+data=$MHO_REGRESSION_DATA/ff_testdata/2836
+[ -d "$data" ] || { echo data not present when it should be ; exit 5; }
+[ -f "$data/cf2836" ] || { echo config file missing ; exit 6; }
+
+# since we rely on this for our test, make sure it is generated
+rm -f ff-2836.ps
+
+# more of a unit test here
+export DEF_CONTROL=/dev/null
+export TEXT=$abs_top_srcdir/source/c_src/vex/text
 
 # second execute some tests and set $passfail appropriately
-# FIXME # however
+$verb && echo \
+$fourfit -t -d diskfile:ff-2836.ps -b AE:X \\ && echo \
+    -c $data/cf2836 \\ && echo \
+    $data/scan001/2145+067.olomfh
+$fourfit -t -d diskfile:ff-2836.ps -b AE:X \
+    -c $data/cf2836 \
+    $data/scan001/2145+067.olomfh
 
-for dir in "$nukables" ; do echo nuking $dir ; rm -rf $dir ; done
+# output file?
+[ -f ./ff-2836.ps ] || { echo ./ff-2836.ps missing && exit 7; }
+
+# pluck out line containing the snr and parse it
+line=$(grep '7570 9653' ./ff-2836.ps)
+
+# check SNR
+IFS='()'
+read a snr b <<<"$line"
+low=139.9
+high=140.9
+aok=$(echo "$snr>$low && $snr<$high" | bc)
+$verb && echo aok is $aok and "$low < $snr < $high" is expected from: $line
+[ "$aok" -gt 0 ] && passfail=0 || passfail=8
+
+for dir in $nukables ; do echo nuking $dir ; rm -rf $dir ; done
 [ "$MHO_REGRESSION_REQ" = ok ] || echo $something $MHO_REGRESSION_REQ $passfail
 exit $passfail
 #
