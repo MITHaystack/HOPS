904c0f7abcce53c613654e48f1c77af4450d5ad3
gcrew <gbc@haystack.mit.edu>
2021-08-10 19:36:17 -0400

    idiot

--- data/switches/provider.sh
+++ data/switches/provider.sh
@@ -4,6 +4,6 @@
 # but eventually, we may have additional data that is not legacy
 #
 [ -x $MHO_REGRESSION_DATA/bootstrap/legacy_unpack.sh ] &&
-exec $MHO_REGRESSION_DATA/bootstrap/legacy_unpack.sh ||
+exec $MHO_REGRESSION_DATA/bootstrap/legacy_unpack.sh "$@" ||
 { echo configuration error ; exit 99 ; }
 # eof
