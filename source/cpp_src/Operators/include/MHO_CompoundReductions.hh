01100d56c3a4d72eb01abfa9d3ff51ff2a95d1dd
John Barrett <barrettj@mit.edu>
2021-08-19 11:04:52 -0400

    fix ODR for reduction identities

--- source/cpp_src/Operators/include/MHO_CompoundReductions.hh
+++ source/cpp_src/Operators/include/MHO_CompoundReductions.hh
@@ -16,42 +16,42 @@
 template< typename XValueType >
 struct MHO_SumIdentity
 {
-    static constexpr XValueType value = 0;
+    static XValueType value() {return 0.0;};
 };
 
 //specializations for common types
-template<> struct MHO_SumIdentity<bool>{static constexpr bool value = false;};
-template<> struct MHO_SumIdentity<int>{static constexpr int value = 0;};
-template<> struct MHO_SumIdentity<short>{static constexpr short value = 0;};
-template<> struct MHO_SumIdentity<unsigned int>{static constexpr unsigned int value = 0;};
-template<> struct MHO_SumIdentity<std::size_t>{static constexpr std::size_t value = 0;};
-template<> struct MHO_SumIdentity<float>{static constexpr float value = 0.0;};
-template<> struct MHO_SumIdentity<double>{static constexpr double value = 0.0;};
-template<> struct MHO_SumIdentity<long double>{static constexpr long double value = 0.0;};
-template<> struct MHO_SumIdentity< std::complex< float > >{static constexpr std::complex< float > value = std::complex<float>(0.0, 0.0);};
-template<> struct MHO_SumIdentity< std::complex< double > >{static constexpr std::complex< double > value = std::complex<double>(0.0, 0.0);};
-template<> struct MHO_SumIdentity< std::complex< long double > >{static constexpr std::complex< long double > value = std::complex<long double>(0.0, 0.0);};
+template<> struct MHO_SumIdentity<bool>{static inline bool value(){ return false;};};
+template<> struct MHO_SumIdentity<int>{static inline int value(){ return 0;};};
+template<> struct MHO_SumIdentity<short>{static inline short value(){ return 0;};};
+template<> struct MHO_SumIdentity<unsigned int>{static inline unsigned int value(){ return 0;};};
+template<> struct MHO_SumIdentity<std::size_t>{static inline std::size_t value(){ return 0;};};
+template<> struct MHO_SumIdentity<float>{static inline float value(){ return 0.0;};};
+template<> struct MHO_SumIdentity<double>{static inline double value(){ return 0.0;};};
+template<> struct MHO_SumIdentity<long double>{static inline long double value(){ return 0.0;};};
+template<> struct MHO_SumIdentity< std::complex< float > >{static inline std::complex< float > value(){ return std::complex<float>(0.0, 0.0);};};
+template<> struct MHO_SumIdentity< std::complex< double > >{static inline std::complex< double > value(){ return std::complex<double>(0.0, 0.0);};};
+template<> struct MHO_SumIdentity< std::complex< long double > >{static inline std::complex< long double > value(){ return std::complex<long double>(0.0, 0.0);};};
 
 
 
 template< typename XValueType >
 struct MHO_MultiplyIdentity
 {
-    static constexpr XValueType value = 1;
+    static XValueType value() {return 0.0;};
 };
 
 //specializations for common types
-template<> struct MHO_MultiplyIdentity<bool>{static constexpr bool value = true;};
-template<> struct MHO_MultiplyIdentity<int>{static constexpr int value = 1;};
-template<> struct MHO_MultiplyIdentity<short>{static constexpr short value = 1;};
-template<> struct MHO_MultiplyIdentity<unsigned int>{static constexpr unsigned int value = 1;};
-template<> struct MHO_MultiplyIdentity<std::size_t>{static constexpr std::size_t value = 1;};
-template<> struct MHO_MultiplyIdentity<float>{static constexpr float value = 1.0;};
-template<> struct MHO_MultiplyIdentity<double>{static constexpr double value = 1.0;};
-template<> struct MHO_MultiplyIdentity<long double>{static constexpr long double value = 1.0;};
-template<> struct MHO_MultiplyIdentity< std::complex< float > >{static constexpr std::complex< float > value = std::complex<float>(1.0, 0.0);};
-template<> struct MHO_MultiplyIdentity< std::complex< double > >{static constexpr std::complex< double > value = std::complex<double>(1.0, 0.0);};
-template<> struct MHO_MultiplyIdentity< std::complex< long double > >{static constexpr std::complex< long double > value = std::complex<long double>(1.0, 0.0);};
+template<> struct MHO_MultiplyIdentity<bool>{static inline bool value(){ return true;};};
+template<> struct MHO_MultiplyIdentity<int>{static inline int value(){ return 1;};};
+template<> struct MHO_MultiplyIdentity<short>{static inline short value(){ return 1;};};
+template<> struct MHO_MultiplyIdentity<unsigned int>{static inline unsigned int value(){ return 1;};};
+template<> struct MHO_MultiplyIdentity<std::size_t>{static inline std::size_t value(){ return 1;};};
+template<> struct MHO_MultiplyIdentity<float>{static inline float value(){ return 1.0;};};
+template<> struct MHO_MultiplyIdentity<double>{static inline double value(){ return 1.0;};};
+template<> struct MHO_MultiplyIdentity<long double>{static inline long double value(){ return 1.0;};};
+template<> struct MHO_MultiplyIdentity< std::complex< float > >{static inline std::complex< float > value(){ return std::complex<float>(1.0, 0.0);};};
+template<> struct MHO_MultiplyIdentity< std::complex< double > >{static inline std::complex< double > value(){ return std::complex<double>(1.0, 0.0);};};
+template<> struct MHO_MultiplyIdentity< std::complex< long double > >{static inline std::complex< long double > value(){ return std::complex<long double>(1.0, 0.0);};};
 
 
 //struct which implements operator as compound summation
@@ -63,7 +63,7 @@ struct MHO_CompoundSum
         a += b;
     }
 
-    const XValueType identity = MHO_SumIdentity<XValueType>::value;
+    const XValueType identity = MHO_SumIdentity<XValueType>::value();
 };
 
 //struct which implements operator as compound multiplication
@@ -75,7 +75,7 @@ struct MHO_CompoundMultiply
         a *= b;
     }
 
-    const XValueType identity = MHO_MultiplyIdentity<XValueType>::value;
+    const XValueType identity = MHO_MultiplyIdentity<XValueType>::value();
 };
 
 //TODO -- Can we think of any other useful reduction operations to put here?
