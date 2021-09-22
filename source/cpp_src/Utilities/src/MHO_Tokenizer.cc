c17b55fa7f25cb7f71435b8223274a0452aeaaa9
violetp <violetp@mit.edu>
2021-09-22 15:53:21 -0400

    remove reference to const

--- source/cpp_src/Utilities/src/MHO_Tokenizer.cc
+++ source/cpp_src/Utilities/src/MHO_Tokenizer.cc
@@ -6,65 +6,64 @@
 namespace hops
 {
 
-    public:
-        MHO_Tokenizer::MHO_Tokenizer()
+MHO_Tokenizer::MHO_Tokenizer()
+{
+    fDelim = " "; //default delim is space
+    fString = nullptr;
+    fIncludeEmptyTokens = false;
+};
+MHO_Tokenizer::~MHO_Tokenizer(){;};
+
+void MHO_Tokenizer::SetIncludeEmptyTokensTrue(){fIncludeEmptyTokens = true;};
+
+void MHO_Tokenizer::SetIncludeEmptyTokensFalse(){fIncludeEmptyTokens = false;};
+
+void MHO_Tokenizer::SetString(const std::string* aString){fString = aString;};
+
+void MHO_Tokenizer::SetDelimiter(const std::string& aDelim){fDelim = aDelim;};
+
+void MHO_Tokenizer::GetTokens(std::vector< std::string>* tokens)
+{
+    if(tokens != NULL && fString != NULL)
+    {
+        tokens->clear();
+        if(fDelim.size() > 0)
         {
-            fDelim = " "; //default delim is space
-            fString = nullptr;
-            fIncludeEmptyTokens = false;
-        };
-        MHO_Tokenizer::~MHO_Tokenizer(){;};
 
-        void MHO_Tokenizer::SetIncludeEmptyTokensTrue(){fIncludeEmptyTokens = true;};
-
-        void MHO_Tokenizer::SetIncludeEmptyTokensFalse(){fIncludeEmptyTokens = false;};
-
-        void MHO_Tokenizer::SetString(const std::string* aString){fString = aString;};
-
-        void MHO_Tokenizer::SetDelimiter(const std::string& aDelim){fDelim = aDelim;};
-
-        void MHO_Tokenizer::GetTokens(std::vector< std::string>* tokens) const
-        {
-            if(tokens != NULL && fString != NULL)
+            size_t start = 0;
+            size_t end = 0;
+            size_t length = 0;
+            while( end != std::string::npos )
             {
-                tokens->clear();
-                if(fDelim.size() > 0)
+                end = fString->find(fDelim, start);
+
+                if(end == std::string::npos)
                 {
-
-                    size_t start = 0;
-                    size_t end = 0;
-                    size_t length = 0;
-                    while( end != std::string::npos )
-                    {
-                        end = fString->find(fDelim, start);
-
-                        if(end == std::string::npos)
-                        {
-                            length = std::string::npos;
-                        }
-                        else
-                        {
-                            length = end - start;
-                        }
+                    length = std::string::npos;
+                }
+                else
+                {
+                    length = end - start;
+                }
 
 
-                        if( fIncludeEmptyTokens || ( (length > 0 ) && ( start < fString->size() ) ) )
-                        {
-                            tokens->push_back( fString->substr(start,length) );
-                        }
+                if( fIncludeEmptyTokens || ( (length > 0 ) && ( start < fString->size() ) ) )
+                {
+                    tokens->push_back( fString->substr(start,length) );
+                }
 
-                        if( end > std::string::npos - fDelim.size() )
-                        {
-                            start = std::string::npos;
-                        }
-                        else
-                        {
-                            start = end + fDelim.size();
-                        }
-                    }
+                if( end > std::string::npos - fDelim.size() )
+                {
+                    start = std::string::npos;
+                }
+                else
+                {
+                    start = end + fDelim.size();
                 }
             }
         }
+    }
+}
 
 }
 
