3122fb20cf52c1ceebe4a5c7cc407644ea86a7e3
0c8a1fd <0c8a1fd@local>
2021-09-22 15:09:17 -0400

    refactor the MHO_Tokenizer header file

---
+++ source/cpp_src/Utilities/src/MHO_Tokenizer.cc
@@ -0,0 +1,70 @@
+#include "MHO_Tokenizer.hh"
+
+#include <vector>
+#include <string>
+
+namespace hops
+{
+
+    public:
+        MHO_Tokenizer::MHO_Tokenizer()
+        {
+            fDelim = " "; //default delim is space
+            fString = nullptr;
+            fIncludeEmptyTokens = false;
+        };
+        MHO_Tokenizer::~MHO_Tokenizer(){;};
+
+        void MHO_Tokenizer::SetIncludeEmptyTokensTrue(){fIncludeEmptyTokens = true;};
+
+        void MHO_Tokenizer::SetIncludeEmptyTokensFalse(){fIncludeEmptyTokens = false;};
+
+        void MHO_Tokenizer::SetString(const std::string* aString){fString = aString;};
+
+        void MHO_Tokenizer::SetDelimiter(const std::string& aDelim){fDelim = aDelim;};
+
+        void MHO_Tokenizer::GetTokens(std::vector< std::string>* tokens) const
+        {
+            if(tokens != NULL && fString != NULL)
+            {
+                tokens->clear();
+                if(fDelim.size() > 0)
+                {
+
+                    size_t start = 0;
+                    size_t end = 0;
+                    size_t length = 0;
+                    while( end != std::string::npos )
+                    {
+                        end = fString->find(fDelim, start);
+
+                        if(end == std::string::npos)
+                        {
+                            length = std::string::npos;
+                        }
+                        else
+                        {
+                            length = end - start;
+                        }
+
+
+                        if( fIncludeEmptyTokens || ( (length > 0 ) && ( start < fString->size() ) ) )
+                        {
+                            tokens->push_back( fString->substr(start,length) );
+                        }
+
+                        if( end > std::string::npos - fDelim.size() )
+                        {
+                            start = std::string::npos;
+                        }
+                        else
+                        {
+                            start = end + fDelim.size();
+                        }
+                    }
+                }
+            }
+        }
+
+}
+
