383220db1cc107da1e7f07a28af3ea6aaf50e2ee
John Barrett <barrettj@mit.edu>
2021-09-30 16:51:22 -0400

    more work on objects section of specification

---
+++ doc/specification/code/MHO_ExtensibleElement.hh
@@ -0,0 +1,82 @@
+#include <vector>
+
+//forward declare these types
+class MHO_Visitor;
+class MHO_Element;
+class MHO_ExtensibleElement;
+template<class XExtensionType> class MHO_ExtendedElement;
+
+class MHO_Element
+{
+    public:
+        MHO_Element(){};
+        virtual ~MHO_Element(){};
+};
+
+class MHO_ExtensibleElement
+{
+    public:
+        MHO_ExtensibleElement(){};
+        virtual ~MHO_ExtensibleElement()
+        {
+            for(auto it = fExtensions.begin(); it != fExtensions.end(); it++)
+            {
+                delete (*it);
+                (*it) = nullptr;
+            }
+            fExtensions.clear();
+        }
+
+        template<class XExtensionType > MHO_ExtendedElement< XExtensionType >* MakeExtension();
+        template<class XExtensionType > MHO_ExtendedElement< XExtensionType >* AsExtension();
+
+    protected:
+
+        std::vector< MHO_Element* > fExtensions;
+};
+
+template<class XExtensionType>
+inline MHO_ExtendedElement<XExtensionType>*
+MHO_ExtensibleElement::MakeExtension()
+{
+    MHO_ExtendedElement<XExtensionType>* extention;
+    for(auto it = fExtensions.begin(); it != fExtensions.end(); it++)
+    {
+        extention = dynamic_cast<MHO_ExtendedElement<XExtensionType>*>( *it );
+        if(extention != nullptr){delete extention; fExtensions.erase(it); break; }
+    }
+    extention = new MHO_ExtendedElement<XExtensionType>(this);
+    fExtensions.push_back(extention);
+    return extention;
+}
+
+template<class XExtensionType>
+inline MHO_ExtendedElement<XExtensionType>*
+MHO_ExtensibleElement::AsExtension()
+{
+    MHO_ExtendedElement<XExtensionType>* extention;
+    for(auto it = fExtensions.begin(); it != fExtensions.end(); it++)
+    {
+        extention = dynamic_cast<MHO_ExtendedElement<XExtensionType>*>( *it );
+        if (extention != nullptr){return extention;};
+    }
+    return nullptr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+template<class XExtensionType >
+class MHO_ExtendedElement: public MHO_Element, public XExtensionType
+{
+    public:
+        MHO_ExtendedElement(MHO_ExtensibleElement* parent):
+            XExtensionType(parent),
+            fParent(parent)
+        {};
+        virtual ~MHO_ExtendedElement(){};
+
+    protected:
+
+        MHO_ExtensibleElement* fParent;
+};
