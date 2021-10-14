modify list handling and fix scan time calculation

source/python_src/visualization/alist_test.py
@@ -56,8 +56,8 @@ class SelectionParamBoxGrid(QWidget):
     def __init__(self, alist_data, orient='col'):
         super().__init__()
 
-        self.snrmin = np.min(alist_data.snr)
-        self.snrmax = np.max(alist_data.snr)
+        self.snrmin = np.min(alist_data.records['snr'])
+        self.snrmax = np.max(alist_data.records['snr'])
         
         # Create a text entry box
         snrmin_box_label = QLabel('Min SNR')
@@ -314,17 +314,15 @@ class SelectionPanel(QWidget):
         # these need to be public variables (self) so they are callable!
         self.param_textbox = SelectionParamBoxGrid(alist_data)
         
-        self.baseline_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.baselines), 'Baselines', autocorr=True)
+        self.baseline_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.records['baseline']), 'Baselines', autocorr=True)
 
         self.station_checkboxes = SelectionCheckboxGrid(alist_data.unique_stations, 'Stations')
         
-        self.qcode_checkboxes = QCodeCheckboxGrid(alist_data.qcodes)
+        self.qcode_checkboxes = QCodeCheckboxGrid(alist_data.records['qcode'])
         
-        self.source_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.sources), 'Sources')
+        self.source_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.records['source']), 'Sources')
 
-        #self.scan_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.scans), 'Scans')
-
-        self.pol_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.pols), 'Polarizations')
+        self.pol_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.records['pols']), 'Polarizations')
 
 
 
@@ -396,7 +394,7 @@ class ScanPanel(QWidget):
         self.alist_data = alist_data
         
         # these need to be public variables (self) so they are callable!
-        self.scan_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.scans), 'Scans')
+        self.scan_checkboxes = SelectionCheckboxGrid(np.unique(alist_data.records['scan']), 'Scans')
 
         select_vbox = QVBoxLayout()
         select_vbox.addLayout(self.scan_checkboxes.label_hbox)
@@ -472,7 +470,7 @@ class PlotPanel(QWidget):
 
         alist_idx = self.alist_data.get_record_indices(data_selection_dict)
 
-        print(np.unique(np.array(self.alist_data.baselines)[alist_idx]))
+        #print(np.unique(np.array(self.alist_data.records['baseline'])[alist_idx]))
 
         # note, this is not sorted - should we sort it?
         #print(alist_idx)
@@ -483,8 +481,9 @@ class PlotPanel(QWidget):
         
 
         #fignum = np.random.randint(0,1000)
-        fig = alist_plt.plot_alist_data(1, np.array(self.alist_data.times)[alist_idx], np.array(self.alist_data.amplitudes)[alist_idx],
-                                        'Time (UT on day 121-105', 'amplitude (e-4)')
+        fig = alist_plt.plot_alist_data(1, np.array(self.alist_data.records['scan_time'])[alist_idx],
+                                        np.array(self.alist_data.records['amplitude'])[alist_idx],
+                                        'Time (Fractional DOY)', 'amplitude (e-4)')
 
         #plt.figure(fignum)
         plt.show()
@@ -608,7 +607,8 @@ class Init_tab(QWidget):
             self.w = DataWindow(alist_data)
         self.w.show()
 
-        
+        #print(alist_data.records.scan_time)
+        #print(np.unique(alist_data.records.scan_time))
 
 
 

source/python_src/visualization/parse_alist.py
@@ -2,7 +2,7 @@
 # eventually this will be replaced with a new format??
 import numpy as np
 
-
+#from collections import defaultdict
 
 # should include some sanity checks and error handling
 
@@ -11,27 +11,46 @@ import numpy as np
 
 # method: given dictionaries of selected parameters, return indices of alist records to plot
 
+# This module makes many assumptions about alist data format! How reliable is this format?
+# Need to store units for each field
+
+# list of dictionary fields for each record in the alist file
+alist_fields = ['source', # phonebook name for the source observed
+                'scan', # scan name, eg 105-0347 or 210-1800a (DOY-HHMM[a,b,c])
+                'baseline', # two-letter baseline for each record
+                'stations', # two-element list of single-letter station identifiers
+                'scan_DOY', # scan day of year
+                'scan_time', # scan time in fractional DOY format (DOY.1234)
+                'scan_length', # scan length in seconds (different from DUR column)
+                'pols', # two-letter description of polarization combination for this baseline (eg XX, RR, XY, YR, etc)
+                'qcode', # scan quality code (0-9, A-H)
+                'amplitude', # amplitude in units of 1e-4
+                'snr',
+                'sbd', # single-band delay
+                'mbd', # multi-band delay
+                'delay_rate']
+
+                
+
 
 
 class ParseAlist:
 
     def __init__(self, afile):
 
-        self.sources = []
-        self.scans = []
-        self.amplitudes = []
-        self.times = []
-        self.baselines = []
-        self.stations = []
-        self.unique_stations = [] # stations are stored as pairs, so get the unique list separately
-        self.pols = []
-        self.qcodes = []
-        self.snr = []
-        self.sbd = []
-        self.mbd = []
-        self.delay_rate = []
-        self.length = []
+
+        self.records = dict.fromkeys(alist_fields)
+        #self.records = defaultdict(list)
+
+        # if we initialize all the dict values to a list with fromkeys(), it will be the *same* list
+        # so initialize the values in a loop
+        for field in alist_fields:
+            self.records[field] = []
         
+        self.unique_stations = [] # stations are stored as pairs, store the list of unique stations separately
+        
+        self.start_day = 0 # for plotting?
+
         
         f = open(afile, 'r')
         lines = f.readlines()
@@ -47,37 +66,38 @@ class ParseAlist:
             # build lists of the useful columns
             #root.append(cols[1])
 
-            self.scans.append(cols[8])
+            self.records['scan_length'].append(cols[5])
+
+            self.records['scan'].append(cols[8])
             
-            # TIME*TAG column is in fractional DOY format?
+            # TIME*TAG column is in DOY-HHMMSS format
+            # reformat into fractional DOY
             timetag = cols[11].split('-')
-            self.times.append(int(timetag[0])+float('0.'+timetag[1]))
+            fractional_day = float(timetag[1][0:2])*3600 + float(timetag[1][2:4])*60 + float(timetag[1][4:6])
+            self.records['scan_time'].append(int(timetag[0])+fractional_day/86400.)
             
-            #self.sources.append(cols[13][0:5]) # only grab the first five characters of the source name
-            self.sources.append(cols[13])
-            self.baselines.append(cols[14])
-            self.qcodes.append(cols[15])
-            self.pols.append(cols[17])
-            self.amplitudes.append(float(cols[19]))
-            self.snr.append(float(cols[20]))
-            self.sbd.append(float(cols[24]))
-            self.mbd.append(float(cols[25]))
-            self.delay_rate.append(float(cols[27]))
-            # snr, sbd, mdb, delay_rate
+            self.records['source'].append(cols[13])
+            self.records['baseline'].append(cols[14])
+            self.records['qcode'].append(cols[15])
+            self.records['pols'].append(cols[17])
+            self.records['amplitude'].append(float(cols[19]))
+            self.records['snr'].append(float(cols[20]))
+            self.records['sbd'].append(float(cols[24]))
+            self.records['mbd'].append(float(cols[25]))
+            self.records['delay_rate'].append(float(cols[27]))
 
-        
+
         # sanitize qcodes - if there's a letter, just save the letter
-        for ii in range(len(self.qcodes)):
-            if len(self.qcodes[ii])>1:
-                self.qcodes[ii]=self.qcodes[ii][1]
-                
-        self.unique_qcodes, self.qcode_counts = np.unique(self.qcodes, return_counts=True)
+        for ii in range(len(self.records['qcode'])):
+            if len(self.records['qcode'][ii])>1:
+                self.records['qcode'][ii]=self.records['qcode'][ii][1]
 
-        self.stations = [[xx[0],xx[1]] for xx in self.baselines]
+        # work out the number of each qcode
+        self.unique_qcodes, self.qcode_counts = np.unique(self.records['qcode'], return_counts=True)
 
-        self.unique_stations = np.unique(np.concatenate(self.stations).flat)
-
-
+        # work out the list of stations present in the data
+        self.records['stations'] = [[xx[0],xx[1]] for xx in self.records['baseline']]
+        self.unique_stations = np.unique(np.concatenate(self.records['stations']).flat)
 
 
 
@@ -88,24 +108,16 @@ class ParseAlist:
 
         # baselines, stations, sources, polarizations, qcodes, snrrange
 
-        baseline_idx = [i for i,x in enumerate(self.baselines) if x in data_selection_dict['baselines']]
+        baseline_idx = [i for i,x in enumerate(self.records['baseline']) if x in data_selection_dict['baselines']]
 
         # stations have to be handled backwards...
         station_idx = []
         for station in data_selection_dict['stations']:
-            station_idx.extend([i for i,x in enumerate(self.stations) if station in x])
+            station_idx.extend([i for i,x in enumerate(self.records['stations']) if station in x])
 
         #print(np.array(self.baselines)[baseline_idx])
         #print(np.array(self.stations)[station_idx])
         
         return list(set(baseline_idx) & set(station_idx))
 
-
-        
-    # find the indices of the rows with data from the baseline we want
-    #idx = np.where(np.array(base, copy=False) == baseline)[0]
-
-    # convert lists to arrays for indexing
-    #amplitudes = np.array(amp)[idx]
-    #times = np.array(t)[idx]
 