c429a1ad050d423f9107b6e5a8954eb234e8fc33
John Barrett <barrettj@mit.edu>
2021-10-20 14:32:35 -0400

    nuke K&R c func defs in compress

--- source/c_src/dfio/include/bytflp.h
+++ source/c_src/dfio/include/bytflp.h
@@ -9,6 +9,7 @@
 #define cp_short(a,b)  {a = b; short_reverse (&(a));}
 #define cp_unsig(a,b)  {a = b; unsig_reverse (&(a));}
 #define cp_int(a,b)    {a = b; int_reverse (&(a));}
+#define cp_uint(a,b)    {a = b; uint_reverse (&(a));}
 #define cp_long(a,b)   {a = b; long_reverse (&(a));}
 #define cp_float(a,b)  {a = b; float_reverse (&(a));}
 #define cp_double(a,b) {a = b; double_reverse (&(a));}
@@ -18,6 +19,7 @@
 #define cp_short(a,b)  a = b
 #define cp_unsig(a,b)  a = b
 #define cp_int(a,b)    a = b
+#define cp_uint(a,b)    a = b
 #define cp_long(a,b)   a = b
 #define cp_float(a,b)  a = b
 #define cp_double(a,b) a = b
