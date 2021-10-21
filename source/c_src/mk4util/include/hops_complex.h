3ec0822d68cb7120c300d70401fba1134a2184a8
John Barrett <barrettj@mit.edu>
2021-10-21 12:27:20 -0400

    tweak hops_complex.h header to deal with 3 possible cases when combining c/c++

--- source/c_src/mk4util/include/hops_complex.h
+++ source/c_src/mk4util/include/hops_complex.h
@@ -1,16 +1,41 @@
 #ifndef HOPS_COMPLEX_WRAPPER__
 #define HOPS_COMPLEX_WRAPPER__
 
-#ifndef __cplusplus
-#define USE_C_COMPLEX
+#ifndef HOPS3_USE_CXX
+    #define USE_C_COMPLEX
+    #ifdef __cplusplus
+        #define USE_CXX_DUMMY
+    #endif
 #endif
+
+#ifdef USE_CXX_DUMMY
+extern "C"
+{
+#endif
+
+typedef struct hops_complex_tag	/* needed in type_230 */
+{
+   double real;
+   double imag;
+}
+hops_scomplex;
 
 #ifdef USE_C_COMPLEX
     //using c definition of complex 
-    #include <complex.h>
-    #if defined(_Complex_I) && defined(complex) && defined(I)
-        typedef double _Complex hops_complex_impl;
-        extern const hops_complex_impl cmplx_unit_I;
+    #ifndef USE_CXX_DUMMY //pure c case
+        #include <complex.h>
+        #if defined(_Complex_I) && defined(complex) && defined(I)
+            typedef double _Complex hops_complex_impl;
+            extern const hops_complex_impl cmplx_unit_I;
+        #endif
+    #else //c++ lib is linking against this c-library 
+        typedef union hops_complex_impl
+        {
+            struct hops_complex_tag named; 
+            double array[2];
+        }
+        hops_complex_impl;
+    extern const hops_complex_impl cmplx_unit_I; 
     #endif
 #else 
     //using c++ definition of complex
@@ -22,13 +47,6 @@
 //alias to the implementation
 #define hops_complex hops_complex_impl
 
-typedef struct hops_complex_tag	/* needed in type_230 */
-{
-   double real;
-   double imag;
-}
-hops_scomplex;
-
 extern void zero_complex(hops_complex* val);
 extern void set_complex(hops_complex* val, double real, double imag);
 extern double abs_complex(hops_complex val);
@@ -37,5 +55,10 @@ extern double real_comp(hops_complex val);
 extern double imag_comp(hops_complex val);
 extern hops_complex exp_complex(hops_complex val);
 extern hops_complex conjugate(hops_complex val);
+
+#ifdef USE_CXX_DUMMY
+}
+#endif
+
 
 #endif /* end of include guard: HOPS_COMPLEX_WRAPPER */
