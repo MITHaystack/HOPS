77c4f6bcc547637554381d95888293633ca9ea78
John Barrett <barrettj@mit.edu>
2022-01-27 10:36:00 -0500

    adding another possible option for Dan and Violet

--- source/cpp_src/Containers/include/MHO_ChannelizedVisibilities.hh
+++ source/cpp_src/Containers/include/MHO_ChannelizedVisibilities.hh
@@ -36,6 +36,13 @@ using ch_baseline_data_type = MHO_TableContainer< visibility_type, ch_baseline_a
 using ch_baseline_weight_type = MHO_TableContainer< weight_type, ch_baseline_axis_pack >;
 using ch_baseline_sbd_type = MHO_TableContainer< visibility_type, ch_baseline_axis_pack >;
 
+
+//other auxilliary data types (perhaps we should have a 'MHO_ChannelizedQuantities' wrapper header for all of this )
+//this is for per-channel manual pcal
+using ch_pcal_axis_pack = MHO_AxisPack< ch_polprod_axis_type, ch_channel_axis_type>;
+using ch_pcal_phase_type = MHO_TableContainer< double, ch_pcal_axis_pack >;  //(here we are storing pcal as a rotation in 'degrees', should/could we use phasor instead?)
+
+
 }//end of hops namespaces
 
 #endif /* end of include guard: MHO_ChannelizedVisibilities */
