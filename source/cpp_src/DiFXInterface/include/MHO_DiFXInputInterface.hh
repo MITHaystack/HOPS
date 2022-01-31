922369c08f7c3262010243bffe2aaddfaf7150ad
John Barrett <barrettj@mit.edu>
2022-01-31 15:01:53 -0500

    some reorg

--- source/cpp_src/DiFXInterface/include/MHO_DiFXInputInterface.hh
+++ source/cpp_src/DiFXInterface/include/MHO_DiFXInputInterface.hh
@@ -10,6 +10,26 @@
 *@brief:
 */
 
+#include <string>
+
+#include "difxio/difx_input.h"
+#include "difxio/parsevis.h"
+
+#include "MHO_Message.hh"
+#include "MHO_Tokenizer.hh"
+
+#include "MHO_Visibilities.hh"
+#include "MHO_ChannelizedVisibilities.hh"
+#include "MHO_VisibilityChannelizer.hh"
+#include "MHO_WeightChannelizer.hh"
+#include "MHO_StationCoordinates.hh"
+
+#include "MHO_BinaryFileStreamer.hh"
+#include "MHO_BinaryFileInterface.hh"
+#include "MHO_ClassIdentityMap.hh"
+
+#include "MHO_DirectoryInterface.hh"
+
 namespace hops
 {
 
@@ -18,7 +38,19 @@ class MHO_DiFXInputInterface
     public:
         MHO_DiFXInputInterface();
         virtual ~MHO_DiFXInputInterface();
+
+        void SetInputDirectory(std::string dir);
+        void SetOutputDirectory(std::string dir);
+        void Initialize();
+
+
     private:
+
+        std::string fInputDirectory;
+        std::string fOutputDirectory;
+
+        MHO_DirectoryInterface fDirInterface;
+
 };
 
 }//end of namespace
--- source/cpp_src/DiFXInterface/src/MHO_DiFXInputInterface.cc
+++ source/cpp_src/DiFXInterface/src/MHO_DiFXInputInterface.cc
@@ -3,8 +3,74 @@
 namespace hops
 {
 
-MHO_DiFXInputInterface::MHO_DiFXInputInterface(){};
+MHO_DiFXInputInterface::MHO_DiFXInputInterface():
+    fInputDirectory(""),
+    fOutputDirectory("")
+{};
 
 MHO_DiFXInputInterface::~MHO_DiFXInputInterface(){};
+
+void
+MHO_DiFXInputInterface::SetInputDirectory(std::string dir)
+{
+    fInputDirectory = fDirInterface.GetDirectoryFullPath(dir);
+}
+
+void
+MHO_DiFXInputInterface::SetOutputDirectory(std::string dir)
+{
+    fOutputDirectory = fDirInterface.GetDirectoryFullPath(dir);
+}
+
+void 
+MHO_DiFXInputInterface::Initialize()
+{
+        //directory interface
+        MHO_DirectoryInterface fDirInterface;
+        bool in_ok = fDirInterface.DoesDirectoryExist(fInputDirectory);
+        bool out_ok = fDirInterface.DoesDirectoryExist(fOutputDirectory);
+
+        msg_info("difx_interface", "input directory: " << fInputDirectory << eom);
+        msg_info("difx_interface", "output directory: " << fOutputDirectory << eom);
+
+        //get list of all the files (and directories) in directory
+        std::vector< std::string > allFiles;
+        std::vector< std::string > allSubDirs;
+
+        fDirInterface.SetCurrentDirectory(fInputDirectory);
+        fDirInterface.ReadCurrentDirectory();
+        fDirInterface.GetFileList(allFiles);
+        fDirInterface.GetSubDirectoryList(allSubDirs);
+
+        //debug
+        for(auto it=allFiles.begin(); it != allFiles.end(); it++)
+        {
+            std::cout<<"file: "<<*it<<std::endl;
+        }
+
+        //debug
+        for(auto it=allSubDirs.begin(); it != allSubDirs.end(); it++)
+        {
+            std::cout<<"dir: "<<*it<<std::endl;
+        }
+
+        //grab all of the .difx directories 
+        std::vector< std::string > difxDirs;
+        fDirInterface.GetSubDirectoriesMatchingExtention(difxDirs, "difx");
+        for(auto it=difxDirs.begin(); it != difxDirs.end(); it++)
+        {
+            std::cout<<"difx sub-dir: "<<*it<<std::endl;
+        }
+
+        // MHO_DirectoryInterface subfDirInterface;
+        // //loop over the difx sub-directories 
+        // for(auto it=difxDirs.begin(); it != difxDirs.end(); it++)
+        // {
+        //     //conver the difx data
+        // }
+
+    
+
+}
 
 }//end of namespace
