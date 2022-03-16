8cbb67552b9a73ed0e321746c332a8d8a9d9215d
John Barrett <barrettj@mit.edu>
2022-03-16 17:02:54 -0400

    some clean up for testing deps

---
+++ source/cpp_src/Plugins/Root/include/MHO_RootCanvasManager.hh
@@ -0,0 +1,49 @@
+#ifndef MHO_RootCanvasManager_HH__
+#define MHO_RootCanvasManager_HH__
+
+//ROOT includes
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TColor.h"
+#include "TMath.h"
+#include "TGraph.h"
+
+#include <vector>
+
+
+
+/*
+*@file: MHO_RootCanvasManager.hh
+*@class: MHO_RootCanvasManager
+*@author: J. Barrett
+*@email: barrettj@mit.edu
+*@date:
+*@brief:
+*/
+
+namespace hops 
+{
+
+class MHO_RootCanvasManager
+{
+    public:
+        MHO_RootCanvasManager();
+        virtual ~MHO_RootCanvasManager();
+
+        void CreateCanvas(std::string name, unsigned int width = 500, unsigned int height = 500);
+
+        TCanvas* SelectCanvasByName(std::string name);
+        TCanvas* GetCurrentCanvas(){return fCanvasList.back();}
+
+    private:
+
+        void ConfigureStyle();
+        void DestroyCanvasList();
+
+        TStyle* fStyle;
+        std::vector< TCanvas* > fCanvasList;
+};
+
+}//end of namespace
+
+#endif /* end of include guard: MHO_RootCanvasManager */
