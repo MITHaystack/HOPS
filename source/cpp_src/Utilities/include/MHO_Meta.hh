a9eb9195961654efbd24461ecd37b253326a2037
John Barrett <barrettj@mit.edu>
2022-03-23 20:12:30 -0400

    wip on multidim array slice view

--- source/cpp_src/Utilities/include/MHO_Meta.hh
+++ source/cpp_src/Utilities/include/MHO_Meta.hh
@@ -34,6 +34,44 @@ template< class... T > struct MHO_TypelistSizeImpl< MHO_Typelist< T... > >
 template< class L > using MHO_TypelistSize = typename MHO_TypelistSizeImpl<L>::type;
 
 
+//utility to return 1 if two types are the same, zero otherwise 
+
+template<class T, class U>
+struct is_same_count
+{
+    constexpr static size_t value = 0;
+};
+
+template<class T>
+struct is_same_count<T, T>
+{
+    constexpr static size_t value = 1;
+};
+
+//utility to count the instances of a particular type in a parameter pack //////
+template<typename XCheckType, size_t N, typename... T > struct count_instances_of_type;
+
+//terminating case is N=0
+template<typename XCheckType, typename... T > 
+struct count_instances_of_type<XCheckType, 0, T...>
+{
+    using current_type = typename std::tuple_element<0, std::tuple<T...> >::type;
+    constexpr static std::size_t value = is_same_count<XCheckType, current_type>::value;
+};
+
+//N = sizeof...(T) - 1
+template<typename XCheckType, size_t N, typename... T > 
+struct count_instances_of_type
+{
+    using current_type = typename std::tuple_element<N, std::tuple<T...> >::type;
+    constexpr static std::size_t value = is_same_count<XCheckType, current_type>::value + count_instances_of_type<XCheckType, N-1, T... >::value;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 //functions needed to stream tuples/////////////////////////////////////////////
 template<size_t N = 0, typename XStream, typename... T >
 typename std::enable_if< (N >= sizeof...(T)), XStream& >::type
@@ -172,6 +210,10 @@ apply_at2(XTupleType& tup1, XTupleType2& tup2, size_t index, XFunctorType functo
 }
 
 
-}
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+}//end of namespace
 
 #endif /* end of include guard: MHO_Meta */
