f18cab47126fc8d12eba52b408cd36b5ff52fc28
John Barrett <barrettj@mit.edu>
2022-04-13 00:15:07 -0400

    need to reconcile mk4 type station coords with difx --- why do we need a channel dependent phase spline? it doesn't appear to be used anywhere

--- source/cpp_src/Containers/include/MHO_StationContainers.hh
+++ source/cpp_src/Containers/include/MHO_StationContainers.hh
@@ -22,6 +22,9 @@ namespace hops
 using station_coord_axis_pack = MHO_AxisPack< coord_axis_type, channel_id_axis_type, interval_axis_type, coeff_axis_type>;
 using station_coord_type = MHO_TableContainer< spline_coeff_type, station_coord_axis_pack >;
 
+using station_coord_axis_pack2 = MHO_AxisPack< coord_axis_type, interval_axis_type, coeff_axis_type>;
+using station_coord_type2 = MHO_TableContainer< spline_coeff_type, station_coord_axis_pack2 >;
+
 //multi-tone pcal data
 using multitone_pcal_axis_type = MHO_AxisPack< pol_axis_type, time_axis_type, frequency_axis_type >;
 using multitone_pcal_type = MHO_TableContainer< pcal_phasor_type, multitone_pcal_axis_type >;
