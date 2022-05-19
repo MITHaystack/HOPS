48e745e4b156c63a0e7b8c648d236f289ad966b9
John Barrett <barrettj@mit.edu>
2022-05-19 16:32:33 -0400

    add a developer script to updated canned copies of external 'single-header' libraries

---
+++ extern/dev_update_submodules.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#simple script for developers to update the canned copy of the git submodules (date, json, pybind11)
+#must be run from the <hops-git>/extern directory
+#make sure to clone the submodules first!
+
+cd ./submodules/date
+git archive --format=tar.gz HEAD > ../../date.tar.gz
+cd ../../
+mkdir -p date 
+cd ./date
+tar -xzvf ../date.tar.gz 
+cd ../
+rm ./date.tar.gz
+
+cd ./submodules/pybind11
+git archive --format=tar.gz HEAD > ../../pybind11.tar.gz
+cd ../../
+mkdir -p ./pybind11
+cd ./pybind11
+tar -xzvf ../pybind11.tar.gz 
+cd ../
+rm ./pybind11.tar.gz
