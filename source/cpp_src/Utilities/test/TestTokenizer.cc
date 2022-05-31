0581c3e48d1c1fc5f344e68863faa9bc7870ad0e
John Barrett <barrettj@mit.edu>
2022-05-31 10:55:57 -0400

    testing the tokenizer

--- source/cpp_src/Utilities/test/TestTokenizer.cc
+++ source/cpp_src/Utilities/test/TestTokenizer.cc
@@ -12,13 +12,14 @@ int main(int /*argc*/, char** /*argv*/)
 {
 
     MHO_Tokenizer tokenizer;
+    tokenizer.SetIncludeEmptyTokensFalse();
 
     std::string test1 = "This is a string separated by spaces.";
     std::string test2 = "This|is|a|string|separated|by|pipes.";
     std::string test3 = "This \t is a\tstring|separated by a\nmix.";
     std::string test4 = "This<d>is<d>a<d>string<d>separated<d>by<d>a<d>multi-character<d>delimiter.";
 
-    std::string test5 = "This is a string that \"contains   quoted   text\". ";
+    std::string test5 = "This is a string that \"contains \t quoted \t text\". ";
 
     std::string delim1 = " ";
     std::string delim2 = "|";
@@ -30,6 +31,8 @@ int main(int /*argc*/, char** /*argv*/)
     std::vector<std::string> tok3;
     std::vector<std::string> tok4;
     std::vector<std::string> tok5;
+    std::vector<std::string> tok6;
+
 
     tokenizer.SetDelimiter(delim1);
     tokenizer.SetString(&test1);
@@ -49,9 +52,15 @@ int main(int /*argc*/, char** /*argv*/)
     tokenizer.GetTokens(&tok4);
 
     tokenizer.SetDelimiter(delim1);
+    tokenizer.SetUseMulticharacterDelimiterFalse();
     tokenizer.SetPreserveQuotesTrue();
     tokenizer.SetString(&test5);
     tokenizer.GetTokens(&tok5);
+
+    tokenizer.SetDelimiter(delim3);
+    tokenizer.SetPreserveQuotesTrue();
+    tokenizer.SetString(&test5);
+    tokenizer.GetTokens(&tok6);
 
     for(std::size_t i=0; i<tok1.size(); i++){std::cout<<tok1[i]<<" ";} 
     std::cout<<std::endl;
@@ -62,6 +71,8 @@ int main(int /*argc*/, char** /*argv*/)
     for(std::size_t i=0; i<tok4.size(); i++){std::cout<<tok4[i]<<" ";}
     std::cout<<std::endl;
     for(std::size_t i=0; i<tok5.size(); i++){std::cout<<tok5[i]<<" ";}
+    std::cout<<std::endl;
+    for(std::size_t i=0; i<tok6.size(); i++){std::cout<<tok6[i]<<" ";}
     std::cout<<std::endl;
 
     return 0;
