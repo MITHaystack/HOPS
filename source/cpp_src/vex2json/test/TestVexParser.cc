4cde638e15092683325849013870ae6840219a1e
John Barrett <barrettj@mit.edu>
2022-06-06 11:59:11 -0400

    fix bug in tokenizer which allowed empty strings to sneak through, add  block

--- source/cpp_src/vex2json/test/TestVexParser.cc
+++ source/cpp_src/vex2json/test/TestVexParser.cc
@@ -18,7 +18,13 @@ int main(int argc, char** argv)
     MHO_VexParser vparser;
     vparser.SetVexVersion("2.0");
     vparser.SetVexFile(vexfile);
-    vparser.ParseVex();
+    mho_json vex = vparser.ParseVex();
+
+    //open and dump to file 
+    std::string output_file("text_vex.json");
+    std::ofstream outFile(output_file.c_str(), std::ofstream::out);
+    outFile << vex;
+    outFile.close();
 
     return 0;
 }
