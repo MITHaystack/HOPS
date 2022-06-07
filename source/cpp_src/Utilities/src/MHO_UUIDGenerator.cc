5c1d2317770e04fb2fbcca8f2c36a6df38417c62
John Barrett <barrettj@mit.edu>
2022-06-07 13:55:29 -0400

    use full 128 bit width for randomly generated UUIDs

--- source/cpp_src/Utilities/src/MHO_UUIDGenerator.cc
+++ source/cpp_src/Utilities/src/MHO_UUIDGenerator.cc
@@ -21,18 +21,31 @@ MHO_UUIDGenerator::~MHO_UUIDGenerator()
 MHO_UUID MHO_UUIDGenerator::GenerateUUID()
 {
     MHO_UUID uuid;
+    // //generate the rest of the bits according to version-4
+    // //(128 bit) pseudo-random UUID based on RFC 4122
+    // for(unsigned int i=4; i<16; i++)
+    // {
+    //     //get a random 32 bit uint between 0 and 255
+    //     uint32_t tmp = (*fUniformDist)(*fRandomGen);
+    //     //set 4 MSB bits to 0100 for 7th byte
+    //     if(i == 6){tmp &= 0x0F; tmp |= 0x40;}
+    //     //set 2 MSB bits to 01 for 9th byte
+    //     if(i == 8){tmp &= 0x3F; tmp |= 0x80;}
+    //     uuid[i] = (uint8_t) tmp;
+    // }
     //generate the rest of the bits according to version-4
     //(128 bit) pseudo-random UUID based on RFC 4122
-    for(unsigned int i=4; i<16; i++)
+    for(unsigned int i=0; i<MHO_UUID_LENGTH; i++)
     {
         //get a random 32 bit uint between 0 and 255
         uint32_t tmp = (*fUniformDist)(*fRandomGen);
-        //set 4 MSB bits to 0100 for 7th byte
-        if(i == 6){tmp &= 0x0F; tmp |= 0x40;}
-        //set 2 MSB bits to 01 for 9th byte
-        if(i == 8){tmp &= 0x3F; tmp |= 0x80;}
+        // //set 4 MSB bits to 0100 for 7th byte
+        // if(i == 6){tmp &= 0x0F; tmp |= 0x40;}
+        // //set 2 MSB bits to 01 for 9th byte
+        // if(i == 8){tmp &= 0x3F; tmp |= 0x80;}
         uuid[i] = (uint8_t) tmp;
     }
+
     return uuid;
 }
 
