f070c55e8cd8106ab2988bb04e4b531538977aed
Violet Pfeiffer <violetp@mit.edu>
2022-06-28 12:09:56 -0400

    bugfix

--- source/cpp_src/MK4Interface/test/TestMK4Type203Convert.cc
+++ source/cpp_src/MK4Interface/test/TestMK4Type203Convert.cc
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
     //struct type_203 my203 = {"202", "000", "unu", {{2, 20.0, "a", "b", "c", 10.0, 20.0, "foo", "bar"}, 
     //                                                { 2, 20.0, "a", "b", "c", 10.0, 20.0, "foo", "bar"}...};
     for (int i = 0; i < 512; i++){
-        if (i == 2 || i = 511){
+        if (i == 2 || i == 511){
             // set to null    
             my203.channels[i].index = 0;
             my203.channels[i].sample_rate = 0;
