0153159bb781e02d0dd3ac549bf3af9d95f17ef9
Violet Pfeiffer <violetp@mit.edu>
2022-07-05 16:21:52 -0400

    add JSON conversion test

---
+++ source/cpp_src/MK4Interface/test/TestMK4Type210Convert.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "MHO_MK4Type210Converter.hh"
+
+const int AMPPHASE = 64;
+
+using namespace hops;
+
+int main(int argc, char** argv) {
+    struct type_210 my210;
+
+    // create and fill in a type_210 struct with some dummy data 
+    strcpy(my210.record_id, "202"); 
+    strcpy(my210.version_no, "2"); 
+    strcpy(my210.unused1, "unused..."); 
+    for (int i = 0; i < AMPPHASE; i++){
+      my210.amp_phas[i].ampl = 22.22;
+      my210.amp_phas[i].phase = 22.22;
+    }
+
+    json obj = convertToJSON(my210);
+    std::cout << obj.dump(2) << std::endl;
+
+    return 0;
+}
