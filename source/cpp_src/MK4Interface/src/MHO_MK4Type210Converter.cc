467606e8cdd9f7016fb05b01c8fcd476f529ce17
Violet Pfeiffer <violetp@mit.edu>
2022-07-05 16:20:55 -0400

    add JSON conversion function

---
+++ source/cpp_src/MK4Interface/src/MHO_MK4Type210Converter.cc
@@ -0,0 +1,46 @@
+#include "MHO_MK4Type210Converter.hh"
+#include <iostream>
+
+const int AMPPHASE = 64;
+
+//struct polars
+//    {
+//    float               ampl;                   /* Correlation coefficient */
+//    float               phase;                  /* Degrees */
+//    };
+//
+//struct type_210 
+//    {
+//    char                record_id[3];           /* Standard 3-digit id */
+//    char                version_no[2];          /* Standard 2-digit version # */
+//    char                unused1[3];             /* Reserved space */
+//    struct polars       amp_phas[64];           /* Residual fringe amp/phase */
+//    };
+
+namespace hops {
+
+  json polarsToJSON(const polars &t){
+    return {
+      {"ampl", t.ampl},
+      {"phase", t.phase}
+    };
+  }
+
+  json ampPhaseArrayToJSON(const polars t[AMPPHASE]) { 
+    int i;
+    json JSONArray[AMPPHASE];
+
+    for (i = 0; i < AMPPHASE; i++){
+      JSONArray[i] = polarsToJSON(t[i]);
+    }
+    return JSONArray;
+  }
+
+  json convertToJSON(const type_210 &t) {
+    return {{"record_id", std::string(t.record_id, 3).c_str()},
+            {"version_no", std::string(t.version_no, 2).c_str()},
+            {"unused1", std::string(t.unused1, 3).c_str()},
+            {"amp_phase", ampPhaseArrayToJSON(t.amp_phas)}};
+  }
+
+} // namespace hops
