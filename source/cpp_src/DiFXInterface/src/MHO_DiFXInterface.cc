494f780e223ec3535c72eadaced0e6e2f76c2ea1
John Barrett <barrettj@mit.edu>
2022-07-07 10:03:23 -0400

    add insertion of mk4 (single char) site ids in ovex, along with experiment number

--- source/cpp_src/DiFXInterface/src/MHO_DiFXInterface.cc
+++ source/cpp_src/DiFXInterface/src/MHO_DiFXInterface.cc
@@ -225,6 +225,7 @@ MHO_DiFXInterface::ProcessScans()
 
     for(std::size_t i=0; i<fScanFileSetList.size(); i++)
     {
+        fScanProcessor.SetExperimentNumber(fExperNum);
         fScanProcessor.SetRootCode(scan_codes[i]);
         fScanProcessor.ProcessScan(fScanFileSetList[i]);
     }
