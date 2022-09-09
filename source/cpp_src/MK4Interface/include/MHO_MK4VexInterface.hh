465b615eb3fed115d5956e9e36e5427a1a50d70a
John Barrett <barrettj@mit.edu>
2022-09-09 15:36:55 -0400

    refreshing my memory on hops4

--- source/cpp_src/MK4Interface/include/MHO_MK4VexInterface.hh
+++ source/cpp_src/MK4Interface/include/MHO_MK4VexInterface.hh
@@ -11,18 +11,6 @@
 */
 
 
-
-// #ifndef HOPS3_USE_CXX
-// extern "C"
-// {
-// #endif
-//     #include "mk4_data.h"
-//     #include "mk4_dfio.h"
-//     #include "mk4_vex.h"
-// #ifndef HOPS3_USE_CXX
-// }
-// #endif
-
 #include <iostream>
 #include <string>
 
@@ -40,25 +28,15 @@ class MHO_MK4VexInterface
         virtual ~MHO_MK4VexInterface();
 
         void OpenVexFile(std::string file_path);
-
-        // //optional point the vex interface to an external struct
-        // //to where the data will be read into
-        // void SetVex(struct vex* root);
-
-        // struct vex* GetVex();
         mho_json GetVex();
 
         bool ExportVexFileToJSON(json& json_obj);
 
     private:
-        // 
-        bool fHaveVex;
-        // bool fOwnVex;
 
-        
+        bool fHaveVex;
         mho_json fVex;
 
-        //struct vex* fVex;
 
 
 };
