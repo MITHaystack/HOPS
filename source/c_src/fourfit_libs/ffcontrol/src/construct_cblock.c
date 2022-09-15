fc38b100d063ccdfa959a100f511f04f08a1e4aa
John Barrett <barrettj@mit.edu>
2022-09-15 10:35:01 -0400

    non re-entrant code with global variables will be first against the wall

--- source/c_src/fourfit_libs/ffcontrol/src/construct_cblock.c
+++ source/c_src/fourfit_libs/ffcontrol/src/construct_cblock.c
@@ -5,7 +5,8 @@
 
 //same basic fuctionality as generate_cblock but without dependence on pass/param struct
 int construct_cblock (char* filename, 
-                      struct c_block* cb_out, 
+                      struct c_block* cb_head,
+                      struct c_block* cb_out,
                       char baseline[2], 
                       char source[31],
                       char fgroup,
@@ -14,28 +15,28 @@ int construct_cblock (char* filename,
     //init control record buffers
     char* control_file_buff = NULL;
     char* set_string_buff = NULL;
-    struct c_block* cb_hd = NULL;
-    cb_hd = (struct c_block *) malloc (sizeof (struct c_block) );
-    nullify_cblock (cb_hd);
-    struct c_block* cb_ptr = cb_hd;
-
-    parse_control_file(filename, &control_file_buff, &set_string_buff);
-    cb_ptr = cb_hd;
+    nullify_cblock (cb_head);
+    struct c_block* cb_ptr = cb_head;
 
     nullify_cblock( cb_out );
     default_cblock( cb_out );
 
-    for (cb_ptr=cb_hd; cb_ptr!=NULL; cb_ptr=cb_ptr->cb_chain)
+    parse_control_file(filename, &control_file_buff, &set_string_buff);
+    cb_ptr = cb_head;
+
+    for (cb_ptr=cb_head; cb_ptr!=NULL; cb_ptr=cb_ptr->cb_chain)
     {
         if(criteria_match (cb_ptr, baseline, source, fgroup, time))
         {
+            printf("matched \n");
+            printf("cb_ptr baseline: %c%c", cb_ptr->baseline[0], cb_ptr->baseline[1]);
+            printf("ref_freq: %f", cb_ptr->ref_freq);
             copy_cblock_parts(cb_ptr,cb_out);
         }
     }
 
     if(control_file_buff != NULL){free(control_file_buff);};
     if(set_string_buff != NULL){free(set_string_buff);};
-    if(cb_hd != NULL){free(cb_hd);};
 
     return 0;
 }
