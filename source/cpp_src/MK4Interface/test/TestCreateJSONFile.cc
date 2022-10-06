cb1d2cc03c982051b00424541013dc67edbe9d78
Violet Pfeiffer <violetp@mit.edu>
2022-10-06 16:59:24 -0400

    Add boiler plate

---
+++ source/cpp_src/MK4Interface/test/TestCreateJSONFile.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+//#include "convertToJSON.hh"
+
+using namespace hops;
+
+int main(int argc, char **argv) {
+
+  // create and fill in a type_200 struct with some dummy data
+  struct type_200s my200s;
+
+  // open file
+  
+  // call type 200s functions on type 200 structs from that file
+
+  // store in my200s
+
+  // dump to file
+  std::cout << obj.dump(2) << std::endl;
+
+  return 0;
+}
