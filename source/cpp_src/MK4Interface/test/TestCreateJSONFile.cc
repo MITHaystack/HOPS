8b0e32f91d00db536a2372bf450abd11a11add27
Violet Pfeiffer <violetp@mit.edu>
2022-10-18 17:12:59 -0400

    Read fringe file

--- source/cpp_src/MK4Interface/test/TestCreateJSONFile.cc
+++ source/cpp_src/MK4Interface/test/TestCreateJSONFile.cc
@@ -1,25 +1,42 @@
 #include <iostream>
 #include <string>
-
-//#include "convertToJSON.hh"
+#include "MHO_MK4Type200Converter.hh"
+#include "MHO_MK4Type201Converter.hh"
+#include "MHO_MK4Type202Converter.hh"
+#include "MHO_MK4Type203Converter.hh"
+#include "MHO_MK4Type204Converter.hh"
+#include "MHO_MK4Type205Converter.hh"
+#include "MHO_MK4Type206Converter.hh"
+#include "MHO_MK4Type207Converter.hh"
+#include "MHO_MK4Type208Converter.hh"
+#include "MHO_MK4Type210Converter.hh"
+#include "MHO_MK4Type212Converter.hh"
 
 using namespace hops;
 
 int main(int argc, char **argv) {
 
-  // create and fill in a type_200 struct with some dummy data
-  struct type_200s my200s;
+  json my200s;
+  // struct type_200 my200;
 
-  // open file
-  
-  // call type 200s functions on type 200 structs from that file
+  // Open file
+  std::string fringeFile = filename;
+  // Call interface to read fringe file
+  MHO_MK4FringeInterface* fringeReader = new MHO_MK4FringeInterface();
+  // Dump it in to fFringe field of that interface
+  fringeReader.ReadFringeFile(&filename);
+  mk4_fringe fringeReader.fFringe
 
-  // store in my200s
 
-  // dump to file
-  std::ofstream outputFile(type200-dump.js);
-  outputFile << std::setw(4) << my200s << std::endl;
-  outputFile.close()
+  // Call type 200s functions on type 200 structs from that file
+  //json obj = convertToJSON(my200);
+
+  // Store in my200s
+
+  // Dump to file
+  //std::ofstream outputFile(type200-dump.js);
+  //outputFile << std::setw(4) << my200s << std::endl;
+  //outputFile.close()
 
   return 0;
 }
