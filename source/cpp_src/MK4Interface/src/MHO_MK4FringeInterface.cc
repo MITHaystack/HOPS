34962cf493b00590eb52b8f126710d8cd61eef83
Violet Pfeiffer <violetp@mit.edu>
2022-11-30 17:15:10 -0500

    Convert all type 203 data to structs

--- source/cpp_src/MK4Interface/src/MHO_MK4FringeInterface.cc
+++ source/cpp_src/MK4Interface/src/MHO_MK4FringeInterface.cc
@@ -98,17 +98,53 @@ MHO_MK4FringeInterface::ExportFringeFile()
         _m.Insert( std::string("type201.coord.dec_mins"), fFringe.t201.coord->dec_mins);
         _m.Insert( std::string("type201.coord.dec_secs"), fFringe.t201.coord->dec_secs);
         _m.Insert( std::string("type201.epoch"), fFringe.t201->epoch );
-        _m.Insert( std::string("type201.unused2"), std::string(fFringe.t201->unused2) );
-        _m.Insert( std::string("type200.coord_date.year"), fFringe.t200.coord_date->year);
-        _m.Insert( std::string("type200.coord_date.day"), fFringe.t200.coord_date->day);
-        _m.Insert( std::string("type200.coord_date.hour"), fFringe.t200.coord_date->hour);
-        _m.Insert( std::string("type200.coord_date.minute"), fFringe.t200.coord_date->minute);
-        _m.Insert( std::string("type200.coord_date.second"), fFringe.t200.coord_date->second);
+        _m.Insert( std::string("type201.unused2"), std::string(fFringe.t201->unused2));
+        _m.Insert( std::string("type201.coord_date.year"), fFringe.t201.coord_date->year);
+        _m.Insert( std::string("type201.coord_date.day"), fFringe.t201.coord_date->day);
+        _m.Insert( std::string("type201.coord_date.hour"), fFringe.t201.coord_date->hour);
+        _m.Insert( std::string("type201.coord_date.minute"), fFringe.t201.coord_date->minute);
+        _m.Insert( std::string("type201.coord_date.second"), fFringe.t201.coord_date->second);
         _m.Insert( std::string("type201.ra_rate"), fFringe.t201->ra_rate );
         _m.Insert( std::string("type201.dec_rate"), fFringe.t201->dec_rate );
         _m.Insert( std::string("type201.pulsar_phase"), create_and_fill_array<double, 4>(fFringe.t201->pulsar_phase) );
         _m.Insert( std::string("type201.pulsar_epoch"), fFringe.t201->pulsar_epoch );
         _m.Insert( std::string("type201.dispersion"), fFringe.t201->dispersion );
+
+        // type_202 data
+        _m.Insert( std::string("type202.record_id"), std::string(fFringe.t202->record_id));
+        _m.Insert( std::string("type202.version_no"), std::string(fFringe.t202->version_no));
+        _m.Insert( std::string("type202.unused1"), std::string(fFringe.t202->unused1));
+        _m.Insert( std::string("type202.baseline"), std::string(fFringe.t202->baseline));
+        _m.Insert( std::string("type202.ref_intl_id"), std::string(fFringe.t202->ref_intl_id));
+        _m.Insert( std::string("type202.rem_intl_id"), std::string(fFringe.t202->rem_intl_id));
+        _m.Insert( std::string("type202.ref_name"), std::string(fFringe.t202->ref_name));
+        _m.Insert( std::string("type202.rem_name"), std::string(fFringe.t202->rem_name));
+        _m.Insert( std::string("type202.ref_tape"), std::string(fFringe.t202->ref_tape));
+        _m.Insert( std::string("type202.rem_tape"), std::string(fFringe.t202->rem_tape));
+        _m.Insert( std::string("type202.nlags"), fFringe.t202->nlags);
+        _m.Insert( std::string("type202.ref_xpos"), fFringe.t202->ref_xpos);
+        _m.Insert( std::string("type202.rem_xpos"), fFringe.t202->rem_xpos);
+        _m.Insert( std::string("type202.rem_ypos"), fFringe.t202->rem_ypos);
+        _m.Insert( std::string("type202.ref_ypos"), fFringe.t202->ref_ypos);
+        _m.Insert( std::string("type202.ref_zpos"), fFringe.t202->ref_zpos);
+        _m.Insert( std::string("type202.rem_zpos"), fFringe.t202->rem_zpos);
+        _m.Insert( std::string("type202.u"), fFringe.t202->u);
+        _m.Insert( std::string("type202.v"), fFringe.t202->v);
+        _m.Insert( std::string("type202.uf"), fFringe.t202->uf);
+        _m.Insert( std::string("type202.vf"), fFringe.t202->vf);
+        _m.Insert( std::string("type202.ref_clock"), fFringe.t202->ref_clock);
+        _m.Insert( std::string("type202.rem_clock"), fFringe.t202->rem_clock);
+        _m.Insert( std::string("type202.ref_clockrate"), fFringe.t202->ref_clockrate);
+        _m.Insert( std::string("type202.rem_clockrate"), fFringe.t202->rem_clockrate);
+        _m.Insert( std::string("type202.ref_idelay"), fFringe.t202->ref_idelay);
+        _m.Insert( std::string("type202.rem_idelay"), fFringe.t202->rem_idelay);
+        _m.Insert( std::string("type202.ref_zdelay"), fFringe.t202->ref_zdelay);
+        _m.Insert( std::string("type202.rem_zdelay"), fFringe.t202->rem_zdelay);
+        _m.Insert( std::string("type202.ref_elev"), fFringe.t202->ref_elev);
+        _m.Insert( std::string("type202.rem_elev"), fFringe.t202->rem_elev);
+        _m.Insert( std::string("type202.ref_az"), fFringe.t202->ref_az);
+        _m.Insert( std::string("type202.rem_az"), fFringe.t202->rem_az);
+
 
 
     }
