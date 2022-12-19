b8042ebdb1a854dbd10999f94c4a3c068bd82e15
John Barrett <barrettj@mit.edu>
2022-12-19 16:33:05 -0500

    some prep work for fft tests

---
+++ source/cpp_src/Math/include/MHO_SingleToneSignal.hh
@@ -0,0 +1,32 @@
+#ifndef MHO_SingleToneSignal_HH__
+#define MHO_SingleToneSignal_HH__
+
+#include "MHO_SimulatedSignalGenerator.hh"
+
+namespace hops
+{
+
+class MHO_SingleToneSignal: public MHO_SimulatedSignalGenerator
+{
+    public:
+        MHO_SingleToneSignal();
+        ~MHO_SingleToneSignal();
+
+        void SetPhaseOffset(double phase_offset){fPhaseOffset = phase_offset;} //radians
+        void SetToneFrequency(double tone_freq){fToneFrequency = tone_freq;};
+
+        //implementation specific
+        virtual void Initialize(){};
+
+    protected:
+
+        virtual bool GenerateSample(const double& /*sample_time*/, double& sample) const override;
+
+        double fPhaseOffset;
+        double fToneFrequency;
+};
+
+
+}
+
+#endif /* end of include guard: MHO_SingleToneSignal_HH__ */
