4c2ba5eddae26dfcd8b1c6a9da64ccf016a86b8d
John Barrett <barrettj@mit.edu>
2023-01-30 12:08:35 -0500

    isolating control block construction to its own class

--- source/cpp_src/Applications/ffcontrol_interface/include/MHO_FourfitControlInterface.hh
+++ source/cpp_src/Applications/ffcontrol_interface/include/MHO_FourfitControlInterface.hh
@@ -1,16 +1,46 @@
 #ifndef MHO_FourfitControlInterface_HH__
 #define MHO_FourfitControlInterface_HH__
 
+#include "ffcontrol.h"
+
+#include <string>
+#include <vector>
+
+#include "MHO_Message.hh"
+
+
+
 namespace hops 
 {
 
 class MHO_FourfitControlInterface
 {
     public:
+
         MHO_FourfitControlInterface();
         virtual ~MHO_FourfitControlInterface();
 
+        void SetControlFile(std::string control_file){fControlFile = control_file;}
+
+        void SetBaseline(std::string baseline){fBaseline = baseline;};
+        void SetSourceName(std::string src_name){fSourceName = src_name;};
+        void SetFrequencyGroup(std::string freq_group){fFrequencyGroup = freq_group;};
+        void SetTime(int time_val){fTime = time_val;};
+
+        //returns true if sucessful, false if error
+        bool ConstructControlBlock();
+        c_block* GetControlBlock();
+
     private:
+
+        std::string fControlFile;
+        std::string fBaseline;
+        std::string fSourceName;
+        std::string fFrequencyGroup;
+        int fTime;
+
+        std::vector< c_block* > fAllocatedControlBlocks;
+
 };
 
 }
--- source/cpp_src/Applications/ffcontrol_interface/src/MHO_FourfitControlInterface.cc
+++ source/cpp_src/Applications/ffcontrol_interface/src/MHO_FourfitControlInterface.cc
@@ -1,10 +1,85 @@
 #include "MHO_FourfitControlInterface.hh"
 
+struct c_block* cb_head; //global extern kludge (due to c-library interface)
+
 namespace hops 
 {
 
-MHO_FourfitControlInterface::MHO_FourfitControlInterface(){};
+MHO_FourfitControlInterface::MHO_FourfitControlInterface()
+{
+    fControlFile = "";
+    fControlFile = "";
+    fBaseline = "";
+    fSourceName = " ";
+    fFrequencyGroup = "X";
+    int fTime = 0;
+    cb_head = (struct c_block *) malloc (sizeof (struct c_block) );
+    nullify_cblock(cb_head);
+    default_cblock(cb_head);
 
-MHO_FourfitControlInterface::~MHO_FourfitControlInterface(){};
+    fAllocatedControlBlocks.push_back(cb_head);
+};
+
+MHO_FourfitControlInterface::~MHO_FourfitControlInterface()
+{
+    for(std::size_t i=0; i<fAllocatedControlBlocks.size(); i++)
+    {
+        free(fAllocatedControlBlocks[i]);
+    }
+    fAllocatedControlBlocks.clear();
+};
+
+
+bool
+MHO_FourfitControlInterface::ConstructControlBlock()
+{
+    if( fControlFile != "" && 
+        fBaseline.size() == 2 &&
+        fFrequencyGroup.size() == 1)
+    {
+        //parse the control file
+        struct c_block* cb_out = (struct c_block *) malloc (sizeof (struct c_block) );
+        fAllocatedControlBlocks.push_back(cb_out);
+        nullify_cblock(cb_head);
+        default_cblock(cb_head);
+        nullify_cblock(cb_out);
+        default_cblock(cb_out);
+        char bl[2]; 
+        bl[0] = fBaseline[0]; 
+        bl[1] = fBaseline[1];
+
+        std::string src = fSourceName;
+        char fgroup = fFrequencyGroup[0];
+
+        msg_debug("ffcontrol_interface", "constructing control block from contol file: "<<fControlFile<< eom);
+        msg_debug("ffcontrol_interface", "using baseline: "<<fBaseline<<", fgroup: "<<fFrequencyGroup<<", "<<" source: "<<fSourceName<<", time: "<<fTime << eom);
+
+        int retval = construct_cblock(const_cast<char*>(fControlFile.c_str()), 
+                                      cb_head, cb_out, 
+                                      bl, const_cast<char*>(src.c_str()), 
+                                      fgroup, fTime);
+
+        if(retval == 0)
+        {
+            return true;
+        }
+        else 
+        {
+            msg_error("ffcontrol_interface", "control block contruction failed with error code: "<< retval << eom);
+        }
+    }
+    else 
+    {
+        msg_error("ffcontrol_interface", "cannot construct control block with no control file given." << eom);
+    }
+    return false;
+}
+
+c_block* 
+MHO_FourfitControlInterface::GetControlBlock()
+{
+    return fAllocatedControlBlocks.back();
+}
+
 
 }
