e70b86a3859407b66ad8eaa8f8fe1be80b14d999
John Barrett <barrettj@mit.edu>
2023-03-22 15:29:46 -0400

    adding another test to verify vex2json converter functionality

--- source/cpp_src/Applications/test/chk_convertvex2json.sh
+++ source/cpp_src/Applications/test/chk_convertvex2json.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+verb=false
+[ -n "$testverb" ] && verb=true
 
-#stub
+[ -d "$srcdir" ] || { echo srcdir not set; exit 1; }
+${HOPS_SETUP-'false'} || . $srcdir/chk_env.sh
+export DATADIR=`cd $srcdir/vt9105; pwd`
+
+RET_VAL=0
+EXP_DIR=$DATADIR
+VEX_FILE="$DATADIR/vt9105.vex"
+REF_JSON_FILE="$DATADIR/vt9105.vex.json"
+NEW_JSON_FILE="$DATADIR/tmp.json"
+cd $EXP_DIR
+
+#conver the vex file to a json file 
+ConvertVexToJSON -i $VEX_FILE -o $NEW_JSON_FILE
+
+#compare the json output to the reference 
+EQUIV=$(@PY_EXE@ @CMAKE_CURRENT_BINARY_DIR@/compjson.py $REF_JSON_FILE $NEW_JSON_FILE)
+echo "Test value is $EQUIV (same if =0)."
+
+exit $EQUIV
