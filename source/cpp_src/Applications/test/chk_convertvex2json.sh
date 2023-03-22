6db4a313876ced5bea8de0453b07982ac09be615
John Barrett <barrettj@mit.edu>
2023-03-22 17:17:22 -0400

    add test for hops2json obj conversion

--- source/cpp_src/Applications/test/chk_convertvex2json.sh
+++ source/cpp_src/Applications/test/chk_convertvex2json.sh
@@ -13,11 +13,13 @@ REF_JSON_FILE="$DATADIR/vt9105.vex.json"
 NEW_JSON_FILE="$DATADIR/tmp.json"
 cd $EXP_DIR
 
-#conver the vex file to a json file 
+#convert the vex file to a json file 
+echo "Running: ConvertVexToJSON -i $VEX_FILE -o $NEW_JSON_FILE"
 ConvertVexToJSON -i $VEX_FILE -o $NEW_JSON_FILE
 
 #compare the json output to the reference 
-EQUIV=$(@PY_EXE@ @CMAKE_CURRENT_BINARY_DIR@/compjson.py $REF_JSON_FILE $NEW_JSON_FILE)
+@PY_EXE@ @CMAKE_CURRENT_BINARY_DIR@/compjson.py $REF_JSON_FILE $NEW_JSON_FILE
+EQUIV=$?
 echo "Test value is $EQUIV (same if =0)."
 
 exit $EQUIV
