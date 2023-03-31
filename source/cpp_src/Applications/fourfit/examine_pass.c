c58932634d3cd747a31c04b8f4902e53a037c483
John Barrett <barrettj@mit.edu>
2023-03-31 15:55:42 -0400

    add function to dump sbd

--- source/cpp_src/Applications/fourfit/examine_pass.c
+++ source/cpp_src/Applications/fourfit/examine_pass.c
@@ -24,9 +24,7 @@ using namespace hops;
 
 void examine_pass(struct type_pass* pass, int pass_index)
 {
-    printf("EXAMINING PASS_STRUCT, nfreq = %d \n", pass->nfreq);
-
-    msg_info("nufourfit", "test message. " <<eom);
+    msg_info("nufourfit", "dumping vis data from pass: " << pass_index << eom);
 
     int npolprod = 4;
     int nchan = 0;
@@ -95,7 +93,7 @@ void examine_pass(struct type_pass* pass, int pass_index)
                 for(int n=0; n<nlags; n++)
                 {
                     std::get<FREQ_AXIS>(*bl_data)(n) = n; //not correct, should be scaled by freq interval
-                    auto lag_ptr = pass->pass_data[ch].data->apdata_ll[sb];
+                    auto lag_ptr = pass->pass_data[ch].data[0].apdata_ll[sb];
                     lag_ptr = nullptr;
                     if(pp == POL_LL){lag_ptr = pass->pass_data[ch].data[ap].apdata_ll[sb];}
                     if(pp == POL_RR){lag_ptr = pass->pass_data[ch].data[ap].apdata_rr[sb];}
@@ -145,5 +143,137 @@ void examine_pass(struct type_pass* pass, int pass_index)
 
     delete bl_data;
     delete wt_data;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void examine_pass_sbd(struct type_pass* pass, int pass_index)
+{
+    msg_info("nufourfit", "dumping sbd data from pass: " << pass_index << eom);
+
+    int npolprod = 4;
+    int nchan = 0;
+    int nap = 0;
+    int nlags = 0;
+
+    int sb = 1;// ONLY LSB RIGHT NOW
+
+    nchan = pass->nfreq;
+    nap = pass->num_ap;
+    int pol = pass->pol;
+
+    for(int ch=0; ch < nchan; ch++)
+    {
+        double chan_freq = pass->pass_data[ch].frequency;
+        std::cout<<"chan: "<<ch<<" freq = "<<chan_freq<<std::endl;
+        std::cout<<"polprod code = "<<pass->pol<<std::endl;
+        if(pol == 0)
+        {
+            int sband = pass->pass_data[ch].data->sband;
+
+            //for(int sb=0; sb<2; sb++)
+            {
+                if(pass->pass_data[ch].data->apdata_ll[sb] != NULL)
+                {
+                    std::cout<<"sband = "<<sb<<std::endl;
+                    std::cout<<"nlags = "<<pass->pass_data[ch].data->apdata_ll[sb]->nlags <<std::endl;
+                    nlags = std::max(nlags, (int) pass->pass_data[ch].data->apdata_ll[sb]->nlags);
+                }
+            }
+        }
+    }
+
+    //lets extract the pass data into a visibility container
+    visibility_type* sbd_data = new visibility_type();
+
+    sbd_data->Resize(npolprod, nchan, nap, nlags);
+    sbd_data->ZeroArray();
+
+    for(int pp=0; pp<npolprod; pp++)
+    {
+        std::string pp_label = "";
+        //pass->linpol; ///[2]; TODO use lin-pol indicators to get the correct pol prod label
+        if(pp == POL_LL){pp_label = "LL";}
+        if(pp == POL_RR){pp_label = "RR";}
+        if(pp == POL_LR){pp_label = "LR";}
+        if(pp == POL_RL){pp_label = "RL";}
+
+        std::get<POLPROD_AXIS>(*sbd_data)(pp) = pp_label;
+        for(int ch=0; ch<nchan; ch++)
+        {
+            //set sky freq on channel axis
+            double chan_freq = pass->pass_data[ch].frequency;
+            std::get<CHANNEL_AXIS>(*sbd_data)(ch) = chan_freq;
+            for(int ap=0; ap<nap; ap++)
+            {
+                std::get<TIME_AXIS>(*sbd_data)(ap) = ap; //not correct, should be scaled by ap_interval
+                auto sbd_ptr = pass->pass_data[ch].data[ap].sbdelay;
+                for(int n=0; n<nlags; n++) //TODO check the size of sbdelay (should be 2*nlags ?)
+                {
+                    std::get<FREQ_AXIS>(*sbd_data)(n) = n; //not correct, should be scaled by freq interval
+
+                    if( sbd_ptr != NULL)
+                    {
+
+                        double rcomp = real_comp(sbd_ptr[n]);
+                        double icomp = imag_comp(sbd_ptr[n]);
+                        std::complex<double> sbd(rcomp, icomp);
+                        (*sbd_data)(pp,ch,ap,n) = sbd;
+                    }
+                }
+            }
+        }
+    }
+
+    //dump bl_data into a file for later inspection
+    std::stringstream ss;
+    ss << "./pass_sbd_";
+    ss << pass_index;
+    ss << ".dump";
+
+    std::string output_file = ss.str();
+    MHO_BinaryFileInterface inter;
+    bool status = inter.OpenToWrite(output_file);
+    if(status)
+    {
+        uint32_t label = 0xFFFFFFFF; //someday make this mean something
+        inter.Write(*sbd_data, "sbd", label);
+    }
+    else
+    {
+        msg_error("file", "Error opening corel output file: " << output_file << eom);
+    }
+
+    inter.Close();
+    delete sbd_data;
 
 }
