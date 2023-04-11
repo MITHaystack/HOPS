00b8e98f460b81d9d08f54799a6a76912644d8ba
John Barrett <barrettj@mit.edu>
2023-04-11 10:10:04 -0400

    rename delayRate_v2 to delayRate and make simple fringe search test more strict

--- source/cpp_src/Calibration/include/MHO_DelayRate.hh
+++ source/cpp_src/Calibration/include/MHO_DelayRate.hh
@@ -16,7 +16,7 @@
 #include "MHO_TableContainer.hh"
 #include "MHO_ContainerDefinitions.hh"
 
-#include "MHO_UnaryOperator.hh"
+#include "MHO_BinaryOperator.hh"
 #include "MHO_CyclicRotator.hh"
 #include "MHO_SubSample.hh"
 
@@ -39,33 +39,44 @@ namespace hops
 {
 
 
-class MHO_DelayRate: public MHO_UnaryOperator< sbd_type >
+class MHO_DelayRate: public MHO_BinaryOperator<
+    visibility_type,
+    weight_type,
+    sbd_type >
 {
     public:
         MHO_DelayRate();
         virtual ~MHO_DelayRate();
-
+        
+        void SetReferenceFrequency(double ref_freq){fRefFreq = ref_freq;};
+        
     protected:
 
-        using XArgType = sbd_type;
+        using XArgType1 = visibility_type;
+        using XArgType2 = weight_type;
+        using XArgType3 = sbd_type;
 
-        virtual bool InitializeInPlace(XArgType* in) override {return false;};
-        virtual bool ExecuteInPlace(XArgType* in) override {return false;};
 
-        virtual bool InitializeOutOfPlace(const XArgType* in, XArgType* out) override;
-        virtual bool ExecuteOutOfPlace(const XArgType* in1, XArgType* out) override;
+
+        virtual bool InitializeImpl(const XArgType1* in1, const XArgType2* in2, XArgType3* out) override;
+        virtual bool ExecuteImpl(const XArgType1* in1, const XArgType2* in2, XArgType3* out) override;
 
     private:
 
         std::size_t fInDims[VIS_NDIM];
         std::size_t fOutDims[VIS_NDIM];
 
-        void ConditionallyResizeOutput(const std::size_t* dims, std::size_t size, XArgType* out);
+        void ConditionallyResizeOutput(const std::size_t* dims, std::size_t size, XArgType3* out);
 
         MHO_MultidimensionalPaddedFastFourierTransform< visibility_type > fPaddedFFTEngine;
 
         MHO_SubSample<sbd_type> fSubSampler;
         MHO_CyclicRotator<sbd_type> fCyclicRotator;
+        
+        sbd_type fWorkspace;
+        sbd_type fWorkspace2;
+        int fDRSPSize;
+        double fRefFreq;
 
         bool fInitialized;
 
