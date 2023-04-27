a5b994c3f69c208973260c22914d2275268e088b
John Barrett <barrettj@mit.edu>
2023-04-27 03:11:35 -0400

    need to fix optimize-closure and non-integral sbd contrib in vrot when computing fringe resphase

--- source/cpp_src/Math/src/MHO_FringeRotation.cc
+++ source/cpp_src/Math/src/MHO_FringeRotation.cc
@@ -5,7 +5,7 @@ namespace hops
 
 const std::complex<double> MHO_FringeRotation::fImagUnit = std::complex<double>(0.0, 1.0);
 
-std::complex<double> 
+std::complex<double>
 MHO_FringeRotation::vrot(double time_delta, double freq, double ref_freq, double dr, double mbd) const
 {
     return vrot_v1(time_delta, freq, ref_freq, dr, mbd);
@@ -13,7 +13,7 @@ MHO_FringeRotation::vrot(double time_delta, double freq, double ref_freq, double
 
 
 
-std::complex<double> 
+std::complex<double>
 MHO_FringeRotation::vrot_v1(double time_delta, double freq, double ref_freq, double dr, double mbd) const
 {
     double theta = freq * dr * time_delta;
@@ -21,6 +21,18 @@ MHO_FringeRotation::vrot_v1(double time_delta, double freq, double ref_freq, dou
     theta += mbd * (freq - ref_freq);
     theta *= (-2.0 * M_PI);             // convert to radians
     return std::exp(fImagUnit*theta);
+
+
+    #pragma message("fix optimize_closure and non-integral sbd phase resid")
+    //NEED TO ADD THIS WHEN CALCULATING THE FRINGE RESIDUAL PHASE
+    // if (pass->control.optimize_closure) // sacrifice mbd fit for less-noisy closure?
+    //     theta += 0.125 * mbd * sb / status.sbd_sep;
+    //
+    // else                                /* effect of non-integral sbd iff SSB
+    //                                      * correct phase to dc edge, based on sb delay */
+    //     theta += 0.125 * status.sbd_max * sb / status.sbd_sep;
+
+
 }
 
 
