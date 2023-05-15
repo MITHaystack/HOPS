05f2f6a35bc69cd684aedaf755c959adbfd9758b
John Barrett <barrettj@mit.edu>
2023-05-14 22:51:34 -0400

    wip parse control elements

--- source/cpp_src/Control2/include/MHO_ControlElementParser.hh
+++ source/cpp_src/Control2/include/MHO_ControlElementParser.hh
@@ -21,11 +21,11 @@
 #include "MHO_JSONHeaderWrapper.hh"
 #include "MHO_DirectoryInterface.hh"
 
-#include "MHO_VexLine.hh"
-#include "MHO_VexTokenProcessor.hh"
-#include "MHO_VexDefinitions.hh"
+#include "MHO_ControlElements.hh"
+#include "MHO_ControlTokenProcessor.hh"
 
-namespace hops 
+
+namespace hops
 {
 
 class MHO_ControlElementParser
@@ -36,58 +36,42 @@ class MHO_ControlElementParser
         virtual ~MHO_ControlElementParser();
 
         void SetFormatDirectory(std::string fdir){fFormatDirectory = fdir;}
-        mho_json ParseBlockLines(std::string block_name, const std::vector< MHO_VexLine >* block_lines);
+        void LoadElementFormats();
+        //mho_json ParseControlStatements(std::string Element_name, const std::vector< MHO_VexLine >* element_lines);
 
     private:
 
-        void LoadBlockFormat(std::string block_name);
-        std::string GetBlockFormatFileName(std::string block_name);
 
-        mho_json ParseBlock();
-        mho_json ParseGlobalBlock();
+        std::string GetElementFormatFileName(std::string element_name);
 
-        bool IsStartTag(const MHO_VexLine& line);
-        bool IsStopTag(const MHO_VexLine& line);
-        bool IsReferenceTag(const MHO_VexLine& line);
+        // mho_json ParseElement();
+        // mho_json ParseGlobalElement();
+        //
+        // bool IsStartTag(const MHO_VexLine& line);
+        // bool IsStopTag(const MHO_VexLine& line);
+        // bool IsReferenceTag(const MHO_VexLine& line);
+        //
+        // bool ProcessLine(const MHO_VexLine& line,
+        //                  std::stack< std::string >& path,
+        //                  mho_json* file_node,
+        //                  mho_json& format_node);
+        //
+        //
+        //
+        // mho_json ProcessTokens(const std::string& element_name, mho_json& format, std::vector< std::string >& tokens);
+        // mho_json ProcessCompound(const std::string& element_name, mho_json& format, std::vector< std::string >& tokens);
+        //
+        // bool MatchesType(const std::string& token, const std::string& type_name);
 
-        bool ProcessStartTag(const MHO_VexLine& line, 
-                             std::stack< std::string >& path,
-                             std::stack< mho_json* >& file_node,
-                             std::stack< mho_json >& format_node);
-
-        bool ProcessStopTag(const MHO_VexLine& line, 
-                              std::stack< std::string >& path,
-                              std::stack< mho_json* >& file_node,
-                              std::stack< mho_json >& format_node);
-
-        bool ProcessLine(const MHO_VexLine& line, 
-                         std::stack< std::string >& path,
-                         mho_json* file_node,
-                         mho_json& format_node);
-
-        bool ProcessReference(const MHO_VexLine& line, 
-                         std::stack< std::string >& path,
-                         mho_json* file_node,
-                         mho_json& format_node);
-
-
-        mho_json ProcessTokens(const std::string& element_name, mho_json& format, std::vector< std::string >& tokens);
-        mho_json ProcessCompound(const std::string& element_name, mho_json& format, std::vector< std::string >& tokens);
-
-        bool MatchesType(const std::string& token, const std::string& type_name);
-
-        bool fBlockFormatLoaded;
-        mho_json fBlockFormat;
-        std::string fBlockName;
+        bool fElementFormatsLoaded;
+        mho_json fElementFormats;
         std::string fFormatDirectory;
-        const std::vector< MHO_VexLine >* fBlockLines;
-        std::size_t fCurrentLineNumber;
-        
-        std::string fStartTag;
-        std::string fStopTag;
+        std::vector< std::string > fKeywordNames;
+        //MHO_DirectoryInterface fDirInterface;
+        // const std::vector< MHO_VexLine >* fElementLines;
 
         MHO_Tokenizer fTokenizer;
-        MHO_VexTokenProcessor fTokenProcessor;
+        MHO_ControlTokenProcessor fTokenProcessor;
 };
 
 
