71921ed3b83e9bb83b93fa5634ce833d76fa9740
John Barrett <barrettj@mit.edu>
2023-05-18 16:57:42 -0400

    minor fixes in json formt defs...add TODO/FIXME to make sure all types in common label map are portable

--- source/cpp_src/Utilities/include/MHO_MultiTypeMap.hh
+++ source/cpp_src/Utilities/include/MHO_MultiTypeMap.hh
@@ -16,6 +16,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <cstdint>
 
 #include "MHO_Meta.hh"
 
@@ -211,6 +212,7 @@ class MHO_MultiTypeMap< XKeyType, XValueType, XValueTypeS...>: public MHO_MultiT
 
 //TODO: Make sure this set of types is complete for data axis labelling needs
 //Consider what other types might be needed (float? short? dates?)
+#pragma message("TODO FIXME -- need to specify fixed sized types in MHO_MultiTypeMap from cstdint for portability.")
 typedef MHO_MultiTypeMap< std::string, char, bool, int, double, std::string > MHO_CommonLabelMap;
 
 }
