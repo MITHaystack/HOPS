2e61269905120fc7fb5f091e699c4902a91fa49d
John Barrett <barrettj@mit.edu>
2023-05-19 13:08:22 -0400

    more work on control condition parsing

--- source/cpp_src/Control/test/TestControlFileParser.cc
+++ source/cpp_src/Control/test/TestControlFileParser.cc
@@ -5,6 +5,8 @@
 #include "MHO_Message.hh"
 #include "MHO_ControlFileParser.hh"
 
+#include "MHO_ControlConditionEvaluator.hh"
+
 using namespace hops;
 
 int main(int argc, char** argv)
@@ -19,8 +21,26 @@ int main(int argc, char** argv)
     MHO_ControlFileParser cparser;
     cparser.SetControlFile(controlfile);
     auto control_statements = cparser.ParseControl();
-    
-    //open and dump to file 
+
+    //    std::cout << control_statements.dump(2) << std::endl;
+
+    MHO_ControlConditionEvaluator eval;
+    for(auto it = control_statements["conditions"].begin(); it != control_statements["conditions"].end(); it++)
+    {
+        //std::cout << it->dump(2) << std::endl;
+        if( it->find("statement_type") != it->end() )
+        {
+            if( !( (*it)["statement_type"].is_null() ) )
+            {
+                if( (*it)["statement_type"].get<std::string>() == "conditional" )
+                {
+                    bool b = eval.Evaluate( *it );
+                }
+            }
+        }
+    }
+
+    //open and dump to file
     std::string output_file("./control.json");
     std::ofstream outFile(output_file.c_str(), std::ofstream::out);
     outFile << control_statements;
