a1ac081c2353fb2e88535ca45e95301fa0aef159
John Barrett <barrettj@mit.edu>
2023-05-27 18:24:12 -0400

    versing for interval label, interval label tree, and object tags

--- source/cpp_src/Utilities/include/MHO_IntervalLabel.hh
+++ source/cpp_src/Utilities/include/MHO_IntervalLabel.hh
@@ -50,10 +50,6 @@ class MHO_IntervalLabel:
             return *this;
         }
 
-    private:
-
-        //none
-
     public: //MHO_Serializable interface
 
         virtual uint64_t GetSerializedSize() const override
@@ -120,154 +116,170 @@ class MHO_IntervalLabel:
 
         template<typename XStream> friend XStream& operator>>(XStream& s, MHO_IntervalLabel& aData)
         {
-            //first item to stream is always the version number
             MHO_ClassVersion vers;
             s >> vers;
-            //check if incoming data is equal to the current class version
-            if( vers != aData.GetVersion() )
+            switch(vers) 
             {
-                MHO_ClassIdentity::ClassVersionErrorMsg(aData, vers);
-                //Flag this as an unknown object version so we can skip over this data
-                MHO_ObjectStreamState<XStream>::SetUnknown(s);
-            }
-            else
-            {
-                //first need to stream the integer interval pair
-                std::size_t low, up;
-                s >> low;
-                s >> up;
-                aData.SetBounds(low, up);
-
-                //then for each type in the multi-type map, we need to stream in
-                //the size of the map and then each subsequent key-value pair
-                std::size_t n_elem;
-                s >> n_elem;
-                for(std::size_t i=0; i<n_elem; i++)
-                {
-                    std::string key;
-                    char val;
-                    s >> key;
-                    s >> val;
-                    aData.Insert(key, val);
-                }
-                n_elem = 0;
-
-                s >> n_elem;
-                for(std::size_t i=0; i<n_elem; i++)
-                {
-                    std::string key;
-                    bool val;
-                    s >> key;
-                    s >> val;
-                    aData.Insert(key, val);
-                }
-                n_elem = 0;
-
-                s >> n_elem;
-                for(std::size_t i=0; i<n_elem; i++)
-                {
-                    std::string key;
-                    int val;
-                    s >> key;
-                    s >> val;
-                    aData.Insert(key, val);
-                }
-                n_elem = 0;
-
-                s >> n_elem;
-                for(std::size_t i=0; i<n_elem; i++)
-                {
-                    std::string key;
-                    double val;
-                    s >> key;
-                    s >> val;
-                    aData.Insert(key, val);
-                }
-                n_elem = 0;
-
-                s >> n_elem;
-                for(std::size_t i=0; i<n_elem; i++)
-                {
-                    std::string key;
-                    std::string val;
-                    s >> key;
-                    s >> val;
-                    aData.Insert(key, val);
-                }
-                n_elem = 0;
+                case 0:
+                    aData.StreamInData_V0(s);
+                break;
+                default:
+                    MHO_ClassIdentity::ClassVersionErrorMsg(aData, vers);
+                    //Flag this as an unknown object version so we can skip over this data
+                    MHO_ObjectStreamState<XStream>::SetUnknown(s);
             }
             return s;
-        };
+        }
 
         template<typename XStream> friend XStream& operator<<(XStream& s, const MHO_IntervalLabel& aData)
         {
-            //first item to stream is always the version number
-            s << aData.GetVersion();
+            switch( aData.GetVersion() ) 
+            {
+                case 0:
+                    s << aData.GetVersion();
+                    aData.StreamOutData_V0(s);
+                break;
+                default:
+                    msg_error("containers", 
+                        "error, cannot stream out MHO_IntervalLabel object with unknown version: " 
+                        << aData.GetVersion() << eom );
+            }
+            return s;
+        }
 
+    private:
+
+        template<typename XStream> void StreamInData_V0(XStream& s)
+        {
             //first need to stream the integer interval pair
-            s << aData.GetLowerBound();
-            s << aData.GetUpperBound();
+            std::size_t low, up;
+            s >> low;
+            s >> up;
+            this->SetBounds(low, up);
+
+            //then for each type in the multi-type map, we need to stream in
+            //the size of the map and then each subsequent key-value pair
+            std::size_t n_elem;
+            s >> n_elem;
+            for(std::size_t i=0; i<n_elem; i++)
+            {
+                std::string key;
+                char val;
+                s >> key;
+                s >> val;
+                this->Insert(key, val);
+            }
+            n_elem = 0;
+
+            s >> n_elem;
+            for(std::size_t i=0; i<n_elem; i++)
+            {
+                std::string key;
+                bool val;
+                s >> key;
+                s >> val;
+                this->Insert(key, val);
+            }
+            n_elem = 0;
+
+            s >> n_elem;
+            for(std::size_t i=0; i<n_elem; i++)
+            {
+                std::string key;
+                int val;
+                s >> key;
+                s >> val;
+                this->Insert(key, val);
+            }
+            n_elem = 0;
+
+            s >> n_elem;
+            for(std::size_t i=0; i<n_elem; i++)
+            {
+                std::string key;
+                double val;
+                s >> key;
+                s >> val;
+                this->Insert(key, val);
+            }
+            n_elem = 0;
+
+            s >> n_elem;
+            for(std::size_t i=0; i<n_elem; i++)
+            {
+                std::string key;
+                std::string val;
+                s >> key;
+                s >> val;
+                this->Insert(key, val);
+            }
+            n_elem = 0;
+        };
+
+        template<typename XStream> void StreamOutData_V0(XStream& s) const
+        {
+            //first need to stream the integer interval pair
+            s << this->GetLowerBound();
+            s << this->GetUpperBound();
 
             //then for each type in the multi-type map, we need to stream out
             //the size of the map and then each subsequent key-value pair
 
             std::vector< std::string > keys;
-            keys = aData.DumpKeys< char >();
+            keys = this->DumpKeys< char >();
             s << keys.size();
             for(std::size_t i=0; i<keys.size(); i++)
             {
                 char val;
-                aData.Retrieve(keys[i], val);
+                this->Retrieve(keys[i], val);
                 s << keys[i];
                 s << val;
             }
             keys.clear();
 
-            keys = aData.DumpKeys<bool>();
+            keys = this->DumpKeys<bool>();
             s << keys.size();
             for(std::size_t i=0; i<keys.size(); i++)
             {
                 bool val;
-                aData.Retrieve(keys[i], val);
+                this->Retrieve(keys[i], val);
                 s << keys[i];
                 s << val;
             }
             keys.clear();
 
-            keys = aData.DumpKeys<int>();
+            keys = this->DumpKeys<int>();
             s << keys.size();
             for(std::size_t i=0; i<keys.size(); i++)
             {
                 int val;
-                aData.Retrieve(keys[i], val);
+                this->Retrieve(keys[i], val);
                 s << keys[i];
                 s << val;
             }
             keys.clear();
 
-            keys = aData.DumpKeys<double>();
+            keys = this->DumpKeys<double>();
             s << keys.size();
             for(std::size_t i=0; i<keys.size(); i++)
             {
                 double val;
-                aData.Retrieve(keys[i], val);
+                this->Retrieve(keys[i], val);
                 s << keys[i];
                 s << val;
             }
             keys.clear();
 
-            keys = aData.DumpKeys<std::string>();
+            keys = this->DumpKeys<std::string>();
             s << keys.size();
             for(std::size_t i=0; i<keys.size(); i++)
             {
                 std::string val;
-                aData.Retrieve(keys[i], val);
+                this->Retrieve(keys[i], val);
                 s << keys[i];
                 s << val;
             }
             keys.clear();
-
-            return s;
         };
 
 
