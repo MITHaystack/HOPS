0881b20c8492cbceee46fb9c42a62ef7e33e5693
John Barrett <barrettj@mit.edu>
2023-05-31 17:11:03 -0400

    wip op builders

---
+++ source/cpp_src/Builders/include/MHO_ManualChannelPhaseCorrectionBuilder.hh
@@ -0,0 +1,31 @@
+#ifndef MHO_ManualChannelPhaseCorrectionBuilderBuilder_HH__
+#define MHO_ManualChannelPhaseCorrectionBuilderBuilder_HH__
+
+/*
+*File: MHO_ManualChannelPhaseCorrectionBuilder.hh
+*Class: MHO_ManualChannelPhaseCorrectionBuilder
+*Author:
+*Email: 
+*Date:
+*Description:
+*/
+
+#include "MHO_OperatorBuilder.hh"
+
+namespace hops
+{
+
+class MHO_ManualChannelPhaseCorrectionBuilder: public MHO_OperatorBuilder
+{
+
+    MHO_ManualChannelPhaseCorrectionBuilder(): MHO_OperatorBuilder(){};
+    virtual ~MHO_ManualChannelPhaseCorrectionBuilder(){};
+
+    virtual bool Build() override;
+    
+};
+
+}//end namespace
+
+
+#endif /* end of include guard: MHO_ManualChannelPhaseCorrectionBuilderBuilder_HH__ */
