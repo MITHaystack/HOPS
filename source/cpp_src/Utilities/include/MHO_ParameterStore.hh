4591338bc3031fc0e4ed23e5bccd9afbf31d45c8
John Barrett <barrettj@mit.edu>
2023-06-08 10:36:46 -0400

    first impl of parameter store

--- source/cpp_src/Utilities/include/MHO_ParameterStore.hh
+++ source/cpp_src/Utilities/include/MHO_ParameterStore.hh
@@ -4,20 +4,29 @@
 #include <string>
 
 #include "MHO_JSONHeaderWrapper.hh"
+#include "MHO_Tokenizer.hh"
 
 namespace hops
 {
 
 //class to store parameters (typically from control file) for later retrieval
+//TODO -- evaluate if using json is appropriate, could also use MHO_MultiTypeMap 
 
 class MHO_ParameterStore
 {
     public:
-        MHO_ParameterStore(){};
+        
+        MHO_ParameterStore()
+        {
+            fTokenizer.SetDelimiter("/");
+            fTokenizer.SetRemoveLeadingTrailingWhitespaceTrue();
+            fTokenizer.SetIncludeEmptyTokensFalse();
+        };
+
         ~MHO_ParameterStore(){};
 
         template< typename XValueType>
-        void Set(const std::string& value_path, const XValueType& value);
+        bool Set(const std::string& value_path, const XValueType& value);
 
         template< typename XValueType>
         bool Get(const std::string& value_path, XValueType& value);
@@ -27,31 +36,67 @@ class MHO_ParameterStore
 
     private:
 
+        //helpers
+        MHO_Tokenizer fTokenizer;
+        std::vector< std::string > fPath;
+
         //stash data in a json object
         mho_json fStore;
 };
 
 template< typename XValueType>
-XValueType
+bool
 MHO_ParameterStore::Set(const std::string& value_path, const XValueType& value)
 {
-    // auto* p = &fStore;
-    // for(const auto& k : value_path)
-    // {
-    //     if( !p->is_object() ){ *p = json::object(); }
-    //     p = &(*p)[k];
-    // }
+  //first we tokenize the value path into a sequence of names 
+    fPath.clear();
+    fTokenizer.SetString(&value_path);
+    fTokenizer.GetTokens(&fPath);
 
+    bool ok = false;
+    if(fPath.size() > 0)
+    {
+        //insert and/or replace an object
+        auto* p = &fStore;
+        for(auto it = fPath.begin(); it != fPath.end(); it++)
+        {
+            if( *it != *(fPath.rbegin()) )
+            {
+                if( p->contains(*it) ) //item with path exists
+                {
+                    p = &(p->at(*it) ); //point to this item 
+                }
+                else 
+                {
+                    //item doesn't exist yet, so create an entry with this key (*it)
+                    p = &(*p)[ *it ]; 
+                }
+            }
+            else //we've arrived at the terminal point, so set this item to the value
+            {
+                p = &(*p)[ *it ];
+                *p = value;
+                ok = true;
+            }
+        }
+    }
+    return ok;
 }
 
-// template< typename XValueType>
-// XValueType
-// MHO_ParameterStore::GetAs(const std::string& value_path)
-// {
-//     mho_json::json_pointer jptr(value_path);
-//     XValueType value = fStore.at(jptr).get<XValueType>();
-//     return value;
-// }
+template< typename XValueType>
+bool
+MHO_ParameterStore::Get(const std::string& value_path, XValueType& value)
+{
+    //TODO sanitise the value_path string -- for example a trailing '/' will cause a crash
+    mho_json::json_pointer jptr(value_path);
+    auto item = fStore.at(jptr);
+    if( !item.empty() )
+    {
+        value = item.get<XValueType>();
+        return true;
+    }
+    return false;
+}
 
 
 
