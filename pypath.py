f1daf2ec67aff2ebe8462ee11cf8f43616396531
John Barrett <barrettj@mit.edu>
2023-07-25 16:36:32 -0400

    consolidate python configuration, add setuptools/pip install for pedantic new systems

--- pypath.py
+++
@@ -1,5 +0,0 @@
-#construct name for local site-packages directory
-import os
-import sys
-version_str = str(sys.version_info.major) + "." + str(sys.version_info.minor)
-print( os.path.join("lib", "python" + version_str, "site-packages") )
