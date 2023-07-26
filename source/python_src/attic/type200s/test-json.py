2e1013067f74f7007578c9f7b3e625b7a4f4132d
John Barrett <barrettj@mit.edu>
2023-07-26 14:42:27 -0400

    adding ffcontrol, hops_test, and vpal (minus proxy cable cal) modules to python_src, also moving extranous libs to attic until proper reorg

---
+++ source/python_src/attic/type200s/test-json.py
@@ -0,0 +1,30 @@
+import json
+from datetime import datetime
+
+def print_data():
+    file = open('type200-example.json', 'r')
+    data = json.load(file)
+
+    #examples of usage
+    print('Experiment name: {}'.format(data['exper_name']))
+
+    print('record_id + version_no: {}'.format(data['record_id']+data['version_no']))
+
+    print('date scantime: {}'.format(data['date']['scantime']))
+
+    print(datetime.strptime(data['time_stamp'], '%Y-%m-%dT%H:%M:%S.%f%z'))
+
+    file.close()
+
+def write_to_data():
+    file = open('test.json', 'w')
+    print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True, indent=4))
+
+    json.dump({"c": 0, "b": 0, "a": 0}, file, sort_keys=True, indent=4)
+    
+    file.close()
+
+
+if __name__ == "__main__":
+    print_data()
+    write_to_data()
---
+++ source/python_src/vpal_module/vpal/__init__.py
@@ -0,0 +1,6 @@
+from . import utility
+from . import report_lib
+from . import fringe_file_manipulation
+from . import processing
+from . import ffres2pcp_lib
+from . import fourphase_lib
