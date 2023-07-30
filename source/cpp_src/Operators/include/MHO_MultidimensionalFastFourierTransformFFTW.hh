27e30b1183cbf630d3c7222b0ddc51fe0b5a9933
John Barrett <barrettj@mit.edu>
2023-07-30 17:13:32 -0400

    xform axis labels for fftw based fft

--- source/cpp_src/Operators/include/MHO_MultidimensionalFastFourierTransformFFTW.hh
+++ source/cpp_src/Operators/include/MHO_MultidimensionalFastFourierTransformFFTW.hh
@@ -4,11 +4,13 @@
 #include <cstring>
 #include <fftw3.h>
 
+#include "MHO_Meta.hh"
 #include "MHO_Message.hh"
 #include "MHO_NDArrayWrapper.hh"
 #include "MHO_UnaryOperator.hh"
 #include "MHO_FFTWTypes.hh"
 
+#include "MHO_TableContainer.hh"
 
 #define HOPS_FFTW_PLAN_ALGO FFTW_ESTIMATE
 //could use FFTW_MEASURE instead
@@ -224,6 +226,17 @@ class MHO_MultidimensionalFastFourierTransformFFTW:
                     }
                     std::memcpy(out->GetData(), fOutPtr, fTotalArraySize*sizeof(typename MHO_FFTWTypes<floating_point_value_type>::fftw_complex_type) );
                 }
+
+                
+                for(size_t d=0; d<XArgType::rank::value; d++)
+                {
+                    if(fAxesToXForm[d])
+                    {
+                        IfTableTransformAxis(in,d)
+                    }
+                };
+
+
                 return true;
             }
             else
@@ -344,7 +357,106 @@ class MHO_MultidimensionalFastFourierTransformFFTW:
         }
 
 
+        //axis handling for table containers 
 
+
+        //default...does nothing
+        template< typename XCheckType = XArgType >
+        typename std::enable_if< !std::is_base_of<MHO_TableContainerBase, XCheckType>::value, void >::type
+        IfTableTransformAxis(XArgType* /*in*/, std::size_t /*axis_index*/){};
+
+        //use SFINAE to generate specialization for MHO_TableContainer types
+        template< typename XCheckType = XArgType >
+        typename std::enable_if< std::is_base_of<MHO_TableContainerBase, XCheckType>::value, void >::type
+        IfTableTransformAxis(XArgType* in, std::size_t axis_index)
+        {
+            if(fAxesToXForm[axis_index]) //only xform axis if this dimension was transformed
+            {
+                TransformAxis axis_xformer;
+                apply_at< typename XArgType::axis_pack_tuple_type, TransformAxis >( *in, axis_index, axis_xformer);
+            }
+        }
+
+
+        class TransformAxis
+        {
+            public:
+                TransformAxis(){};
+                ~TransformAxis(){};
+
+                //generic axis, do nothing
+                template< typename XAxisType >
+                void operator()(XAxisType& /*axis1*/){};
+
+                //overload for doubles
+                void operator()(MHO_Axis<double>& axis1)
+                {
+                    //this is under the expectation that all axis labels are equi-spaced
+                    //this should be a safe assumption since we are doing DFT anyway
+                    //one issue here is that we are not taking into account units (e.g. nanosec or MHz)
+                    std::size_t N = axis1.GetSize();
+                    double length = N;
+                    if(N > 1)
+                    {
+                        double delta = axis1(1) - axis1(0);
+                        double spacing = (1.0/delta)*(1.0/length);
+                        double start = 0;//-1*length/2;
+                        for(std::size_t i=0; i<N; i++)
+                        {
+                            double x = i;
+                            if(i<N/2)
+                            {
+                                start = 0;
+                                double value = (x+start)*spacing;
+                                axis1(i) = value;
+                            }
+                            else
+                            {
+                                start = -1*length;
+                                double value = (x+start)*spacing;
+                                axis1(i) = value;
+                            }
+                        }
+                    }
+                }
+
+                //overload for floats
+                void operator()(MHO_Axis<float>& axis1)
+                {
+                    //this is under the expectation that all axis labels are equi-spaced
+                    //this should be a safe assumption since we are doing DFT anyway
+                    //one issue here is that we are not taking into account units (e.g. nanosec or MHz)
+                    std::size_t N = axis1.GetSize();
+                    float length = N;
+                    if(N > 1)
+                    {
+                        float delta = axis1(1) - axis1(0);
+                        float spacing = (1.0/delta)*(1.0/length);
+                        float start = 0;//-1*length/2;
+                        for(std::size_t i=0; i<N; i++)
+                        {
+                            float x = i;
+                            if(i<N/2)
+                            {
+                                start = 0;
+                                float value = (x+start)*spacing;
+                                axis1(i) = value;
+                            }
+                            else
+                            {
+                                start = -1*length;
+                                float value = (x+start)*spacing;
+                                axis1(i) = value;
+                            }
+                        }
+                    }
+                }
+
+        };
+
+
+
+        //data
         bool fIsValid;
         bool fForward;
         bool fInitialized;
