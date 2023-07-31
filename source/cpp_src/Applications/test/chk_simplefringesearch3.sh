b89a62431ce0b85d9b9dafed20a2ebb78408e35e
John Barrett <barrettj@mit.edu>
2023-07-31 00:20:07 -0400

    start work on new test

--- source/cpp_src/Applications/test/chk_simplefringesearch3.sh
+++ source/cpp_src/Applications/test/chk_simplefringesearch3.sh
@@ -4,24 +4,23 @@ verb=false
 
 [ -d "$srcdir" ] || { echo srcdir not set; exit 1; }
 ${HOPS_SETUP-'false'} || . $srcdir/chk_env.sh
-export DATADIR=`cd $srcdir/vt9105; pwd`
+export DATADIR=`cd $srcdir/3764; pwd`
 
 RET_VAL=0
 EXP_DIR=$DATADIR
-D2H_EXP_NUM=1111
-D2M4_EXP_NUM=1234
-SCAN_DIR=105-1800
+D2H_EXP_NUM=hops4
+D2M4_EXP_NUM=.
+SCAN_DIR=104-1228
 cd $EXP_DIR
 
-echo "Running: SimpleFringeSearch -d ./${D2H_EXP_NUM}/${SCAN_DIR} -c ./cf_test4 -b GE -p XX"
-time SimpleFringeSearch -d ./${D2H_EXP_NUM}/${SCAN_DIR} -c ./cf_test4 -b GE -p XX | grep max555 | tee ./sfs.out
+echo "Running: SimpleFringeSearch -d ./${D2H_EXP_NUM}/${SCAN_DIR} -c ./test0.cf -b AS -p RR"
+time SimpleFringeSearch -d ./${D2H_EXP_NUM}/${SCAN_DIR} -c ./test0.cf -b AS -p RR | tee ./sfs.out
 
-echo "Running: fourfit -m 4 -c ./cf_test4 -b GE -P XX ./${D2M4_EXP_NUM}/${SCAN_DIR}"
-time fourfit -m 4 -c ./cf_test4 -b GE -P XX ./${D2M4_EXP_NUM}/${SCAN_DIR} 2>&1  | tee ./ff.out
+echo "Running: fourfit -m 4 -c ./test0.cf -b AS -P RR ./${SCAN_DIR} set plot_data_dir ./chk3 "
+time fourfit -m 1 -c ./test0.cf -b AS -P RR ./${SCAN_DIR} set plot_data_dir ./chk3 2>&1  | tee ./ff.out
 
-FF_OUTPUT=$(cat ./ff.out |  awk '{print $2}')
+cat ./chk3/104-1228-AS-B-RR.2GAB6T
 
-echo "$FF_OUTPUT"
 RET_VAL=0
 
 exit $RET_VAL
