b54318eaacecab68807529d2a29b89306500006e
John Barrett <barrettj@mit.edu>
2023-07-31 17:50:28 -0400

    ocl fft wip

--- source/cpp_src/Plugins/OpenCL/Operators/include/MHO_OpenCLMultidimensionalFastFourierTransform.hh
+++ source/cpp_src/Plugins/OpenCL/Operators/include/MHO_OpenCLMultidimensionalFastFourierTransform.hh
@@ -11,6 +11,7 @@
 #include "MHO_FastFourierTransform.hh"
 #include "MHO_FastFourierTransformWorkspace.hh"
 #include "MHO_FastFourierTransformUtilities.hh"
+#include "MHO_MultidimensionalFastFourierTransformInterface.hh"
 
 #include "MHO_OpenCLInterface.hh"
 #include "MHO_OpenCLKernelBuilder.hh"
@@ -22,7 +23,8 @@ namespace hops
 
 template< typename XArgType >
 class MHO_OpenCLMultidimensionalFastFourierTransform:
-    public MHO_UnaryInPlaceOperator< XArgType >
+    public MHO_UnaryInPlaceOperator< XArgType >,
+    public MHO_MultidimensionalFastFourierTransformInterface< XArgType >
 {
     public:
 
@@ -30,39 +32,16 @@ class MHO_OpenCLMultidimensionalFastFourierTransform:
         using complex_value_type = typename XArgType::value_type;
         using floating_point_value_type = typename XArgType::value_type::value_type;
 
-        MHO_OpenCLMultidimensionalFastFourierTransform()
+        MHO_OpenCLMultidimensionalFastFourierTransform():
+            MHO_MultidimensionalFastFourierTransformInterface< XArgType >()
         {
-            for(size_t i=0; i<XArgType::rank::value; i++)
-            {
-                fDimensionSize[i] = 0;
-                fAxesToXForm[i] = true;
-            }
 
-            fIsValid = false;
-            fInitialized = false;
-            fForward = true;
         };
 
         virtual ~MHO_OpenCLMultidimensionalFastFourierTransform()
         {
             DeallocateWorkspace();
         };
-
-        virtual void SetForward(){fForward = true;}
-        virtual void SetBackward(){fForward = false;};
-
-        //sometimes we may want to select/deselect particular dimensions of the x-form
-        //default is to transform along every dimension, but that may not always be needed
-        void SelectAllAxes(){for(std::size_t i=0; i<XArgType::rank::value; i++){fAxesToXForm[i] = true;}}
-        void DeselectAllAxes(){for(std::size_t i=0; i<XArgType::rank::value; i++){fAxesToXForm[i] = false;}}
-        void SelectAxis(std::size_t axis_index)
-        {
-            if(axis_index < XArgType::rank::value){fAxesToXForm[axis_index] = true;}
-            else
-            {
-                msg_error("operators", "Cannot transform axis with index: " << axis_index << "for array with rank: " << XArgType::rank::value << eom);
-            }
-        }
 
     protected:
 
@@ -119,12 +98,7 @@ class MHO_OpenCLMultidimensionalFastFourierTransform:
         
         //data
 
-        bool fIsValid;
-        bool fForward;
-        bool fInitialized;
 
-        size_t fDimensionSize[XArgType::rank::value];
-        bool fAxesToXForm[XArgType::rank::value];
 
 };
 
