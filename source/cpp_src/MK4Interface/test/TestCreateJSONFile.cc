4852258bfa126688863cd8b8d1c12c054b94dacc
Violet Pfeiffer <violetp@mit.edu>
2023-08-01 10:38:23 -0400

    Call MK4FringeInterface

--- source/cpp_src/MK4Interface/test/TestCreateJSONFile.cc
+++ source/cpp_src/MK4Interface/test/TestCreateJSONFile.cc
@@ -1,16 +1,5 @@
 #include <iostream>
 #include <string>
-#include "MHO_MK4Type200Converter.hh"
-#include "MHO_MK4Type201Converter.hh"
-#include "MHO_MK4Type202Converter.hh"
-#include "MHO_MK4Type203Converter.hh"
-#include "MHO_MK4Type204Converter.hh"
-#include "MHO_MK4Type205Converter.hh"
-#include "MHO_MK4Type206Converter.hh"
-#include "MHO_MK4Type207Converter.hh"
-#include "MHO_MK4Type208Converter.hh"
-#include "MHO_MK4Type210Converter.hh"
-#include "MHO_MK4Type212Converter.hh"
 #include "MHO_MK4FringeInterface.hh"
 
 using namespace hops;
@@ -18,12 +7,14 @@ using namespace hops;
 int main(int argc, char **argv) {
 
   // Call all the type200 tests here
+  //std::ofstream o("fringe-files.json");
 
   // Call interface to read fringe file
-  MHO_MK4FringeInterface* fringeReader = new MHO_MK4FringeInterface();
-  //MHO_MK4FringeInterface mk4FringeInterface;
+  //MHO_MK4FringeInterface* fringeReader = new MHO_MK4FringeInterface();
+  MHO_MK4FringeInterface mk4FringeInterface;
   //mk4FringeInterface.ExportFringeFilesToJSON(fringeFile);
-  fringeReader->ExportFringeStructsToJSON();
+  //fringeReader->ExportFringeFilesToJSON();
+  mk4FringeInterface.ExportFringeFilesToJSON();
 
   return 0;
 }
