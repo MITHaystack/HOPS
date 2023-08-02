443268bfaff6a4c5838d503877acadf7ee1baa6b
John Barrett <barrettj@mit.edu>
2023-08-02 04:10:14 -0400

    fix fft call with stride

--- source/cpp_src/Math/include/MHO_FastFourierTransformCalls.hh
+++ source/cpp_src/Math/include/MHO_FastFourierTransformCalls.hh
@@ -24,7 +24,7 @@ void FFTRadix2(std::complex<XFloatType>* data, MHO_FastFourierTransformWorkspace
 
     //use radix-2
     MHO_BitReversalPermutation::PermuteArray< std::complex<XFloatType> >(work.fN, work.fPermutation, data);
-    MHO_FastFourierTransformUtilities<XFloatType>::FFTRadixTwo_DIT(work.fN, data, work.fTwiddle);
+    MHO_FastFourierTransformUtilities<XFloatType>::FFTRadixTwo_DIT(work.fN, data, work.fTwiddle, stride);
 
     //for DFT we conjugate again (NOTE: this matches FFTW3 convention)
     if(isForward){ MHO_FastFourierTransformUtilities<XFloatType>::Conjugate(work.fN, data, stride); }
@@ -38,7 +38,7 @@ void FFTBluestein(std::complex<XFloatType>* data, MHO_FastFourierTransformWorksp
     if(isForward){ MHO_FastFourierTransformUtilities<XFloatType>::Conjugate(work.fN, data, stride); }
 
     //use bluestein algorithm for arbitrary (non-power of 2) N
-    MHO_FastFourierTransformUtilities<XFloatType>::FFTBluestein(work.fN, work.fM, data, work.fTwiddle, work.fConjugateTwiddle, work.fScale, work.fCirculant, work.fWorkspace);
+    MHO_FastFourierTransformUtilities<XFloatType>::FFTBluestein(work.fN, work.fM, data, work.fTwiddle, work.fConjugateTwiddle, work.fScale, work.fCirculant, work.fWorkspace, stride);
 
     //for DFT we conjugate again (NOTE: this matches FFTW3 convention)
     if(isForward){ MHO_FastFourierTransformUtilities<XFloatType>::Conjugate(work.fN, data, stride); }
