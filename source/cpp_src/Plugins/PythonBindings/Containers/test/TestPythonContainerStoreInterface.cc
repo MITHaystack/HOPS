472a002f159f73a942489c1f0d4a8a330a50bd93
John Barrett <barrettj@mit.edu>
2023-09-14 16:42:44 -0400

    object uuid look-up seems to work

--- source/cpp_src/Plugins/PythonBindings/Containers/test/TestPythonContainerStoreInterface.cc
+++ source/cpp_src/Plugins/PythonBindings/Containers/test/TestPythonContainerStoreInterface.cc
@@ -13,6 +13,9 @@ namespace py = pybind11;
 #include "MHO_ContainerStore.hh"
 #include "MHO_PyContainerStoreInterface.hh"
 
+#include "MHO_ParameterStore.hh"
+#include "MHO_PyParameterStoreInterface.hh"
+
 using namespace hops;
 
 
@@ -37,17 +40,23 @@ int main()
     key.fLabel = 0;
     strncpy(key.fName, "vis", 3);
 
+    //stuff something in the container store
     store.AddObject(obj);
     store.SetObjectLabel(obj->GetObjectUUID(), key.fLabel);
     std::string shortname = std::string(key.fName, MHO_FileKeyNameLength ).c_str();
     store.SetShortName(obj->GetObjectUUID(), shortname);
 
-    //stuff something in the container store
+    //now put the object uuid in the parameter store so we can look it up on the python side
+    MHO_ParameterStore paramStore;
+    std::string obj_uuid_string = obj->GetObjectUUID().as_string();
+    paramStore.Set("my_uuid", obj_uuid_string);
 
+    //create the interfaces
     MHO_PyContainerStoreInterface conInter(&store);
+    MHO_PyParameterStoreInterface parmInter(&paramStore);
 
     auto mho_constore_test = py::module::import("mho_container_store_test");
-    mho_constore_test.attr("test_inter")(conInter);
+    mho_constore_test.attr("test_inter")(conInter, parmInter);
 
     return 0;
 }
--- source/cpp_src/Plugins/PythonBindings/Containers/test/mho_container_store_test.py
+++ source/cpp_src/Plugins/PythonBindings/Containers/test/mho_container_store_test.py
@@ -1,5 +1,9 @@
 import pyMHO_Containers
 
-def test_inter(interface_obj):
-    n_obj =  interface_obj.GetNObjects();
+def test_inter(cstore_interface_obj, param_interface_obj):
+    n_obj =  cstore_interface_obj.GetNObjects();
     print("n objects present = ", n_obj)
+    uuid_string = param_interface_obj.Get("my_uuid");
+    print("expecting an object with uuid: ", uuid_string)
+    print("object with that uuid is present? ", cstore_interface_obj.IsObjectPresent(uuid_string) )
+    #std::cout<<"object uuid = "<<uuid_string<<std::endl;
