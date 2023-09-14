a32e9055e1195e61ba3faf8c30af02e97c536490
John Barrett <barrettj@mit.edu>
2023-09-14 00:48:10 -0400

    make mbd_anchor model is set as default

--- source/cpp_src/Applications/ffit/set_default_parameters.cc
+++ source/cpp_src/Applications/ffit/set_default_parameters.cc
@@ -2,14 +2,18 @@
 
 void set_default_parameters(MHO_ContainerStore* conStore, MHO_ParameterStore* paramStore)
 {
-
     //set the selected pol-product
     std::string polprod = paramStore->GetAs<std::string>("/cmdline/polprod");
     paramStore->Set("selected_polprod", polprod);
 
-    //grab the visibility data, to determine the default reference frequency
+    //grab the visibility data, so we can determine the default reference frequency
     visibility_type* vis_data = conStore->GetObject<visibility_type>(std::string("vis"));
+
     //the first frequency in the array serves as the reference frequency if this value remains unset in the control file
     double first_freq = std::get<CHANNEL_AXIS>(*vis_data)(0);
     paramStore->Set("ref_freq", first_freq);
+
+    //default mbd_anchor is model (instead of sbd)
+    std::string mbd_anchor = "model";
+    paramStore->Set("mbd_anchor", mbd_anchor);
 };
