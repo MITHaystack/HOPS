2f0e3537a7e85f8a64be6ceea1f664c861f583ad
John Barrett <barrettj@mit.edu>
2023-09-21 10:50:21 -0400

    renamed vex2json library to conform to standard library naming scheme

--- source/cpp_src/Fringe/src/MHO_BasicFringeDataConfiguration.cc
+++ source/cpp_src/Fringe/src/MHO_BasicFringeDataConfiguration.cc
@@ -217,4 +217,20 @@ MHO_BasicFringeDataConfiguration::configure_data_library(MHO_ContainerStore* sto
     store->SetShortName(wt_data->GetObjectUUID(), wt_shortname);
 }
 
+
+void 
+MHO_BasicFringeDataConfiguration::init_and_exec_operators(MHO_OperatorBuilderManager& build_manager, MHO_OperatorToolbox* opToolbox, const char* category)
+{
+    std::string cat(category);
+    build_manager.BuildOperatorCategory(cat);
+    auto ops = opToolbox->GetOperatorsByCategory(cat);
+    for(auto opIt= ops.begin(); opIt != ops.end(); opIt++)
+    {
+        msg_debug("main", "initializing and executing operator: "<< (*opIt)->GetName() << eom);
+        (*opIt)->Initialize();
+        (*opIt)->Execute();
+    }
+}
+
+
 }//end namespace
