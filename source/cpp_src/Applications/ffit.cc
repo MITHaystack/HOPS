a94e4ded38f1486def8798a6679b490ddb766cf6
John Barrett <barrettj@mit.edu>
2023-09-21 13:16:24 -0400

    put back the pybind11 operator extenions -- formalize this process later

--- source/cpp_src/Applications/ffit.cc
+++ source/cpp_src/Applications/ffit.cc
@@ -65,12 +65,12 @@ int main(int argc, char** argv)
     
     ffit.Configure();
     
-    // #ifdef USE_PYBIND11
-    // #pragma message("TODO FIXME -- formalize the means by which plugin dependent operator builders are added to the configuration")
-    // ffit.GetOperatorBuildManager()->AddBuilderType<MHO_PythonOperatorBuilder>("python_labelling", "python_labelling"); 
-    // ffit.GetOperatorBuildManager()->AddBuilderType<MHO_PythonOperatorBuilder>("python_flagging", "python_flagging"); 
-    // ffit.GetOperatorBuildManager()->AddBuilderType<MHO_PythonOperatorBuilder>("python_calibration", "python_calibration"); 
-    // #endif
+    #ifdef USE_PYBIND11
+    #pragma message("TODO FIXME -- formalize the means by which plugin dependent operator builders are added to the configuration")
+    ffit.GetOperatorBuildManager()->AddBuilderType<MHO_PythonOperatorBuilder>("python_labelling", "python_labelling"); 
+    ffit.GetOperatorBuildManager()->AddBuilderType<MHO_PythonOperatorBuilder>("python_flagging", "python_flagging"); 
+    ffit.GetOperatorBuildManager()->AddBuilderType<MHO_PythonOperatorBuilder>("python_calibration", "python_calibration"); 
+    #endif
     
     ffit.Initialize();
     while( ! ffit.IsFinished() )
