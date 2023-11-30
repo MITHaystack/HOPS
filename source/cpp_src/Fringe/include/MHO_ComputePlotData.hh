6f3a20dc458b2dd3f35fb7e03febae14a9c7e598
John Barrett <barrettj@mit.edu>
2023-11-30 12:11:00 -0500

    fill in more plot data in text table

--- source/cpp_src/Fringe/include/MHO_ComputePlotData.hh
+++ source/cpp_src/Fringe/include/MHO_ComputePlotData.hh
@@ -65,8 +65,13 @@ class MHO_ComputePlotData
 
         double calc_phase();
 
-        void calc_freqrms(phasor_type& phasors, double coh_avg_phase, double fringe_amp, double total_summed_weights, double& freqrms_phase, double& freqrms_amp);
-        void calc_timerms(phasor_type& phasors, std::size_t nseg, std::size_t apseg, double coh_avg_phase, double fringe_amp, double total_summed_weights, double snr, double& timerms_phase, double& timerms_amp);
+        void calc_freqrms(phasor_type& phasors, double coh_avg_phase, double fringe_amp, 
+                          double total_summed_weights, double snr, 
+                          double& freqrms_phase, double& freqrms_amp, double& inc_avg_amp_freq);
+                          
+        void calc_timerms(phasor_type& phasors, std::size_t nseg, std::size_t apseg,
+                          double coh_avg_phase, double fringe_amp, double total_summed_weights, double snr,
+                          double& timerms_phase, double& timerms_amp, double& inc_avg_amp);
 
         std::string calc_quality_code(); //quality only, not error
 
