20fbc9124bc0869a2a732ae3123fde35f2b18eb5
John Barrett <barrettj@mit.edu>
2023-12-04 13:04:06 -0500

    ultra basic pc delay extraction test works

--- source/cpp_src/Applications/TestPCalExtraction.cc
+++ source/cpp_src/Applications/TestPCalExtraction.cc
@@ -26,7 +26,61 @@
 
 #include "MHO_VexInfoExtractor.hh"
 
+
+#ifdef HOPS_USE_FFTW3
+#include "MHO_MultidimensionalFastFourierTransformFFTW.hh"
+#else
+#include "MHO_MultidimensionalFastFourierTransform.hh"
+#endif
+
+
 using namespace hops;
+
+
+double dwin(double value, double lower, double upper)
+{
+    if (value < lower) return (lower);
+    else if (value > upper) return (upper);
+    else return (value);
+}
+
+int
+parabola (double y[3], double lower, double upper, double* x_max, double* amp_max, double q[3])
+{
+    int i, rc;
+    double x, range;
+    //extern double dwin(double, double, double);
+    range = std::fabs (upper - lower);
+
+    q[0] = (y[0] - 2 * y[1] + y[2]) / 2;      /* This is trivial to derive,
+    	                              or see rjc's 94.1.10 derivation */
+    q[1] = (y[2] - y[0]) / 2;
+    q[2] = y[1];
+
+
+    if (q[0] < 0.0)
+        x = -q[1] / (2 * q[0]);                      /* x value at maximum y */
+    else                                         /* no max, pick higher side */
+        x = (y[2] > y[0]) ? 1.0 : -1.0;
+
+    *x_max = dwin (x, lower, upper);
+
+    *amp_max = q[0] * *x_max * *x_max  +  q[1] * *x_max  +  q[2];
+
+    // Test for error conditions
+
+    rc = 0;                         // default: indicates error-free interpolation
+    if (q[0] >= 0)                  // 0 or positive curvature is an interpolation error
+        rc = 2;
+                                    // Is maximum at either edge?
+                                    // (simple floating point equality test can fail
+                                    // in machine-dependent way)
+    else if (std::fabs (*x_max - x) > (0.001 * range))
+        rc = 1;
+
+    return (rc);
+}
+
 
 int main(int argc, char** argv)
 {
@@ -216,9 +270,86 @@ int main(int argc, char** argv)
     std::cout<<"start tone = "<<start_tone_frequency<<", start tone index = "<<start_tone_index<<", ntones = "<<ntones<<std::endl;
 
     //should we channelize the pca-data? yes...but for now just do an FFT on one chunk to test
+    //extract the p-cal data between the start/stop indices for this particular pol from the first AP
+    using pcal_axis_pack = MHO_AxisPack< frequency_axis_type >;
+    using pcal_type = MHO_TableContainer< std::complex<double>, pcal_axis_pack >;
+
+    int FFTSIZE = 256; //default x-form size in pcalibrate
+    pcal_type test;
+    test.Resize(FFTSIZE);
+    test.ZeroArray();
+
+    for(std::size_t j=0; j<ntones; j++)
+    {
+        std::complex<double> phasor = pcal_data->at(0, 0, start_tone_index+j);
+        double tone_freq = tone_freq_ax(start_tone_index+j);
+        test.at(j) = phasor;
+        std::get<0>(test).at(j) = tone_freq;
+    }
+
+    double pc_tone_delta = 1e6*(std::get<0>(test)(1) - std::get<0>(test)(0));
+    std::cout<<"PCAL TONE DELTA = "<<pc_tone_delta << std::endl;
+
+    for(std::size_t i=0; i<FFTSIZE; i++)
+    {
+        std::get<0>(test).at(i) = i*pc_tone_delta; //actual freq is irrelevant
+    }
+
+    #ifdef HOPS_USE_FFTW3
+    using FFT_ENGINE_TYPE = MHO_MultidimensionalFastFourierTransformFFTW< pcal_type >;
+    #else
+    using FFT_ENGINE_TYPE = MHO_MultidimensionalFastFourierTransform< pcal_type >;
+    #endif
+
+    FFT_ENGINE_TYPE fFFTEngine;
 
 
+    fFFTEngine.SetArgs(&test);
+    fFFTEngine.DeselectAllAxes();
+    fFFTEngine.SelectAxis(0); //only perform padded fft on frequency (to lag) axis
+    fFFTEngine.SetForward();//forward DFT
 
+    ok = fFFTEngine.Initialize();
+    ok = fFFTEngine.Execute();
+
+    double max_val = 0;
+    int max_idx = 0;
+    double max_del = 0;
+    for(std::size_t i=0; i<FFTSIZE; i++)
+    {
+        std::complex<double> phasor = test.at(i);
+        double abs_val = std::abs(phasor);
+        if(abs_val > max_val)
+        {
+            max_val = abs_val;
+            max_idx = i;
+            max_del = std::get<0>(test)(i);
+        }
+    }
+
+    double delay_delta = (std::get<0>(test)(1) - std::get<0>(test)(0));
+    std::cout<<"delay_delta = "<<delay_delta<<std::endl;
+
+    std::cout<<"max, max_idx, max_del = "<<max_val<<", "<<max_idx<<", "<<max_del<<std::endl;
+
+    double ymax, ampmax;
+    double y[3];
+    double q[3];
+    y[1] = max_val;
+    y[0] =std::abs (test[(max_idx+FFTSIZE-1)%FFTSIZE]);
+    y[2] = std::abs (test[(max_idx+FFTSIZE+1)%FFTSIZE]);
+    parabola (y, -1.0, 1.0, &ymax, &ampmax, q);
+
+                        // DC is in 0th element
+    double delay = (max_idx+ymax) / 256.0 / pc_tone_delta;
+
+    double delay2 = (max_idx+ymax)*delay_delta;
+    std::cout<<"ymax = "<<ymax<<std::endl;
+    std::cout<<"DELAY = "<<delay<<std::endl;
+    std::cout<<"DELAY2 = "<<delay2<<std::endl;
+
+                        // find corresponding delay in suitable range
+    double pc_amb = 1 / pc_tone_delta;
 
 
     return 0;
