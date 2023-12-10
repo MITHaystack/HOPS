624f933ba749fb7d6fd2f3ef62a72b38a7843f75
John Barrett <barrettj@mit.edu>
2023-12-09 20:08:31 -0500

    debug

--- source/cpp_src/Initialization/src/MHO_ParameterManager.cc
+++ source/cpp_src/Initialization/src/MHO_ParameterManager.cc
@@ -6,7 +6,7 @@ namespace hops
 void
 MHO_ParameterManager::ConfigureAll()
 {
-    //loop over control statements, find the parameter statements and put them in 
+    //loop over control statements, find the parameter statements and put them in
     //parameter store, then remove them from further processing
     for(auto ctrl_iter = fControl->begin(); ctrl_iter != fControl->end(); ctrl_iter++)
     {
@@ -16,7 +16,7 @@ MHO_ParameterManager::ConfigureAll()
         for(auto stmt_iter = statements->begin(); stmt_iter != statements->end(); )
         {
             std::string name = (*stmt_iter)["name"];
-            
+
             if(fFormat.contains(name) && fFormat[name].contains("statement_type") )
             {
                 std::string stmt_type = fFormat[name]["statement_type"].get<std::string>();
@@ -26,13 +26,13 @@ MHO_ParameterManager::ConfigureAll()
                     if(fFormat[name].contains("parameter_type")){parameter_type = fFormat[name]["parameter_type"].get<std::string>();}
                     msg_debug("initialization", "configuring parameter: "<<name<<" of type: "<<parameter_type<<eom);
                     consumed_elements.push_back(name);
-                    fDefaultParameterConfig.SetConditions(*ctrl_iter);
+                    fDefaultParameterConfig.SetConditions( (*ctrl_iter)["value"] );
                     fDefaultParameterConfig.SetAttributes(*stmt_iter);
                     fDefaultParameterConfig.Configure();
                     stmt_iter = statements->erase(stmt_iter);
                 }
                 else{stmt_iter++;}
-                //if need expand the possible parameter/statement types 
+                //if need expand the possible parameter/statement types
 
             }
             else
