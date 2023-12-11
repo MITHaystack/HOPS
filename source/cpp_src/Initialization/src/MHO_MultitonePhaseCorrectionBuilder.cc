baaf1d2c07602365deb0b7985dcef9a26da986ee
John Barrett <barrettj@mit.edu>
2023-12-11 16:59:05 -0500

    getting it compile..not yet working

--- source/cpp_src/Initialization/src/MHO_MultitonePhaseCorrectionBuilder.cc
+++ source/cpp_src/Initialization/src/MHO_MultitonePhaseCorrectionBuilder.cc
@@ -20,15 +20,31 @@ MHO_MultitonePhaseCorrectionBuilder::Build()
         // std::string channel_name_str = fAttributes["value"]["channel_names"].get<std::string>();
         // std::vector<double> pc_phases = fAttributes["value"]["pc_phases"].get< std::vector<double> >();
 
-
         std::string mk4id = ExtractStationMk4ID();
 
         //retrieve the arguments to operate on from the container store
         visibility_type* vis_data = fContainerStore->GetObject<visibility_type>(std::string("vis"));
-        visibility_type* pcal_data = fContainerStore->GetObject<visibility_type>(std::string("rem_pcal"));
+
+        multitone_pcal_type* pcal_data = nullptr;
+        if(op_name == "ref_multitone_pcal")
+        {
+            multitone_pcal_type* pcal_data = fContainerStore->GetObject<multitone_pcal_type>(std::string("ref_pcal"));
+        }
+
+        if(op_name == "rem_multitone_pcal")
+        {
+            multitone_pcal_type* pcal_data = fContainerStore->GetObject<multitone_pcal_type>(std::string("rem_pcal"));
+        }
+
         if( vis_data == nullptr )
         {
             msg_error("initialization", "cannot construct MHO_MultitonePhaseCorrection without visibility data." << eom);
+            return false;
+        }
+
+        if( pcal_data == nullptr )
+        {
+            msg_error("initialization", "cannot construct MHO_MultitonePhaseCorrection without pcal data." << eom);
             return false;
         }
 
@@ -58,20 +74,20 @@ std::string
 MHO_MultitonePhaseCorrectionBuilder::ExtractStationMk4ID()
 {
     std::string station_id = "?";
-    std::vector< std::string > condition_values = fConditions["value"].get< std::vector< std::string > >();
-
-    for(auto it = condition_values.begin(); it != condition_values.end(); it++)
-    {
-         //grab the first station ID in the 'if' statement
-         //this is ok 99% of the time, but what about if there is statement like: 'if station X or station X'?
-         //would then need to check that this station is a member of this pass too, and if not use the next
-        if(*it == "station")
-        {
-            it++;
-            if(it != condition_values.end()){station_id = *it; return station_id;}
-        }
-    }
-    return station_id;
+    // std::vector< std::string > condition_values = fConditions["value"].get< std::vector< std::string > >();
+    //
+    // for(auto it = condition_values.begin(); it != condition_values.end(); it++)
+    // {
+    //      //grab the first station ID in the 'if' statement
+    //      //this is ok 99% of the time, but what about if there is statement like: 'if station X or station X'?
+    //      //would then need to check that this station is a member of this pass too, and if not use the next
+    //     if(*it == "station")
+    //     {
+    //         it++;
+    //         if(it != condition_values.end()){station_id = *it; return station_id;}
+    //     }
+    // }
+     return station_id;
 }
 
 
