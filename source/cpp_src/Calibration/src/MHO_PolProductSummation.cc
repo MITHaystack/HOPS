9ec900f5edcd925f0050e07afa3f209c2c4e4102
John Barrett <barrettj@mit.edu>
2024-01-15 11:03:31 -0500

    pol prod summation wip

--- source/cpp_src/Calibration/src/MHO_PolProductSummation.cc
+++ source/cpp_src/Calibration/src/MHO_PolProductSummation.cc
@@ -7,102 +7,78 @@ namespace hops
 
 MHO_PolProductSummation::MHO_PolProductSummation()
 {
-    // fStationKey = "station";
-    // fRemStationKey = "remote_station";
-    // fRefStationKey = "reference_station";
-    // fRemStationMk4IDKey = "remote_station_mk4id";
-    // fRefStationMk4IDKey = "reference_station_mk4id";
-    // fChannelLabelKey = "channel_label";
-    // 
-    // fStationCode = "";
-    // fMk4ID = "";
-    // 
-    // fImagUnit = MHO_Constants::imag_unit;
-    // fDegToRad = MHO_Constants::deg_to_rad;
-    // 
-    // fPhaseOffset = 0.0;
+    fSummedPolProdLabel = "XX";
 };
 
 MHO_PolProductSummation::~MHO_PolProductSummation(){};
 
 
-// bool
-// MHO_PolProductSummation::ExecuteInPlace(visibility_type* in)
-// {
-    // std::size_t st_idx = DetermineStationIndex(in);
-    // if(st_idx != 0 && st_idx != 1){return false;}
-    // 
-    // //loop over pol-products and apply pc-phases to the appropriate pol/channel
-    // auto pp_ax = &(std::get<POLPROD_AXIS>(*in) );
-    // std::string pp_label;
-    // for(std::size_t pp=0; pp < pp_ax->GetSize(); pp++)
-    // {
-    //     pp_label = pp_ax->at(pp);
-    //     if( PolMatch(st_idx, pp_label) )
-    //     {
-    //         visibility_element_type pc_phasor = std::exp( fImagUnit*fPhaseOffset*fDegToRad );
-    // 
-    //         //conjugate the phase for the reference station, but not remote?
-    //         //should this behavior change depending on the USB/LSB?
-    //         #pragma message("TODO FIXME - test all manual pc phase correction cases (ref/rem/USB/LSB/DSB)")
-    //         if(st_idx == 1){pc_phasor = std::conj(pc_phasor);} //conjugate for remote but not reference station
-    //         //retrieve and multiply the appropriate sub view of the visibility array
-    //         auto chunk = in->SubView(pp);
-    //         chunk *= pc_phasor;
-    //     }
-    // }
-// 
-//     return true;
-// }
+bool
+MHO_PolProductSummation::ExecuteInPlace(visibility_type* in)
+{
+    PreMultiply(in);
+    bool ok = fReducer.Execute();
+    FixLabels(in);
+    return ok;
+}
 
 
 bool
 MHO_PolProductSummation::ExecuteOutOfPlace(const visibility_type* in, visibility_type* out)
 {
     out->Copy(*in);
-    return ExecuteInPlace(out);
+    PreMultiply(out);
+    bool ok = fReducer.Execute();
+    FixLabels(out);
+    return ok;
+}
+
+bool
+MHO_PolProductSummation::InitializeInPlace(visibility_type* in)
+{
+    fReducer.SetArgs(in);
+    return fReducer.Initialize();
+}
+
+bool
+MHO_PolProductSummation::InitializeOutOfPlace(const visibility_type* in, visibility_type* out)
+{
+    fReducer.SetArgs(out);
+    return fReducer.Initialize();
 }
 
 
-// std::size_t
-// MHO_PolProductSummation::DetermineStationIndex(const visibility_type* in)
-// {
-//     //determine if the p-cal corrections are being applied to the remote or reference station
-//     std::string val;
-// 
-//     if(fMk4ID != "") //selection by mk4 id
-//     {
-//         in->Retrieve(fRemStationMk4IDKey, val);
-//         if(fMk4ID == val){return 1;}
-//         in->Retrieve(fRefStationMk4IDKey, val);
-//         if(fMk4ID == val){return 0;}
-//     }
-// 
-//     if(fStationCode != "")//seletion by 2-char station code
-//     {
-//         in->Retrieve(fRemStationKey, val);
-//         if(fStationCode == val){return 1;}
-//         in->Retrieve(fRefStationKey, val);
-//         if(fStationCode == val){return 0;}
-//     }
-// 
-//     msg_warn("calibration", "manual pcal, remote/reference station do not match selection."<< eom );
-//     return 2;
-// }
+void 
+MHO_PolProductSummation::PreMultiply(visibility_type* in)
+{
+    auto pp_ax = &(std::get<POLPROD_AXIS>(*in) );
 
-// bool
-// MHO_PolProductSummation::PolMatch(std::size_t station_idx, std::string& polprod)
-// {
-//     make_upper(polprod);
-//     return (fPol[0] == polprod[station_idx]);
-// }
+    for(std::size_t i=0; i < pp_ax->GetSize(); i++)
+    {
+        std::complex<double> prefac = GetPrefactor(pp_ax->at(i));
+        in->SubView(i) *= prefac;
+    }
+}
 
 
-bool
-MHO_PolProductSummation::InitializeInPlace(visibility_type* /*in*/){ return true;}
+std::complex<double> 
+MHO_PolProductSummation::GetPrefactor(std::string pp_label)
+{
+    //this needs to compute the pol-product dependent scaling/rotation factor 
+    //for the given pol products
+    //depending on the telescope mount type, this may have varied dependance 
+    //on (delta) parallactic angle
+    
+    #pragma message("FIXME TODO -- implement prefactor calculations for linear and circular pol-products (XX, YY, RR, etc).")
 
-bool
-MHO_PolProductSummation::InitializeOutOfPlace(const visibility_type* /*in*/, visibility_type* /*out*/){return true;}
+    //for now we just return 1 
+    return std::complex<double>(1.0, 0.0);
+}
 
+void 
+MHO_PolProductSummation::FixLabels(visibility_type* in)
+{
+    ( &(std::get<POLPROD_AXIS>(*in)) )->at(0) = fSummedPolProdLabel;
+}
 
 }//end of namespace
