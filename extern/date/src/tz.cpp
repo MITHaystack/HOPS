e6dd3e058f7c4d308374bda18501e62b76c9d88f
John Barrett <barrettj@mit.edu>
2024-01-18 11:35:04 -0500

    update git submodules

--- extern/date/src/tz.cpp
+++ extern/date/src/tz.cpp
@@ -2820,6 +2820,7 @@ find_read_and_leap_seconds()
         }
         return leap_seconds;
     }
+#if !MISSING_LEAP_SECONDS
     in.clear();
     in.open(get_tz_dir() + std::string(1, folder_delimiter) + "right/UTC",
                      std::ios_base::binary);
@@ -2834,6 +2835,7 @@ find_read_and_leap_seconds()
     {
         return load_just_leaps(in);
     }
+#endif
     return {};
 }
 
@@ -3659,6 +3661,10 @@ init_tzdb()
                 else if (line[0] == '\t' && continue_zone)
                 {
                     db->zones.back().add(line);
+                }
+                else if (word.size() > 0 && word[0] == '#')
+                {
+                    continue;
                 }
                 else
                 {
