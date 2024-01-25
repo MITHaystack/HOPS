902023db0fefb8a2955b41c55ef7bb47b52189cc
John Barrett <barrettj@mit.edu>
2024-01-24 21:04:04 -0500

    fix some plot formating

--- source/python_src/hops_visualization/fourfit_plot.py
+++ source/python_src/hops_visualization/fourfit_plot.py
@@ -429,13 +429,13 @@ def make_info_text_box(plot_dict):
         str(np.round(float(plot_dict['ResPhase']),1)) + '\n' + \
         str(np.round(float(plot_dict['PFD']),2)) + '\n' + \
         '\n' + \
-        str(np.round(float(plot_dict['ResidSbd(us)']),6)) + '\n' + \
-        str(np.round(float(plot_dict['ResidMbd(us)']),6))  + '\n' + \
+        str( format( float(plot_dict['ResidSbd(us)']), '.6f') ) + '\n' + \
+        str( format( float(plot_dict['ResidMbd(us)']), '.6f') )  + '\n' + \
         '\n' + \
-        str(np.round(float(plot_dict['FringeRate(Hz)']),6)) + '\n' + \
+        str( format( float(plot_dict['FringeRate(Hz)']), '.6f') ) + '\n' + \
         plot_dict['IonTEC(TEC)'] + '\n\n' + \
-        str(np.round(float(plot_dict['RefFreq(MHz)']),4)) + '\n' + \
-        str(np.round(float(plot_dict['AP(sec)']),3)) + '\n' + \
+        str( format( float(plot_dict['RefFreq(MHz)']), '.4f') ) + '\n' + \
+        str( format( float(plot_dict['AP(sec)']), '.3f') ) + '\n' + \
         plot_dict['ExperName'] + '\n' + \
         str( plot_dict['ExperNum'] ) + '\n' + \
         plot_dict['YearDOY'] + '\n' + \
