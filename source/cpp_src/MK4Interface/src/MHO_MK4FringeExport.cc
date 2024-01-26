859be0bb6da288e5024c91690b87c68128883afc
John Barrett <barrettj@mit.edu>
2024-01-25 21:05:22 -0500

    wip export

---
+++ source/cpp_src/MK4Interface/src/MHO_MK4FringeExport.cc
@@ -0,0 +1,97 @@
+#include "MHO_MK4FringeExport.hh"
+
+#include "MHO_MultiTypeMap.hh"
+#include <array>
+
+namespace hops
+{
+
+
+MHO_MK4FringeExport::MHO_MK4FringeExport()
+{
+
+}
+
+MHO_MK4FringeExport::~MHO_MK4FringeExport()
+{
+
+}
+
+
+void
+MHO_MK4FringeExport::ExportFringeFile()
+{
+
+}
+
+int MHO_MK4FringeExport::fill_200( struct type_200 *t200)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_201( struct type_201 *t201)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_202( struct type_202 *t202)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_203( struct type_203 *t203)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_204( struct type_204 *t204)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_205( struct type_203 *t203, struct type_205 *t205)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_206( struct type_206 *t206)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_207( struct type_207 *t207)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_208( struct type_202 *t202, struct type_208 *t208)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_210( struct type_210 *t210)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_212( int fr, struct type_212 *t212)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_222( struct type_222 **t222)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_230( int fr, int ap, struct type_230 *t230)
+{
+
+}
+
+int MHO_MK4FringeExport::fill_fringe_info(char *filename)
+{
+
+}
+
+}//end of namespace
