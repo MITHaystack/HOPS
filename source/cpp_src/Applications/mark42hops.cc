a09b47a53e5e9d4e2f74c160fdae0bd74541e66c
John Barrett <barrettj@mit.edu>
2024-02-16 14:11:43 -0500

    adapt mark42hops cli interface

--- source/cpp_src/Applications/mark42hops.cc
+++ source/cpp_src/Applications/mark42hops.cc
@@ -1,4 +1,5 @@
-#include <getopt.h>
+//option parsing and help text library
+#include "CLI11.hpp"
 
 #include "MHO_MK4ScanConverter.hh"
 
@@ -6,53 +7,41 @@ using namespace hops;
 
 int main(int argc, char** argv)
 {
-    std::string usage = "mark42hops -i <input_directory> -o <output_directory>";
+    std::string in_dir = "";
+    std::string out_dir = "";
+    int message_level = 0;
 
+    CLI::App app{"mark42hops"};
+
+    app.add_option("input_dir,-i,--input-dir", in_dir, "name of the input directory")->required();
+    app.add_option("output_dir,-o,--output-dir", out_dir, "name of the output directory, if not passed the output files will be written out to the input directory");
+    app.add_option("-m,--message-level", message_level, "message level to be used, range: -2 (debug) to 5 (silent)");
+
+    CLI11_PARSE(app, argc, argv);
+
+    //clamp message level
+    if(message_level > 5){message_level = 5;}
+    if(message_level < -2){message_level = -2;}
     MHO_Message::GetInstance().AcceptAllKeys();
-    MHO_Message::GetInstance().SetMessageLevel(eStatus);
+    MHO_Message::GetInstance().SetLegacyMessageLevel(message_level);
 
-    std::string in_dir;
-    std::string out_dir;
-
-    static struct option longOptions[] = {{"help", no_argument, 0, 'h'},
-                                          {"input_directory", required_argument, 0, 'i'},
-                                          {"output_directory", required_argument, 0, 'o'}};
-
-    static const char* optString = "hi:o:";
-
-    while(true)
+    if(in_dir == "" )
     {
-        char optId = getopt_long(argc, argv, optString, longOptions, NULL);
-        if (optId == -1)
-            break;
-        switch(optId)
-        {
-            case ('h'):  // help
-                std::cout << usage << std::endl;
-                return 0;
-            case ('i'):
-                in_dir = std::string(optarg);
-                break;
-            case ('o'):
-                out_dir = std::string(optarg);
-                break;
-            default:
-                std::cout << usage << std::endl;
-                return 1;
-        }
-    }
-
-    if(in_dir == "" || out_dir == "")
-    {
-        msg_fatal("main", "must specify input and output directories" << eom );
+        msg_fatal("main", "must specify input directory" << eom );
         return 1;
     }
-    
+
+    if(out_dir == "")
+    {
+        out_dir = in_dir;
+        msg_status("main", "no output directory passed, writing output files to input directory" << eom );
+    }
+
     int dir_type = MHO_MK4ScanConverter::DetermineDirectoryType(in_dir);
 
     if(dir_type == MK4_UNKNOWNDIR)
     {
-        msg_fatal("main", "could not determine the directory type of: "<<in_dir << 
+        msg_fatal("main", "could not determine the directory type of: "<<in_dir <<
             " it is not a mk4 experiment or scan directory." << eom);
         return 1;
     }
@@ -63,7 +52,7 @@ int main(int argc, char** argv)
         MHO_MK4ScanConverter::ProcessScan(in_dir, out_dir);
         return 0;
     }
-    
+
     if(dir_type == MK4_EXPDIR)
     {
         //directory interface
@@ -82,7 +71,7 @@ int main(int argc, char** argv)
         {
             dirInterface.CreateDirectory(output_dir);
         }
-        
+
         //TODO we may want to sanitize the sub directory list (to make sure we only have scan directories for sure)
 
         std::vector< std::string > scanOutputDirs;
@@ -104,6 +93,6 @@ int main(int argc, char** argv)
 
         return 0;
     }
-    
+
     return 0;
 }
