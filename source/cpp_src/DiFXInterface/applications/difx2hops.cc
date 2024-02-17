d0a53cec6cea87bb09a271a06da35eb83ec06293
John Barrett <barrettj@mit.edu>
2024-02-16 21:31:16 -0500

    update difx2hops cli

--- source/cpp_src/DiFXInterface/applications/difx2hops.cc
+++ source/cpp_src/DiFXInterface/applications/difx2hops.cc
@@ -23,73 +23,43 @@
 #include "MHO_DirectoryInterface.hh"
 #include "MHO_DiFXInterface.hh"
 
+//option parsing and help text library
+#include "CLI11.hpp"
+
 using namespace hops;
 
 int main(int argc, char** argv)
 {
-    std::string usage = "difx2hops -e <exp. number> -i <input_directory> -c <station_codes_file> -o <output_directory>";
 
-    //MHO_Message::GetInstance().AcceptAllKeys();
-    MHO_Message::GetInstance().SetMessageLevel(eDebug);
-    MHO_Message::GetInstance().AddKey("difx_interface");
-
-    std::string input_dir = "./";
-    std::string output_dir = "./";
-    bool output_dir_specified = false;
+    std::string input_dir = "";
+    std::string output_dir = "";
+    int message_level = 0;
     std::string station_codes_file = "";
     int exper_num = 1234;
-    bool normalize = true;
+    bool raw_mode = false;
     bool preserve = false;
 
-    static struct option longOptions[] = {{"help", no_argument, 0, 'h'},
-                                          {"input_directory", required_argument, 0, 'i'},
-                                          {"station_codes", required_argument, 0, 'c'},
-                                          {"experiment_number", required_argument, 0, 'e'},
-                                          {"output_directory", required_argument, 0, 'o'},
-                                          {"raw", no_argument, 0, 'r'}, //turns on 'raw' mode, no normalization done
-                                          {"preserve", no_argument, 0, 'p'} //uses original difx scan names to name the scans (otherwise uses DOY-HHMM)
-                                        };
+    CLI::App app{"difx2hops"};
 
-    static const char* optString = "hi:c:e:o:rp";
+    app.add_option("input_dir,-i,--input-dir", input_dir, "name of the input directory (difx) file to be converted")->required();
+    app.add_option("output_dir,-o,--output-dir", output_dir, "name of the output directory where results (hops) will be written (if unspecified defaults to <input_dir>/<experiment>)");
+    app.add_option("-m,--message-level", message_level, "message level to be used, range: -2 (debug) to 5 (silent)");
+    app.add_option("-s,--scode", station_codes_file, "name of file containing 2-char station codes to 1-char mk4id");
+    app.add_option("-e,--exp-num", exper_num, "experiment identification number");
+    app.add_option("-r,--raw-mode", raw_mode, "enable raw mode (do not apply auto-corr normalization)");
+    app.add_option("-p,--preserve-difx-names", preserve, "use original difx scan names to name each scans (otherwise uses DOY-HHMM");
 
-    while(true)
-    {
-        char optId = getopt_long(argc, argv, optString, longOptions, NULL);
-        if (optId == -1)
-            break;
-        switch(optId)
-        {
-            case ('h'):  // help
-                std::cout << usage << std::endl;
-                return 0;
-            case ('i'):
-                input_dir = std::string(optarg);
-                break;
-            case ('c'):
-                station_codes_file = std::string(optarg);
-                break;
-            case ('e'):
-                exper_num = std::atoi(optarg);
-                break;
-            case ('o'):
-                output_dir = std::string(optarg);
-                output_dir_specified = true;
-                break;
-            case ('r'):
-                normalize = false;
-                break;
-            case ('p'):
-                preserve = true;
-                break;
-            default:
-                std::cout << usage << std::endl;
-                return 1;
-        }
-    }
+    CLI11_PARSE(app, argc, argv);
 
-    //if not output directory was specified assume we are going to dump the
-    //converted data into <input_directory>/exper_num
-    if(!output_dir_specified)
+    //clamp message level
+    if(message_level > 5){message_level = 5;}
+    if(message_level < -2){message_level = -2;}
+    MHO_Message::GetInstance().AcceptAllKeys();
+    MHO_Message::GetInstance().SetLegacyMessageLevel(message_level);
+
+    //if no output directory was specified assume we are going to dump the
+    //converted data into <input_directory>/<exper_num>
+    if(output_dir == "")
     {
         std::stringstream ss;
         ss << exper_num;
@@ -117,7 +87,7 @@ int main(int argc, char** argv)
     difxInterface.SetStationCodes(&stcode_map);
     difxInterface.SetExperimentNumber(exper_num);
     difxInterface.SetNormalizeFalse();
-    if(normalize){difxInterface.SetNormalizeTrue();}
+    if(!raw_mode){difxInterface.SetNormalizeTrue();}
     if(preserve){difxInterface.SetPreserveDiFXScanNamesTrue();}
 
     difxInterface.Initialize();
