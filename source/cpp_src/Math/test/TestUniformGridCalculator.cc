107ba22ed783ad0500e3f1cd6676b0c04fae4416
John Barrett <barrettj@mit.edu>
2024-04-29 11:18:04 -0400

    cleanup

--- source/cpp_src/Math/test/TestUniformGridCalculator.cc
+++ source/cpp_src/Math/test/TestUniformGridCalculator.cc
@@ -200,54 +200,6 @@ int main(int argc, char** argv)
         13944.4
     };
 
-    //
-    // A
-    //         0    1    2    6    9    11    13    14
-    //
-    //         i    j    k    l    m    n    o    p
-    // B
-    //     vo Band C    111    112    113    117    120    122    124    125
-    //
-    //         q    r    s    t    u    v    w    x
-    // C
-    //     covers X-band    178    179    180    184    187    189    191    192
-    //
-    //         y    z    A    B    C    D    E    F
-    // D
-
-
-//     upper edge of channel
-// vr2304 (0)    15    14    13    9    6    4    2    1
-// UDC(MHz)    a    b    c    d    e    f    g    h
-// A    6195.1    2840.4    2872.4    2904.4    3032.4    3128.4    3192.4    3256.4    3288.4
-// 0    1    2    6    9    11    13    14
-//
-// i    j    k    l    m    n    o    p
-// B    7083.1    6392.4    6424.4    6456.4    6584.4    6680.4    6744.4    6808.4    6840.4
-// vo Band C    111    112    113    117    120    122    124    125
-//
-// q    r    s    t    u    v    w    x
-// C    7619.1    8536.4    8568.4    8600.4    8728.4    8824.4    8888.4    8952.4    8984.4
-// covers X-band    178    179    180    184    187    189    191    192
-//
-// y    z    A    B    C    D    E    F
-// D    8491.1    12024.4    12056.4    12088.4    12216.4    12312.4    12376.4    12440.4    12472.4
-// 287    288    289    293    296    298    300    301
-    // // pts.push_back(1.0);
-    // // pts.push_back(2.0);
-    // pts.push_back(3.0);
-    // pts.push_back(5.0);
-    // pts.push_back(7.0);
-    // // pts.push_back(11.32332342);
-    // pts.push_back(13.0);
-    // pts.push_back(17.0);
-    // pts.push_back(17.1);
-    // pts.push_back(17.33);
-    // pts.push_back(19.0);
-    // pts.push_back(71.0);
-    // pts.push_back(72.0);
-
-
 
     MHO_UniformGridPointsCalculator grid_calc;
     grid_calc.SetPoints(pts_mode0);
