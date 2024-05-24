3513a75a9c4f91ee6e95a69ba07ace4406889160
John Barrett <barrettj@mit.edu>
2024-05-24 11:04:07 -0400

    cleanup up visib correction...need to understand 180deg phase flip

--- source/cpp_src/Fringe/src/MHO_ComputePlotData.cc
+++ source/cpp_src/Fringe/src/MHO_ComputePlotData.cc
@@ -316,184 +316,17 @@ MHO_ComputePlotData::calc_segs()
 
 }
 
-
-// 
-// visibility_type*
-// MHO_ComputePlotData::calc_corrected_vis()
-// {
-//     //clone the SBD array 
-//     visibility_type* corrected_vis = fSBDArray->Clone();
-// 
-//     std::size_t POLPROD = 0;
-//     std::size_t nchan = corrected_vis->GetDimension(CHANNEL_AXIS);
-//     std::size_t nap = corrected_vis->GetDimension(TIME_AXIS);
-//     std::size_t nbins = corrected_vis->GetDimension(FREQ_AXIS);
-// 
-//     auto chan_ax = &( std::get<CHANNEL_AXIS>(*corrected_vis) );
-//     auto ap_ax = &(std::get<TIME_AXIS>(*corrected_vis));
-//     auto lag_ax = &(std::get<FREQ_AXIS>(*corrected_vis));
-//     double ap_delta = ap_ax->at(1) - ap_ax->at(0);
-//     double frt_offset = fParamStore->GetAs<double>("/config/frt_offset");
-// 
-//     //grab the fourfit channel names
-//     std::string chan_label_key = "channel_label";
-//     std::vector< std::string > channel_labels;
-//     for(std::size_t ch=0; ch < chan_ax->GetSize(); ch++)
-//     {
-//         std::string ch_label;
-//         bool key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, chan_label_key, ch_label);
-//         if(key_present){channel_labels.push_back(ch_label);}
-//     }
-// 
-//     std::string sidebandlabelkey = "net_sideband";
-//     for(std::size_t ap=0; ap < nap; ap++)
-//     {
-//         std::complex<double> sum = 0; //sum over all channels
-//         double sumwt = 0.0;
-//         double tdelta = (ap_ax->at(ap) + ap_delta/2.0) - frt_offset; //need time difference from the f.r.t?
-//         for(std::size_t ch=0; ch < nchan; ch++)
-//         {
-//             double freq = (*chan_ax)(ch);//sky freq of this channel
-//             std::string net_sideband = "?";
-//             bool key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, sidebandlabelkey, net_sideband);
-//             if(!key_present){msg_error("fringe", "missing net_sideband label for channel "<< ch << "." << eom);}
-// 
-//             fRot.SetSideband(0); //DSB
-//             if(net_sideband == "U"){fRot.SetSideband(1);}
-//             if(net_sideband == "L"){fRot.SetSideband(-1);}
-// 
-//             std::complex<double> vr = fRot.vrot(tdelta, freq, fRefFreq, fDelayRate, fMBDelay);
-//             for(std::size_t lag=0; lag < nbins; lag++)
-//             {
-//                 //apply the rotation....hey wait a minute, what about the frequency/delay change across the channel??!
-//                 std::complex<double> vis = (*corrected_vis)(POLPROD, ch, ap, lag);
-//                 (*corrected_vis)(POLPROD, ch, ap, lag) = vis*vr;
-//             }
-//         }
-//     }
-// 
-//     FFT_ENGINE_TYPE fftEngine;
-// 
-//     fftEngine.SetArgs(corrected_vis);
-//     fftEngine.DeselectAllAxes();
-//     fftEngine.SelectAxis(FREQ_AXIS); //only perform padded fft on frequency (to lag) axis
-//     fftEngine.SetBackward();//backward DFT
-//     fftEngine.Initialize();
-// 
-//     //now we need to appy an FFT to take us from single-band-delay back to frequency space
-//     bool status = fftEngine.Execute();
-//     if(!status){msg_error("fringe", "Could not execute FFT in MHO_ComputePlotData" << eom);}
-// 
-//     //normalize the array
-//     double norm =  1.0/(double)nbins;
-//     *(corrected_vis) *= norm;
-// 
-//     //first do a cyclic rotation to fix things after the FFT
-//     MHO_CyclicRotator<visibility_type> cyclicRotator;
-//     cyclicRotator.SetOffset(FREQ_AXIS, -nbins/4);
-//     cyclicRotator.SetArgs(corrected_vis);
-//     status = cyclicRotator.Initialize();
-//     status = cyclicRotator.Execute();
-// 
-//     if(!status){msg_error("fringe", "Could not execute cyclic rotation in MHO_ComputePlotData." << eom);}
-// 
-//     //TODO...we need select-repack the data (and re-label the FREQ_AXIS too)!!
-//     MHO_SelectRepack<visibility_type> repacker;
-//     std::vector< std::size_t > selected_freq;
-//     for(std::size_t i=0; i<nbins/4; i++)
-//     {
-//         selected_freq.push_back(i);
-//     }
-//     repacker.SelectAxisItems(FREQ_AXIS,selected_freq);
-//     repacker.SetArgs(corrected_vis);
-//     repacker.Initialize();
-//     repacker.Execute();
-// 
-//     return corrected_vis;
-// }
-
-// 
-// 
-// visibility_type*
-// MHO_ComputePlotData::calc_corrected_vis()
-// {
-//     //clone the SBD array 
-//     visibility_type* corrected_vis = fVisibilities->Clone();
-// 
-//     std::size_t POLPROD = 0;
-//     std::size_t nchan = corrected_vis->GetDimension(CHANNEL_AXIS);
-//     std::size_t nap = corrected_vis->GetDimension(TIME_AXIS);
-//     std::size_t nbins = corrected_vis->GetDimension(FREQ_AXIS);
-// 
-//     auto chan_ax = &( std::get<CHANNEL_AXIS>(*corrected_vis) );
-//     auto ap_ax = &(std::get<TIME_AXIS>(*corrected_vis));
-//     auto lag_ax = &(std::get<FREQ_AXIS>(*corrected_vis));
-//     double ap_delta = ap_ax->at(1) - ap_ax->at(0);
-//     double frt_offset = fParamStore->GetAs<double>("/config/frt_offset");
-// 
-//     //grab the fourfit channel names
-//     std::string chan_label_key = "channel_label";
-//     std::vector< std::string > channel_labels;
-//     for(std::size_t ch=0; ch < chan_ax->GetSize(); ch++)
-//     {
-//         std::string ch_label;
-//         bool key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, chan_label_key, ch_label);
-//         if(key_present){channel_labels.push_back(ch_label);}
-//     }
-// 
-//     std::cout<<"sbd delay = "<<fSBDelay<<std::endl;
-// 
-//     std::string sidebandlabelkey = "net_sideband";
-//     for(std::size_t ap=0; ap < nap; ap++)
-//     {
-//         std::complex<double> sum = 0; //sum over all channels
-//         double sumwt = 0.0;
-//         double tdelta = (ap_ax->at(ap) + ap_delta/2.0) - frt_offset; //need time difference from the f.r.t?
-//         for(std::size_t ch=0; ch < nchan; ch++)
-//         {
-//             double freq = (*chan_ax)(ch);//sky freq of this channel
-//             std::string net_sideband = "?";
-//             bool key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, sidebandlabelkey, net_sideband);
-//             if(!key_present){msg_error("fringe", "missing net_sideband label for channel "<< ch << "." << eom);}
-// 
-//             fRot.SetSideband(0); //DSB
-//             if(net_sideband == "U"){fRot.SetSideband(1);}
-//             if(net_sideband == "L"){fRot.SetSideband(-1);}
-// 
-//             std::complex<double> imag_unit(0,1);
-//             for(std::size_t lag=0; lag < nbins; lag++)
-//             {
-//                 //apply the rotation....hey wait a minute, what about the frequency change across the channel??!
-//                 double fr = freq + (*lag_ax)[lag];
-//                 std::complex<double> vr = fRot.vrot(tdelta, fr, fRefFreq, fDelayRate, fMBDelay);
-//                 // std::cout<<"vrot = @"<<ap<<", "<<lag<<" = "<<std::arg(vr)*(180/M_PI)<<std::endl;
-//                 // double theta = (*lag_ax)[lag] * fSBDelay;
-//                 // std::cout<<"THETA @ lag "<<lag<<" = "<<theta<<std::endl;
-//                 // std::complex<double> sbd_rot = std::exp(-2.0 * M_PI * imag_unit * theta); 
-// 
-//                 std::complex<double> vis =  (*corrected_vis)(POLPROD, ch, ap, lag);
-//                 (*corrected_vis)(POLPROD, ch, ap, lag) = vr*vis;//*sbd_rot;
-//             }
-//         }
-//     }
-//     return corrected_vis;
-// }
-
-
 void
 MHO_ComputePlotData::correct_vis()
 {
-    //clone the SBD array 
-    visibility_type* corrected_vis = fVisibilities;
-
     std::size_t POLPROD = 0;
-    std::size_t nchan = corrected_vis->GetDimension(CHANNEL_AXIS);
-    std::size_t nap = corrected_vis->GetDimension(TIME_AXIS);
-    std::size_t nbins = corrected_vis->GetDimension(FREQ_AXIS);
+    std::size_t nchan = fVisibilities->GetDimension(CHANNEL_AXIS);
+    std::size_t nap = fVisibilities->GetDimension(TIME_AXIS);
+    std::size_t nbins = fVisibilities->GetDimension(FREQ_AXIS);
 
-    auto chan_ax = &( std::get<CHANNEL_AXIS>(*corrected_vis) );
-    auto ap_ax = &(std::get<TIME_AXIS>(*corrected_vis));
-    auto lag_ax = &(std::get<FREQ_AXIS>(*corrected_vis));
+    auto chan_ax = &( std::get<CHANNEL_AXIS>(*fVisibilities) );
+    auto ap_ax = &(std::get<TIME_AXIS>(*fVisibilities));
+    auto freq_ax = &(std::get<FREQ_AXIS>(*fVisibilities));
     double ap_delta = ap_ax->at(1) - ap_ax->at(0);
     double frt_offset = fParamStore->GetAs<double>("/config/frt_offset");
 
@@ -510,6 +343,7 @@ MHO_ComputePlotData::correct_vis()
     std::cout<<"sbd delay = "<<fSBDelay<<std::endl;
 
     std::string sidebandlabelkey = "net_sideband";
+    std::string bandwidthlabelkey = "bandwidth";
     for(std::size_t ap=0; ap < nap; ap++)
     {
         std::complex<double> sum = 0; //sum over all channels
@@ -519,25 +353,42 @@ MHO_ComputePlotData::correct_vis()
         {
             double freq = (*chan_ax)(ch);//sky freq of this channel
             std::string net_sideband = "?";
+
             bool key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, sidebandlabelkey, net_sideband);
             if(!key_present){msg_error("fringe", "missing net_sideband label for channel "<< ch << "." << eom);}
 
+            double bw = 0;
+            key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, bandwidthlabelkey, bw);
+            if(!key_present){msg_error("fringe", "missing bandwidth label for channel "<< ch << "." << eom);}
+
+            double sb_sign = 0.0;
             fRot.SetSideband(0); //DSB
-            if(net_sideband == "U"){fRot.SetSideband(1);}
-            if(net_sideband == "L"){fRot.SetSideband(-1);}
+            if(net_sideband == "U")
+            {
+                fRot.SetSideband(1);
+                sb_sign = 1.0;
+            }
+
+            if(net_sideband == "L")
+            {
+                fRot.SetSideband(-1);
+                sb_sign = -1.0;
+            }
 
             std::complex<double> imag_unit(0,1);
+            //calculate the per-channel phase rotation due to MBD and delay_rate
             std::complex<double> vr = fRot.vrot(tdelta, freq, fRefFreq, fDelayRate, fMBDelay);
-            for(std::size_t lag=0; lag < nbins; lag++)
+            //now apply a rotation for the SBD at each spectral point
+            for(std::size_t sp=0; sp < nbins; sp++)
             {
                 //apply the rotation....hey wait a minute, what about the frequency change across the channel??!
-                //double fr = freq - (*lag_ax)[lag];
+                //double fr = freq - (*freq_ax)[lag];
                 // std::cout<<"vrot = @"<<ap<<", "<<lag<<" = "<<std::arg(vr)*(180/M_PI)<<std::endl;
-                double theta = -1.0*( (*lag_ax)[lag] - 16.0 ) * fSBDelay;
+                double theta = sb_sign*( (*freq_ax)[sp] - bw/2.0 ) * fSBDelay;
                 // std::cout<<"THETA @ lag "<<lag<<" = "<<theta<<std::endl;
-                std::complex<double> sbd_rot = std::exp(-2.0 * M_PI * imag_unit * theta); 
-                std::complex<double> vis =  (*corrected_vis)(POLPROD, ch, ap, lag);
-                (*corrected_vis)(POLPROD, ch, ap, lag) = std::conj(sbd_rot)*std::conj(vr)*vis;//*sbd_rot;
+                std::complex<double> sbd_rot = std::exp(-2.0 * M_PI * imag_unit * theta);
+                std::complex<double> vis = (*fVisibilities)(POLPROD, ch, ap, sp);
+                (*fVisibilities)(POLPROD, ch, ap, sp) = std::conj(sbd_rot)*std::conj(vr)*vis;
             }
         }
     }
@@ -965,7 +816,7 @@ MHO_ComputePlotData::DumpInfoToJSON(mho_json& plot_dict)
     auto phasors_clone = phasors.Clone();
     bool ok = fContainerStore->AddObject(phasors_clone);
     ok = fContainerStore->SetShortName(phasors_clone->GetObjectUUID(), "phasors");
-    
+
     // auto cvis = calc_corrected_vis();
     // cvis->CopyTags(*fSBDArray);
     // cvis->Insert("name", "corrected_visibilities");
@@ -1232,7 +1083,7 @@ MHO_ComputePlotData::DumpInfoToJSON(mho_json& plot_dict)
     std::string errcode = calc_error_code(plot_dict);
     plot_dict["extra"]["error_code"] = errcode;
     fParamStore->Set("/fringe/error_code", errcode);
-    
+
     //last thing is apply residual delay/delay rate correction to visibilities
     correct_vis();
 
