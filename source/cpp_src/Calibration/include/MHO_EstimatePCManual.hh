8d2d42362ba6170e02ca3cfd5ac02c5c4e00df8b
John Barrett <barrettj@mit.edu>
2024-05-29 16:05:35 -0400

    starting work on porting est_pc_manual

---
+++ source/cpp_src/Calibration/include/MHO_EstimatePCManual.hh
@@ -0,0 +1,85 @@
+#ifndef MHO_EstimatePCManual_HH__
+#define MHO_EstimatePCManual_HH__
+
+
+#include <cmath>
+#include <complex>
+#include <vector>
+#include <map>
+#include <cctype>
+
+#include "MHO_Message.hh"
+#include "MHO_Constants.hh"
+
+#include "MHO_TableContainer.hh"
+#include "MHO_ContainerDefinitions.hh"
+#include "MHO_InspectingOperator.hh"
+
+
+namespace hops
+{
+
+/*!
+*@file MHO_EstimatePCManual.hh
+*@class MHO_EstimatePCManual
+*@author J. Barrett - barrettj@mit.edu
+*@date Wed May 29 03:40:50 PM EDT 2024
+*@brief
+*/
+
+
+class MHO_EstimatePCManual: public MHO_InspectingOperator< visibility_type >
+{
+    public:
+
+        MHO_EstimatePCManual();
+        virtual ~MHO_EstimatePCManual();
+
+        // void SetStation(std::string station){fStationCode = station;}; //2-char station code
+        // void SetStationMk4ID(std::string station_id){fMk4ID = station_id;} //1-char mk4id
+        // void SetPolarization(const std::string& pol){fPol = pol; make_upper(fPol);};
+        // void SetPCPhaseOffset(const double& pc_phase_offset){fPhaseOffset = pc_phase_offset;}
+
+    protected:
+
+        virtual bool InitializeImpl(const visibility_type* in) override { return true; };
+        
+        virtual bool ExecuteImpl(const visibility_type* in) override;
+
+    private:
+
+        // std::size_t DetermineStationIndex(const visibility_type* in);
+        // bool PolMatch(std::size_t station_idx, std::string& polprod);
+        // 
+        // //constants
+        // std::complex<double> fImagUnit;
+        // double fDegToRad;
+        // 
+        // //selection
+        // std::string fStationCode;
+        // std::string fMk4ID;
+        // std::string fPol;
+        // 
+        // //pc rotation
+        // double fPhaseOffset;
+        // 
+        // //keys for tag retrieval
+        // std::string fStationKey;
+        // std::string fRemStationKey;
+        // std::string fRefStationKey;
+        // std::string fRemStationMk4IDKey;
+        // std::string fRefStationMk4IDKey;
+        // std::string fChannelLabelKey;
+
+        // //minor helper function to make sure all strings are compared as upper-case only
+        // void make_upper(std::string& s){ for(char& c : s){c = toupper(c); };
+    }
+
+
+};
+
+
+}
+
+
+#endif /*! end of include guard: MHO_EstimatePCManual */
