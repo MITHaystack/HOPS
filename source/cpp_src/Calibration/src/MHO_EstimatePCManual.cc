c1c84a1a34c3ff66af768351078a49fc8ef68285
John Barrett <barrettj@mit.edu>
2024-05-29 16:44:28 -0400

    cosmetic adj

--- source/cpp_src/Calibration/src/MHO_EstimatePCManual.cc
+++ source/cpp_src/Calibration/src/MHO_EstimatePCManual.cc
@@ -67,8 +67,7 @@ static int pol_letter(int pol, int rr)
 }
 
 /* generate information about where the results came from */
-static void masthead(int mode, char *rf,
-    struct type_pass *pass, int first_ch, int final_ch)
+static void masthead(int mode, char *rf, struct type_pass *pass, int first_ch, int final_ch)
 {
     // msg("rf:  %s", 3, rf);
     // msg("cf:  %s", 3, control_filename);
@@ -89,17 +88,16 @@ static void masthead(int mode, char *rf,
 static double pbranch(double phase)
 {
     phase = ( fmod(phase, 360.0) );
-    if (phase < -180.0) phase += 360.0;
-    if (phase >  180.0) phase -= 360.0;
-    return(phase);
+    if (phase < -180.0){ phase += 360.0; }
+    if (phase >  180.0){ phase -= 360.0; }
+    return phase;
 }
 
 /*
  * This routine calculates adjustments to channel phases
  * designed to remove the multiband delay (however anchored).
  */
-static void est_phases(struct type_pass *pass, int first, int final,
-    int rr, int keep)
+static void est_phases(struct type_pass *pass, int first, int final, int rr, int keep)
 {
     static char buf[720], tmp[80], *pb;
     int ch, ss, pol, nd;
@@ -111,7 +109,8 @@ static void est_phases(struct type_pass *pass, int first, int final,
     // msg("*est: phases on %s station", 1, rr ? "ref" : "rem");
 
     /* support for bias operation */
-    if (keep) {
+    if (keep) 
+    {
         phase_bias = (epb) ? atof(epb) : 0.0;
         // msg("*est: phase bias %f (mod res phase is %f)", 3,
         //     phase_bias, status.coh_avg_phase * (180.0 / M_PI));
@@ -128,7 +127,8 @@ static void est_phases(struct type_pass *pass, int first, int final,
     *pb = 0;
     // msg(buf, 3);
 
-    for (buf[nd = ss = 0] = 0, ch = first; ch <= final; ch++) {
+    for (buf[nd = ss = 0] = 0, ch = first; ch <= final; ch++) 
+    {
         /* assume it is all usb or lsb for this estimate */
         sbmult = (status.total_usb_frac > 0) ? 1.0 : -1.0;
         est_phase = status.pc_phase[ch][0][stnpol[0][pass->pol]]
@@ -164,7 +164,8 @@ static void est_phases(struct type_pass *pass, int first, int final,
         strncat(buf, tmp, sizeof(buf)-1);
 
         /* eight phases per line for a line length of 73 */
-        if (++ss == 8) {
+        if (++ss == 8) 
+        {
             // msg(buf, 3);
             buf[ss = 0] = 0;
         }
@@ -190,8 +191,7 @@ static int sbd_cmp(const void *a, const void *b)
  *    0x10: use original SBD values
  *    0x20: use heuristics to discard outliers
  */
-static void adj_delays(double sbd[], double esd[], double delta_delay,
-    int first, int final, int rr, int how)
+static void adj_delays(double sbd[], double esd[], double delta_delay, int first, int final, int rr, int how)
 {
     static double cpy[MAXFREQ];
     double tol, medly, ave, tot = status.sbd_max * 1e3;
@@ -201,8 +201,10 @@ static void adj_delays(double sbd[], double esd[], double delta_delay,
     for (ch = first; ch <= final; ch++) esd[ch] = 0.0;
 
     /* for methods requiring a median value */
-    if ( how & 0x026 ) {
-        for (ch = first, ave=0.0; ch <= final; ch++) {
+    if ( how & 0x026 ) 
+    {
+        for (ch = first, ave=0.0; ch <= final; ch++) 
+        {
             cpy[ch] = sbd[ch];
             ave += sbd[ch];
         }
@@ -215,7 +217,8 @@ static void adj_delays(double sbd[], double esd[], double delta_delay,
     }
 
     /* heuristic is to replace outliers with the median delay */
-    if (how & 0x20) {
+    if (how & 0x20) 
+    {
         tol = fabs(cpy[med] - tot);
         // msg("*est: tolerance %.3f, retaining %.3f+/-%.3f",3,tol,medly,3*tol);
         for (ch = first; tol > 0 && ch <= final; ch++)
@@ -226,21 +229,32 @@ static void adj_delays(double sbd[], double esd[], double delta_delay,
         // msg("*est: revised average delay is %.3f",3,ave);
     }
 
-    if        (how & 0x02) {            /* use the median value */
+    if (how & 0x02) 
+    {            /* use the median value */
         // msg("*est: using median delay (mode %x)",3,how);
-        for (ch = first; ch <= final; ch++) esd[ch] = medly - delta_delay;
-    } else if (how & 0x04) {            /* compute and use average */
+        for (ch = first; ch <= final; ch++){ esd[ch] = medly - delta_delay; }
+    }
+    else if (how & 0x04) 
+    {            /* compute and use average */
         // msg("*est: using ave delay (mode %x)",3,how);
-        for (ch = first; ch <= final; ch++) esd[ch] = ave - delta_delay;
-    } else if (how & 0x08) {            /* use total SBD value */
+        for (ch = first; ch <= final; ch++){ esd[ch] = ave - delta_delay; }
+    }
+    else if (how & 0x08) 
+    {            /* use total SBD value */
         // msg("*est: using total SBD delay (mode %x)",3,how);
-        for (ch = first; ch <= final; ch++) esd[ch] = tot - delta_delay;
-    } else if (how & 0x10) {            /* use the measured values */
+        for (ch = first; ch <= final; ch++){ esd[ch] = tot - delta_delay; }
+    }
+    else if (how & 0x10) 
+    {            /* use the measured values */
         // msg("*est: using measured SBD delay (mode %x)",3,how);
-        for (ch = first; ch <= final; ch++) esd[ch] = sbd[ch] - delta_delay;
+        for (ch = first; ch <= final; ch++){ esd[ch] = sbd[ch] - delta_delay; }
     }
 
-    if (!rr) for (ch = first; ch <= final; ch++) esd[ch] = -esd[ch];
+    if (!rr) 
+    {
+        for (ch = first; ch <= final; ch++){ esd[ch] = -esd[ch]; }
+    }
+    
 }
 
 /*
@@ -248,8 +262,7 @@ static void adj_delays(double sbd[], double esd[], double delta_delay,
  *  designed to remove the sbd.  Several methods and options
  *  are available via the 'how' argument.  See adj_delays().
  */
-static void est_delays(struct type_pass *pass,
-    int first, int final, int rr, int how)
+static void est_delays(struct type_pass *pass, int first, int final, int rr, int how)
 {
     static char buf[720], tmp[80];
     static double sbd[MAXFREQ], rdy[MAXFREQ], esd[MAXFREQ];
@@ -263,25 +276,30 @@ static void est_delays(struct type_pass *pass,
 
     /* restrict operation to only one delay calculation */
     if ((((how & 0x02)>>1) + ((how & 0x04)>>2) +
-         ((how & 0x08)>>3) + ((how & 0x10)>>4)) > 1) {
+         ((how & 0x08)>>3) + ((how & 0x10)>>4)) > 1) 
+    {
         // msg("*est: too many delay modes selected: 0x%02x",3,how);
         return;
     }
 
     /* consider a delay correction due to mbd */
-    if (how & 0x100) {
+    if (how & 0x100) 
+    {
         //delta_delay = (param.mbd_anchor == MODEL)
         //            ? fringe.t208->resid_mbd
         //            : fringe.t208->resid_mbd - fringe.t208->resid_sbd;
         delta_delay = fringe.t208->resid_mbd;
         delta_delay *= ((epd) ? atof(epd) : 1.0) * 1000.0;
         // msg("*est: post-MDLY sbd adjustment %f ns", 3, delta_delay);
-    } else {
+    } 
+    else 
+    {
         delta_delay = 0.0;
     }
 
     /* build an array of per-channel sbd values */
-    for (ch = first; ch <= final; ch++) {
+    for (ch = first; ch <= final; ch++) 
+    {
         /* Cf. status.sbdbox[MAXFREQ] <=> status.sbd_max */
         sbd[ch] = (status.sbdbox[ch] - param.nlags - 1) * status.sbd_sep;
         sbd[ch] *= 1000.0;  /* us to ns */
@@ -307,19 +325,22 @@ static void est_delays(struct type_pass *pass,
     *pb = 0;
     // msg(buf, 3);
 
-    for (buf[nd = ss = 0] = 0, ch = first; ch <= final; ch++) {
+    for (buf[nd = ss = 0] = 0, ch = first; ch <= final; ch++) 
+    {
         esd[ch] += rdy[ch];     /* work relative to input value */
         if (fabs(esd[ch] - rdy[ch]) > 0.01) nd ++;
         snprintf(tmp, sizeof(tmp), " %+8.3f", esd[ch]);
         strncat(buf, tmp, sizeof(buf)-1);
   
         /* eight delays per line for a line length of 73 */
-        if (++ss == 8) {
+        if (++ss == 8) 
+        {
             // msg(buf, 3);
             buf[ss = 0] = 0;
         }
     }
-    if (buf[0]) msg(buf, 3);
+    
+    //if (buf[0]) msg(buf, 3);
     // msg("*est: delays %s (%d)", 2, nd ? "converging" : "converged", nd);
 }
 
@@ -374,14 +395,11 @@ void est_pc_manual(int mode, char *rootfile, struct type_pass *pass)
  */
 
 
-
-
 namespace hops
 {
 
 
 MHO_EstimatePCManual::MHO_EstimatePCManual(){};
-
 MHO_EstimatePCManual::~MHO_EstimatePCManual(){};
 
 
