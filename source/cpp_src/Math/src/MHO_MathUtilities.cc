2518bb3163db3ad47efd4f2ed58cd378b37416c7
John Barrett <barrettj@mit.edu>
2024-06-10 15:26:44 -0400

    wip...add function to determine overlap of 1d intervals (double)

--- source/cpp_src/Math/src/MHO_MathUtilities.cc
+++ source/cpp_src/Math/src/MHO_MathUtilities.cc
@@ -79,4 +79,99 @@ MHO_MathUtilities::angular_average(std::vector<double>& vec)
 }
 
 
+int
+MHO_MathUtilities::FindIntersection(double a, double b, double c, double d, double result[2])
+{
+    //looks for overlap between the intervals
+    //[a,b) and [c,d)
+    //although if a,b and c,d are the end-points of an intervals
+    //we do not explicitly assume they are ordered there
+
+    double arr[4];
+    int index[4];
+
+    arr[0] = a;
+    index[0] = 0;
+    arr[1] = b;
+    index[1] = 0;
+    arr[2] = c;
+    index[2] = 1;
+    arr[3] = d;
+    index[3] = 1;
+
+    if (arr[1] > arr[3]) {
+        std::swap(arr[1], arr[3]);
+        std::swap(index[1], index[3]);
+    };
+    if (arr[0] > arr[2]) {
+        std::swap(arr[0], arr[2]);
+        std::swap(index[0], index[2]);
+    };
+    if (arr[0] > arr[1]) {
+        std::swap(arr[0], arr[1]);
+        std::swap(index[0], index[1]);
+    };
+    if (arr[2] > arr[3]) {
+        std::swap(arr[2], arr[3]);
+        std::swap(index[2], index[3]);
+    };
+    if (arr[1] > arr[2]) {
+        std::swap(arr[1], arr[2]);
+        std::swap(index[1], index[2]);
+    };
+
+    //now the values in arr should be sorted in increasing order
+    //and the values in index should show which interval's end-points they belong to
+
+    //if the values in index have the form:
+    //0011 or 1100 then there is no overlap...although the end points may
+    //just touch
+
+    //if the values in the index have the form:
+    // 1001, 0110, 0101, or 1010 then there is overlap and the overlap interval
+    //is {arr[1], arr[2]}
+
+    int sum;
+    sum = index[0] + index[1];
+
+    if( (sum == 0) || (sum == 2) )
+    {
+        //there is no overlap, but we need to see if the end-points of the
+        //two intervals are the same number
+        if( arr[2] == arr[1] )
+        {
+            //endpoints are the same value
+            //call this an intersection of 1 point
+            result[0] = arr[1];
+            return 1;
+        }
+        else
+        {
+            //no intersection at all
+            return 0;
+        }
+    }
+    else
+    {
+        //there is overlap, but check how big the overlap interval is
+        if( arr[2] == arr[1] )
+        {
+            //the two overlapping points are the same value
+            //call this an intersection of 1 point
+            result[0] = arr[1];
+            return 1;
+        }
+        else
+        {
+            //overlap is larger than zero, return the interval of overlap
+            result[0] = arr[1];
+            result[1] = arr[2];
+            return 2;
+        }
+
+    }
+}
+
+
+
 }//end namespace
