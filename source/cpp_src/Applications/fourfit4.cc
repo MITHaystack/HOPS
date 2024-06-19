c68a45c656d4f349d9b08a09fb3d5cbc7cc09bee
John Barrett <barrettj@mit.edu>
2024-06-19 08:41:25 -0400

    cleanup, remove std::couts

--- source/cpp_src/Applications/fourfit4.cc
+++ source/cpp_src/Applications/fourfit4.cc
@@ -59,7 +59,7 @@ int main(int argc, char** argv)
     int parse_status = MHO_BasicFringeDataConfiguration::parse_fourfit_command_line(argc, argv, &cmdline_params );
     if(parse_status != 0){msg_fatal("main", "could not parse command line options." << eom); std::exit(1);}
 
-    //data-loop order is scans, then baselines, then fgroups, then pol-products
+    //pass search order is scans, then baselines, then fgroups, then pol-products
     std::vector< std::string > scans; //list of scan directories
     std::vector< std::pair< std::string, std::string > > baseline_files;
     std::vector< std::string > fgroups;
@@ -68,49 +68,50 @@ int main(int argc, char** argv)
     //determine which directories contain scans to process
     std::string initial_dir = cmdline_params.GetAs<std::string>("/cmdline/directory");
     MHO_BasicFringeDataConfiguration::determine_scans(initial_dir, scans);
-    cmdline_params.Dump(); //TODO REMOVE
 
-    //form all the data passes that must be processes
+    //form all the data passes that must be processed
     std::vector< mho_json > pass_vector;
     for(auto sc = scans.begin(); sc != scans.end(); sc++)
     {
         std::string scan_dir = *sc;
-        std::cout<<"scan directory = "<<scan_dir<<std::endl;
         std::string root_file = MHO_BasicFringeDataConfiguration::find_associated_root_file(scan_dir);
-        std::cout<<"root file = "<<root_file<<std::endl;
-        if(root_file == ""){continue;} //TODO FIXME get rid of this!!
-        std::string cmd_bl = cmdline_params.GetAs<std::string>("/cmdline/baseline"); //if not passed, will be "??"
-        MHO_BasicFringeDataConfiguration::determine_baselines(scan_dir, cmd_bl, baseline_files);
-
-        for(auto bl = baseline_files.begin(); bl != baseline_files.end(); bl++)
+        if(root_file != "")
         {
-            std::string baseline = bl->first;
-            std::string corFile = bl->second;
-            std::cout<<" baseline: "<<baseline<<std::endl;
-            std::string cmd_fg = cmdline_params.GetAs<std::string>("/cmdline/frequency_group"); //if not passed, this will be "?"
-            std::string cmd_pp = cmdline_params.GetAs<std::string>("/cmdline/polprod"); //if not passed, this will be "??"
-            MHO_BasicFringeDataConfiguration::determine_fgroups_polproducts(corFile, cmd_fg, cmd_pp, fgroups, polproducts);
-
-            for(auto fg = fgroups.begin(); fg != fgroups.end(); fg++)
+            std::string cmd_bl = cmdline_params.GetAs<std::string>("/cmdline/baseline"); //if not passed, will be "??"
+            MHO_BasicFringeDataConfiguration::determine_baselines(scan_dir, cmd_bl, baseline_files);
+            for(auto bl = baseline_files.begin(); bl != baseline_files.end(); bl++)
             {
-                std::string fgroup = *fg;
-                for(auto pprod = polproducts.begin(); pprod != polproducts.end(); pprod++)
+                std::string baseline = bl->first;
+                std::string corFile = bl->second;
+                std::string cmd_fg = cmdline_params.GetAs<std::string>("/cmdline/frequency_group"); //if not passed, this will be "?"
+                std::string cmd_pp = cmdline_params.GetAs<std::string>("/cmdline/polprod"); //if not passed, this will be "??"
+                MHO_BasicFringeDataConfiguration::determine_fgroups_polproducts(corFile, cmd_fg, cmd_pp, fgroups, polproducts);
+
+                for(auto fg = fgroups.begin(); fg != fgroups.end(); fg++)
                 {
-                    std::string polprod = *pprod;
-                    mho_json pass;
-                    pass["directory"] = scan_dir;
-                    pass["root_file"] = root_file;
-                    pass["baseline"] = baseline;
-                    pass["polprod"] = polprod;
-                    pass["frequency_group"] = fgroup;
-                    pass_vector.push_back(pass);
-                } //end of pol-product loop
-            } //end of frequency group loop
-        } //end of baseline loop
+                    std::string fgroup = *fg;
+                    for(auto pprod = polproducts.begin(); pprod != polproducts.end(); pprod++)
+                    {
+                        std::string polprod = *pprod;
+                        mho_json pass;
+                        pass["directory"] = scan_dir;
+                        pass["root_file"] = root_file;
+                        pass["baseline"] = baseline;
+                        pass["polprod"] = polprod;
+                        pass["frequency_group"] = fgroup;
+                        pass_vector.push_back(pass);
+                    } //end of pol-product loop
+                } //end of frequency group loop
+            }//end of baseline loop
+        } //end only if root exists
     } //end of scan loop
 
+    std::size_t n_scans = scans.size();
+    std::size_t n_pass = pass_vector.size();
+    msg_info("main", "fourfit will fringe "<<n_pass<<" passes of data in "<<n_scans<<" directories" << eom);
 
-    for(std::size_t pass_num=0; pass_num < pass_vector.size(); pass_num++)
+    //this loop could be trivially parallelized (with the exception of plotting)
+    for(std::size_t pass_num=0; pass_num < n_pass; pass_num++)
     {
         //grab this pass info
         mho_json pass = pass_vector[pass_num];
