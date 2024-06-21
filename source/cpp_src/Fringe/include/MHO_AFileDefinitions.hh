2eb8eae540fcfda07f232475b25542168945bf0a
John Barrett <barrettj@mit.edu>
2024-06-21 12:25:37 -0400

    work on afile format def

---
+++ source/cpp_src/Fringe/include/MHO_AFileDefinitions.hh
@@ -0,0 +1,44 @@
+#ifndef MHO_AFileDefinitions_HH__
+#define MHO_AFileDefinitions_HH__
+
+
+#include <string>
+#include <vector>
+
+#include "MHO_Message.hh"
+#include "MHO_Clock.hh"
+
+#include "MHO_ParameterStore.hh"
+
+#include "MHO_DirectoryInterface.hh"
+#include "MHO_JSONHeaderWrapper.hh"
+
+namespace hops
+{
+
+/*!
+*@file MHO_AFileDefinitions.hh
+*@class MHO_AFileDefinitions
+*@author
+*Email:
+*@date Wed Sep 20 16:12:23 2023 -0400
+*@brief extract useful information from .cor, .frng. and root files for afile generation
+*/
+
+class MHO_AFileDefinitions
+{
+
+    public:
+        MHO_AFileDefinitions(){};
+        virtual ~MHO_AFileDefinitions(){};
+
+    public:
+
+        static std::string GetFormatDirectory();
+        static std::vector< std::string > GetKeywordNames();
+        static mho_json GetAFileFormat();
+};
+
+}//end namespace
+
+#endif /*! end of include guard: MHO_AFileDefinitions_HH__ */
---
+++ source/cpp_src/Fringe/src/MHO_AFileDefinitions.cc
@@ -0,0 +1,76 @@
+#include "MHO_AFileDefinitions.hh"
+
+#include <iostream>
+#include <fstream>
+
+
+namespace hops
+{
+
+
+std::string
+MHO_AFileDefinitions::GetFormatDirectory()
+{
+    std::string format_dir = HOPS_AFILE_FORMAT_DIR;
+    format_dir += "/afio/";
+    return format_dir;
+}
+
+std::vector< std::string >
+MHO_AFileDefinitions::GetKeywordNames()
+{
+
+    std::vector< std::string > keywords;
+    std::string format_dir = GetFormatDirectory();
+    MHO_DirectoryInterface dirInterface;
+
+    dirInterface.SetCurrentDirectory(format_dir);
+    dirInterface.ReadCurrentDirectory();
+    dirInterface.GetFilesMatchingExtention(keywords, "json");
+
+    for(std::size_t i=0; i<keywords.size(); i++)
+    {
+        std::string tmp = MHO_DirectoryInterface::GetBasename( keywords[i] );
+        keywords[i] = MHO_DirectoryInterface::StripExtensionFromBasename(tmp);
+    }
+
+    return keywords;
+}
+
+
+mho_json
+MHO_AFileDefinitions::GetAFileFormat()
+{
+    //load all of the .json format files here so they are in memory
+    //TODO -- we may also want to combine all the format files into a single file
+
+    std::string format_dir = GetFormatDirectory();
+    std::vector< std::string > keywords = GetKeywordNames();
+    mho_json format_obj;
+
+    for(auto keyIt = keywords.begin(); keyIt != keywords.end(); keyIt++ )
+    {
+        std::string key = *keyIt;
+        std::string element_format_file = key + ".json";
+        std::string format_file = format_dir + element_format_file;
+
+        //TODO should check that the file exists
+        std::ifstream bf_ifs;
+        bf_ifs.open( format_file.c_str(), std::ifstream::in );
+
+        mho_json bformat;
+        if(bf_ifs.is_open())
+        {
+            bformat = mho_json::parse(bf_ifs);
+            format_obj[key] = bformat;
+        }
+        bf_ifs.close();
+    }
+
+    std::cout<<"format = "<<format_obj.dump(2)<<std::endl;
+
+    return format_obj;
+}
+
+
+}//end namespace
