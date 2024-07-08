83dc80052b6606384f36feaf6db4d8018ffa4cac
John Barrett <barrettj@mit.edu>
2024-07-08 17:34:48 -0400

    make sure freq band label is attached by difx2hops in test

--- source/cpp_src/DiFXInterface/applications/test/chk_difx2hops.sh
+++ source/cpp_src/DiFXInterface/applications/test/chk_difx2hops.sh
@@ -17,7 +17,7 @@ EXP_NUM=1111
 #paths are used, but that doesn't work outside of a correlator environment.
 cd $EXP_DIR
 rm -rf $EXP_DIR/1111/
-difx2hops -e $EXP_NUM -s ./station.codes -i ./
+difx2hops -e $EXP_NUM -s ./station.codes -b X 0 100000 -i ./
 RET_VAL=$?
 
 SCAN_DIR=$(ls -1 $EXP_DIR/1111/)
