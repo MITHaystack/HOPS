82f680ac75c23510c97cb87d7730c5f9df7a9563
John Barrett <barrettj@mit.edu>
2024-07-10 13:47:30 -0400

    start work on porting RJC's ion_covariance method

--- source/cpp_src/Fringe/src/MHO_BasicFringeInfo.cc
+++ source/cpp_src/Fringe/src/MHO_BasicFringeInfo.cc
@@ -71,7 +71,7 @@ MHO_BasicFringeInfo::calculate_sbd_error(double sbd_sep, double snr, double sbav
 {
     /* get proper weighting for sbd error estimate */
     // status->sbavg = 0.0;
-    // for (fr = 0; fr < pass->nfreq; fr++)
+    // for (fr = 0; fr < nfreq; fr++)
     // for (ap = pass->ap_off; ap < pass->ap_off + pass->num_ap; ap++)
     // status->sbavg += pass->pass_data[fr].data[ap].sband;
     // status->sbavg /= status->total_ap;
@@ -144,7 +144,7 @@ MHO_BasicFringeInfo::calculate_theory_timerms_phase(double nseg, double snr)
     /* true_nseg is meant to be effective */
     /* number of segments actually included */
     /* in the fit for switched mode */
-    //true_nseg = status.nseg * totap / (pass->num_ap * pass->nfreq);
+    //true_nseg = status.nseg * totap / (pass->num_ap * nfreq);
     double true_nseg = nseg;
     double th_timerms_phase = std::sqrt(true_nseg) * 180. / (M_PI * snr);
     return th_timerms_phase;
@@ -185,6 +185,67 @@ MHO_BasicFringeInfo::correct_phases_mbd_anchor_sbd(double ref_freq, double freq0
     resphase_deg = std::fmod(resphase_deg, 360.0);
 }
 
+void
+MHO_BasicFringeInfo::ion_covariance(int nfreq, double famp, double snr, double ref_freq, 
+                                    const std::vector<double>& chan_freqs,
+                                    const std::vector< std::complex<double> >& chan_phasors)
+{
+    int i, j, fr;
+    double sigma_fr,
+           fk,
+           f0,
+           w,
+           A[3][3],                     // normal equation matrix
+           C[3][3];                     // covariance matrix
+    const double b = -1.3445;           // for correct TEC units
+
+    // extern struct type_status status;
+    // extern struct type_param param;
+
+
+    for (i=0; i<3; i++)                 // pre-clear the normal matrix
+        for (j=0; j<3; j++)
+            A[i][j] = 0.0;
+
+    for (fr = 0; fr < nfreq; fr++)
+    {
+                                        // increment normal equations
+        sigma_fr = sqrt ((double)nfreq) * famp / ( 2.0 * M_PI * snr * std::abs(chan_phasors[fr]) );
+                                        // coefficient matrix weight
+        w = 1.0 / (sigma_fr * sigma_fr);
+                                        // convenience variables to match rjc memo
+        fk = 1e-3 * chan_freqs[fr]; //channel frequency;
+        f0 = 1e-3 * ref_freq;     // (GHz)
+
+        A[0][0] += w * (fk - f0) * (fk - f0);
+        A[0][1] += w * (fk - f0);
+        A[0][2] += w * b * (fk - f0) / fk;
+        A[1][1] += w;
+        A[1][2] += w * b / fk;
+        A[2][2] += w * (b / fk) * (b / fk);
+    }
+    A[1][0] = A[0][1];                  // fill in rest of symmetric normal matrix
+    A[2][0] = A[0][2];
+    A[2][1] = A[1][2];
+
+    //                                     // invert the normal matrix to get covariance matrix
+    // if (minvert3( A, C))              // error returned?
+    //     if (status.nion)
+    //         {                           // - yes
+    //         msg ("unable to compute ionosphere errors due to singular matrix", 2);
+    //         }
+    // 
+    // for (i=0; i<3; i++)             // std devs. are sqrt of diag of covariance matrix
+    //     status.ion_sigmas[i] = sqrt (C[i][i]);
+    // for (i=0; i<3; i++)             // normalize covariance to get correlation matrix
+    //     for (j=0; j<3; j++)
+    //         C[i][j] /= (status.ion_sigmas[i] * status.ion_sigmas[j]);
+    // msg ("ionospheric sigmas: delay %f (ps) phase %f (deg) dTEC %f", 0,
+    //       1e3 * status.ion_sigmas[0], 360 * status.ion_sigmas[1], status.ion_sigmas[2]);
+    // msg ("ionosphere correlation matrix:\n"
+    //      "%7.3f %7.3f %7.3f\n%7.3f %7.3f %7.3f\n%7.3f %7.3f %7.3f", 1,
+    //      C[0][0], C[0][1], C[0][2], C[1][0], C[1][1], C[1][2], C[2][0], C[2][1], C[2][2]);
+}
 
 
 
