00a828f67254b4be34b8161d8ad657ef4e586665
John Barrett <barrettj@mit.edu>
2024-07-11 15:24:07 -0400

    wrapping python binding tests so that environment can be setup properly

---
+++ source/cpp_src/Plugins/PythonBindings/Containers/test/chk_pycontainers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+verb=false
+[ -n "$testverb" ] && verb=true
+
+[ -d "$srcdir" ] || { echo srcdir not set; exit 1; }
+${HOPS_SETUP-'false'} || . $srcdir/chk_env.sh
+export DATADIR=`cd $srcdir/3764; pwd`
+
+TestPythonContainers
+
+RET_VAL=$?
+exit $RET_VAL
