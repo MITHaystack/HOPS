df745d8ab1ae8b3a5d11dce069ebdb21f8754911
John Barrett <barrettj@mit.edu>
2024-07-11 14:27:23 -0400

    ensure hops4 version of test data is available when difx not built against

--- source/cpp_src/Applications/test/chk_m42h.sh
+++ source/cpp_src/Applications/test/chk_m42h.sh
@@ -5,6 +5,7 @@ verb=false
 [ -d "$srcdir" ] || { echo srcdir not set; exit 1; }
 ${HOPS_SETUP-'false'} || . $srcdir/chk_env.sh
 export DATADIR=`cd $srcdir/3764; pwd`
+export DATADIR2=`cd $srcdir/vt9105; pwd`
 
 RET_VAL=0
 EXP_DIR=$DATADIR
@@ -14,6 +15,14 @@ CM42H_DIR=104-1228a
 cd $EXP_DIR
 echo "Running: mark42hops -i ./${SCAN_DIR} -o ./${CM42H_DIR}"
 mark42hops -i ./${SCAN_DIR} -o ./${CM42H_DIR}
+
+EXP_DIR=$DATADIR2
+SCAN_DIR=105-1800
+CM42H_DIR=105-1800b
+cd $EXP_DIR
+mkdir -p ./1111
+echo "Running: mark42hops -i ./${SCAN_DIR} -o ./${CM42H_DIR}"
+mark42hops -i ./1234/${SCAN_DIR} -o ./1111/${CM42H_DIR}
 
 RET_VAL=$?
 exit $RET_VAL
--- source/cpp_src/Applications/test/chk_simplefringesearch4.sh
+++ source/cpp_src/Applications/test/chk_simplefringesearch4.sh
@@ -11,14 +11,13 @@ EXP_DIR=$DATADIR
 D2H_EXP_NUM=1111
 D2M4_EXP_NUM=1234
 SCAN_DIR=105-1800
-HOPS4_DIR=105-1800
+HOPS4_DIR=105-1800b
 cd $EXP_DIR
 export HOPS_PLOT_DATA_MASK=0x83FFFFFF
 
 if [ ! -d "./${D2H_EXP_NUM}" ]; then
     echo "difx2hops not run, using mark42hops converted data (105-1800a) for test"
-    D2H_EXP_NUM="."
-    HOPS4_DIR="105-1800a"
+    HOPS4_DIR="105-1800b"
 fi
 
 # echo "Running: fourfit4 -c ./cf_test5 -b GE -P I ./${D2H_EXP_NUM}/${SCAN_DIR}"
