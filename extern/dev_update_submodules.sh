cd12d4789b488381b8c4ee1e90897ab55cf9088e
John Barrett <barrettj@mit.edu>
2024-07-12 14:17:39 -0400

    add comment on how to pull submodules

--- extern/dev_update_submodules.sh
+++ extern/dev_update_submodules.sh
@@ -2,7 +2,8 @@
 
 #simple script for developers to update the canned copy of the git submodules (date, json, pybind11)
 #must be run from the <hops-git>/extern directory
-#make sure to clone the submodules first!
+#make sure to clone the submodules first with (from the root repo directory):
+# git submodule update --init --recursive
 
 cd ./submodules/date
 git archive --format=tar.gz HEAD > ../../date.tar.gz
