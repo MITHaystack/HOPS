4c0caa4a562f8ca53a386a3709507e4b04fe96e0
John Barrett <barrettj@mit.edu>
2024-07-18 12:51:57 -0400

    moving MHO_DelayModel, MHO_StationModel, and MHO_InterpolateFringePeak from Fring library into calibration library. Deprecating MHO_MultidimensionalPaddedFastFourierTransform

--- source/cpp_src/Calibration/src/MHO_InterpolateFringePeak.cc
+++ source/cpp_src/Calibration/src/MHO_InterpolateFringePeak.cc
@@ -225,7 +225,7 @@ MHO_InterpolateFringePeak::fine_peak_interpolation()
     // std::cout<< std::setprecision(15);
     // std::cout<<"coarse location (sbd, mbd, dr) = "<<sbd<<", "<<mbd<<", "<<dr<<std::endl;
     // std::cout<<"change (sbd, mbd, dr) = "<<sbd_change<<", "<<mbd_change<<", "<<dr_change<<std::endl;
-    msg_debug("fringe", "Peak max555, sbd "<<sbd_max<<" mbd "<<mbd_max_global<<" dr "<<dr_max_global<<" amp "<< fFringeAmp << eom );
+    msg_info("fringe", "Peak max555, sbd "<<sbd_max<<" mbd "<<mbd_max_global<<" dr "<<dr_max_global<<" amp "<< fFringeAmp << eom );
 
     profiler_stop();
 
