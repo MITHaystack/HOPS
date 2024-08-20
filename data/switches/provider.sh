43513c9ecc074e22cd15378a14710dc6f3b7afcd
John Barrett <barrettj@mit.edu>
2024-08-20 16:29:13 -0400

    in prep for alpha release, paring down the build to the absolute minimum, removing autotools build system, most of doc folder, data folder, .github and other extraneous unmaintained stuff. Revert this commit to bring them back into the build and maintain them

--- data/switches/provider.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# for the moment, this is the same as legacy unpack
-# but eventually, we may have additional data that is not legacy
-#
-[ -x $MHO_REGRESSION_DATA/bootstrap/legacy_unpack.sh ] &&
-exec $MHO_REGRESSION_DATA/bootstrap/legacy_unpack.sh "$@" ||
-{ echo configuration error ; exit 99 ; }
-# eof
