4889c41efb850f261c8d222de88298e31bf04622
John Barrett <barrettj@mit.edu>
2024-08-31 23:56:04 -0400

    add arg parse to alist4

--- source/cpp_src/Applications/alist4.cc
+++ source/cpp_src/Applications/alist4.cc
@@ -8,8 +8,6 @@
 //option parsing and help text library
 #include "CLI11.hpp"
 
-#define EXTRA_DEBUG
-
 #include "MHO_Message.hh"
 #include "MHO_ContainerDefinitions.hh"
 
@@ -23,13 +21,56 @@ using namespace hops;
 
 
 
-
-
-
 int main(int argc, char** argv)
 {
     MHO_Message::GetInstance().AcceptAllKeys();
 
+    std::vector< std::string > input_files;
+    std::string delimiter = "\t";
+    std::string output_file = "alist.out";
+    int message_level = 0;
+    int version = 6; //default format version
+
+    CLI::App app{"alist"};
+
+    app.add_option("input_files,-i,--input-files", input_files, "list of the files to process")->required();
+    app.add_option("-o,--output-file", output_file, "name of the output file (default: alist.out)");
+    app.add_option("-m,--message-level", message_level, "message level to be used, range: -2 (debug) to 5 (silent)");
+    app.add_option("-c,--column-delimiter", delimiter, "the column delimiter for the alist output (default: <tab>)");
+    app.add_option("-v,--version", version, "the alist version (default: 6)");
+
+    //options of original alist program are:
+    //'-o' to specify the output file name
+    //'-m' to specify the message level
+    //'-c' to set the afile delimiter character/string
+    //'-v' to specify the format version
+    //the rest of the args are a positional list of files to process
+
+    //some other arguments we want to consider implmenting:
+    //(1) '-s' for sort (sort the files by time/baseline)
+    //(2) '--only-<something>' to process only cor or fringe or station files, etc
+    //(3) '-j' export data as a json file
+    //(4) maybe some other options governing header behavior
+
+    CLI11_PARSE(app, argc, argv);
+
+    //clamp message level
+    if(message_level > 5){message_level = 5;}
+    if(message_level < -2){message_level = -2;}
+    MHO_Message::GetInstance().AcceptAllKeys();
+    MHO_Message::GetInstance().SetLegacyMessageLevel(message_level);
+
+    std::cout<<"options:"<<std::endl;
+    std::cout<<delimiter<<std::endl;
+    std::cout<<output_file<<std::endl;
+    std::cout<<message_level<<std::endl;
+    std::cout<<version<<std::endl;
+
+    std::cout<<"input files:"<<std::endl;
+    for(std::size_t i=0; i<input_files.size(); i++)
+    {
+        std::cout<<input_files[i]<<std::endl;
+    }
 
     return 0;
 }
