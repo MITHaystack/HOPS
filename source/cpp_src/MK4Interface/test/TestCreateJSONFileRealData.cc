f490a5b7407f04aabd95b8966e2493d7abca3e94
Violet Pfeiffer <violetp@mit.edu>
2024-09-06 15:34:17 -0400

    Add boiler plate for t200 test with real data

---
+++ source/cpp_src/MK4Interface/test/TestCreateJSONFileRealData.cc
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include "MHO_MK4FringeInterface.hh"
+
+const int NUMBEROFFFITCHAN = 16;
+const int NUMBEROFSIDEBANDSANDSBWEIGHTS = 64;
+const int REFANDREMSIZE = 64;
+const int AMPPHASE = 64;
+const int DATASIZE = 1;
+
+using namespace hops;
+
+int main(int argc, char **argv) {
+
+  MHO_MK4FringeInterface mk4FringeInterface;
+
+  return 0;
+}
