3a7ef2e525d24c0107df28ed3216b3ee2a3a11db
Violet Pfeiffer <violetp@mit.edu>
2024-09-18 16:23:57 -0400

    Add test for JSON with real data

--- source/cpp_src/MK4Interface/test/TestCreateJSONFileRealData.cc
+++ source/cpp_src/MK4Interface/test/TestCreateJSONFileRealData.cc
@@ -15,5 +15,15 @@ int main(int argc, char **argv) {
 
   MHO_MK4FringeInterface mk4FringeInterface;
 
+  // Read fringe file
+  std::string fringeFile = "/home/violetp/code-projects/hops/hops-git/x86_64-4.00/data/test_data/testdata/3562/141-0002/GH.X.3.yxhoyl";
+  mk4FringeInterface.ReadFringeFile(fringeFile);
+  //mk4FringeInterface.ExportFringeFiles();
+
+  // convert to JSON (data dumped automatically)
+  
+  // Add the code from TestCreatJSONFile.cc that checks if the JSON file was dumped 
+  // and verifies that it is valid JSON.
+
   return 0;
 }
