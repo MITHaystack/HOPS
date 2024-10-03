4804a5eb5f8a9958e59f0c67e451d4a22bb41dc0
John Barrett <barrettj@mit.edu>
2024-10-02 23:11:14 -0400

    clean up NormFX

--- source/cpp_src/Calibration/src/MHO_NormFX.cc
+++ source/cpp_src/Calibration/src/MHO_NormFX.cc
@@ -125,8 +125,7 @@ bool MHO_NormFX::ExecuteOutOfPlace(const XArgType* in, XArgType* out)
         std::vector< mho_json > dsb_labels = std::get< CHANNEL_AXIS >(*(in)).GetMatchingIntervalLabels("double_sideband");
         std::size_t n_dsb_chan = dsb_labels.size();
 
-        FillSBDTableNoDSB(in, out); //fill the table as normal, ignore dsb channels
-        // else{FillSBDTableWithDSB(in, out);} //deal with the complication caused by dsb channels
+        FillSBDTable(in, out); //fill the table as normal, ignore dsb channels
 
         //filter out any NaNs
         status = fNaNBroadcaster.Execute();
@@ -150,7 +149,7 @@ bool MHO_NormFX::ExecuteOutOfPlace(const XArgType* in, XArgType* out)
             return false;
         }
 
-        //for lower sideband we complex conjugate the data (could do this before FFT)
+        //for lower sideband we complex conjugate the data after the FFT
         auto chan_ax = &(std::get< CHANNEL_AXIS >(*out));
         for(std::size_t ch = 0; ch < chan_ax->GetSize(); ch++)
         {
@@ -200,11 +199,9 @@ bool MHO_NormFX::ExecuteInPlace(XArgType* in)
     return status;
 }
 
-bool MHO_NormFX::FillSBDTableNoDSB(const XArgType* in, XArgType* out)
+bool MHO_NormFX::FillSBDTable(const XArgType* in, XArgType* out)
 {
-    //copy in the visibility data in a zero-padded fashion (zeros go on the end)
-    //this is only valid when there are no double-sideband channels that need to
-    //be merged
+    //copy in the visibility data in a zero-padded fashion (zeros go on the end here)
     out->ZeroArray();
     bool status = fZeroPadder.Execute(); //use pre-configured zero-padder operator
     if(!status)
@@ -212,86 +209,7 @@ bool MHO_NormFX::FillSBDTableNoDSB(const XArgType* in, XArgType* out)
         msg_error("calibration", "could not execute zero padder in MHO_NormFX" << eom);
         return false;
     }
-
-    // //for lower sideband we complex conjugate and flip the data and conjugate (equivalent to a conjugation after the FFT)
-    // auto pp_ax = &(std::get<POLPROD_AXIS>(*out));
-    // auto chan_ax = &(std::get<CHANNEL_AXIS>(*out));
-    // auto time_ax = &(std::get<TIME_AXIS>(*out));
-    // for(std::size_t pp=0; pp < pp_ax->GetSize(); pp++)
-    // {
-    //     for(std::size_t ch=0; ch < chan_ax->GetSize(); ch++)
-    //     {
-    //         std::string net_sideband;
-    //         bool key_present = chan_ax->RetrieveIndexLabelKeyValue(ch, "net_sideband", net_sideband);
-    //         if(!key_present){msg_error("calibration", "norm_fx missing net_sideband label for channel "<< ch << eom); }
-    //         if(net_sideband == "L")
-    //         {
-    //             for(std::size_t ap=0; ap< time_ax->GetSize(); ap++)
-    //             {
-    //                 //just grab the slice that matches this chunk
-    //                 auto slice = out->SubView(pp, ch, ap);
-    //                 //flip the order of the elements,
-    //                 std::reverse(slice.begin(), slice.end() );
-    //                 //then conjugate it
-    //                 for(auto it = slice.begin(); it != slice.end(); it++){*it = std::conj(*it);}
-    //             }
-    //         }
-    //     }
-    // }
-
     return status;
-}
-
-bool MHO_NormFX::FillSBDTableWithDSB(const XArgType* in, XArgType* out)
-{
-    //now we copy the data from in -> out keeping in mind that:
-    //USB and LSB channels get copied in at the front (with the zero-padding at the end)
-    //DSB channels must be merged, with USB at the front and DSB at the back
-    out->ZeroArray();
-
-    //copy the pol-product and time axes (as they are the same size as input table)
-    auto pp_ax = std::get< POLPROD_AXIS >(*in);
-    auto ap_ax = std::get< TIME_AXIS >(*in);
-    //
-    // //now deal with the channel axis...these have a different sizes, because we
-    // //need to merge any DSB channels
-    // auto in_chan_ax = &(std::get<CHANNEL_AXIS>(*in));
-    // auto out_chan_ax = &(std::get<CHANNEL_AXIS>(*out));
-    // std::size_t out_chan_count = 0;
-    // for(std::size_t i=0; i<in_chan_ax->GetSize(); i++)
-    // {
-    //     mho_json index_label_obj = in_chan_ax->GetLabelObject(i);
-    //     //check if this channel is a member of a double side-band pair
-    //     if(index_label_obj.contains("dsb_partner"))
-    //     {
-    //         //this is a double sideband channel, get the partner's index
-    //         //this should be the USB partner
-    //         int pindex = index_label_obj["dsb_partner"].get<int>();
-    //         if(pindex != i+1)
-    //         {
-    //             //something has gone seriously wrong, this data can't be processed
-    //             msg_fatal("calibration", "non-adjacent double-sideband pair, cannot merge LSB/USB channels of this form " << eom);
-    //             std::exit(1);
-    //         }
-    //         //copy the sky frequency value
-    //         out_chan_ax->at(out_chan_count) = in_chan_ax->at(i);
-    //         //copy the label, but modify the net_sideband
-    //         index_label_obj["net_sideband"] = "D";
-    //         out_chan_ax->SetLabelObject(index_label_obj, out_chan_count);
-    //         i = pindex; //jump to adjacent USB channel
-    //         out_chan_count++;
-    //     }
-    //     else
-    //     {
-    //         //just a regular channel
-    //         out_chan_ax->at(out_chan_count) = in_chan_ax->at(i); //copy sky frequency
-    //         out_chan_ax->SetLabelObject(index_label_obj, out_chan_count); //copy labels
-    //         out_chan_count++;
-    //     }
-    // }
-    //
-
-    return false;
 }
 
 } // namespace hops
