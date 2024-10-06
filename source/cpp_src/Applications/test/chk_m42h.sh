b0bb513003cc859c7da5c9ae1285f0bd380e71e1
John Barrett <barrettj@mit.edu>
2024-10-06 18:11:21 -0400

    add new tarball containing mixed-mode data to testdata download for DSB testing

--- source/cpp_src/Applications/test/chk_m42h.sh
+++ source/cpp_src/Applications/test/chk_m42h.sh
@@ -6,6 +6,7 @@ verb=false
 ${HOPS_SETUP-'false'} || . $srcdir/chk_env.sh
 export DATADIR=`cd $srcdir/3764; pwd`
 export DATADIR2=`cd $srcdir/vt9105; pwd`
+export DATADIR3=`cd $srcdir/3741; pwd`
 
 RET_VAL=0
 EXP_DIR=$DATADIR
@@ -23,6 +24,13 @@ cd $EXP_DIR
 mkdir -p ./1111
 echo "Running: mark42hops -i ./${SCAN_DIR} -o ./${CM42H_DIR}"
 mark42hops -i ./1234/${SCAN_DIR} -o ./1111/${CM42H_DIR}
+
+EXP_DIR=$DATADIR3
+SCAN_DIR=190-1800a
+CM42H_DIR=190-1800b
+cd $EXP_DIR
+echo "Running: mark42hops -i ./${SCAN_DIR} -o ./${CM42H_DIR}"
+mark42hops -i ./${SCAN_DIR} -o ./${CM42H_DIR}
 
 RET_VAL=$?
 exit $RET_VAL
