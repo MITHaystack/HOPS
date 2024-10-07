edcf6286e26085a38b8f0226b614e3c7ff8eb071
John Barrett <barrettj@mit.edu>
2024-10-07 18:01:23 -0400

    wip lsb offset builder

---
+++ source/cpp_src/Initialization/include/MHO_LSBOffsetBuilder.hh
@@ -0,0 +1,34 @@
+#ifndef MHO_LSBOffsetBuilderBuilder_HH__
+#define MHO_LSBOffsetBuilderBuilder_HH__
+
+#include "MHO_OperatorBuilder.hh"
+
+namespace hops
+{
+
+/*!
+ *@file MHO_LSBOffsetBuilder.hh
+ *@class MHO_LSBOffsetBuilder
+ *@author J. Barrett - barrettj@mit.edu
+ *@date Tue Jun 20 12:35:56 2023 -0400
+ *@brief
+ */
+
+class MHO_LSBOffsetBuilder: public MHO_OperatorBuilder
+{
+    public:
+        MHO_LSBOffsetBuilder(MHO_OperatorToolbox* toolbox, MHO_ContainerStore* cstore = nullptr,
+                                            MHO_ParameterStore* pstore = nullptr)
+            : MHO_OperatorBuilder(toolbox, cstore, pstore){};
+
+        virtual ~MHO_LSBOffsetBuilder(){};
+
+        virtual bool Build() override;
+
+    private:
+        std::string ExtractStationIdentifier();
+};
+
+} // namespace hops
+
+#endif /*! end of include guard: MHO_LSBOffsetBuilderBuilder_HH__ */
