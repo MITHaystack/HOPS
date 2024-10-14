756022f8691e003be8aed695e26c83db7335184f
John Barrett <barrettj@mit.edu>
2024-10-13 20:17:29 -0400

    add ff factory class

---
+++ source/cpp_src/Fringe/include/MHO_FringeFitterFactory.hh
@@ -0,0 +1,38 @@
+#ifndef MHO_FringeFitterFactory_HH__
+#define MHO_FringeFitterFactory_HH__
+
+//global messaging util
+#include "MHO_Message.hh"
+#include "MHO_FringeData.hh"
+#include "MHO_FringeFitter.hh"
+
+namespace hops
+{
+
+/*!
+ *@file MHO_FringeFitterFactory.hh
+ *@class MHO_FringeFitterFactory
+ *@author J. Barrettj - barrettj@mit.edu
+ *@date Sun Oct 13 08:05:51 PM EDT 2024
+ *@brief Abstract base class for a basic fringe fitter
+ */
+
+class MHO_FringeFitterFactory
+{
+    public:
+        MHO_FringeFitterFactory(MHO_FringeData* data);
+
+        virtual ~MHO_FringeFitterFactory();
+
+        MHO_FringeFitter* ConstructFringeFitter();
+
+    protected:
+
+        MHO_FringeData* fFringeData;
+        MHO_FringeFitter* fFringeFitter;
+
+};
+
+} // namespace hops
+
+#endif /*! end of include guard: MHO_FringeFitterFactory_HH__ */
---
+++ source/cpp_src/Fringe/src/MHO_FringeFitterFactory.cc
@@ -0,0 +1,46 @@
+#include "MHO_FringeFitterFactory.hh"
+#include "MHO_BasicFringeFitter.hh"
+#include "MHO_IonosphericFringeFitter.hh"
+
+
+namespace hops 
+{
+
+MHO_FringeFitterFactory::MHO_FringeFitterFactory(MHO_FringeData* data):
+    fFringeData(data),
+    fFringeFitter(nullptr)
+{}
+
+MHO_FringeFitterFactory::~MHO_FringeFitterFactory()
+{
+    //delete the fringe fitter 
+    delete fFringeFitter;
+}
+
+MHO_FringeFitter* 
+MHO_FringeFitterFactory::ConstructFringeFitter()
+{
+    //if it has already been built, just return the existing one
+    if(fFringeFitter != nullptr){return fFringeFitter;}
+
+    //currently we only have two fringe-fitting options (basic or with ionosphere)
+    //but if we add more types, we need to add logic to decide what type should be built
+    bool do_ion = false;
+    fFringeData->GetParameterStore()->Get("/config/do_ion", do_ion);
+
+    if(do_ion)
+    {
+        msg_debug("fringe", "constructing an ionospheric fringe fitter" << eom );
+        fFringeFitter = new MHO_IonosphericFringeFitter(fFringeData);
+    }
+    else
+    {
+        msg_debug("fringe", "constructing a basic fringe fitter" << eom);
+        fFringeFitter = new MHO_BasicFringeFitter(fFringeData);
+    }
+    fFringeFitter->Configure();
+    return fFringeFitter;
+}
+
+
+}//end namespace
