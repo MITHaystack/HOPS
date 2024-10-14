3d8cbd9b1fb5cc37629a57a539c97f3b94632f8f
John Barrett <barrettj@mit.edu>
2024-10-13 20:18:37 -0400

    minor comments

--- source/cpp_src/Fringe/src/MHO_FringeFitterFactory.cc
+++ source/cpp_src/Fringe/src/MHO_FringeFitterFactory.cc
@@ -23,7 +23,7 @@ MHO_FringeFitterFactory::ConstructFringeFitter()
     //if it has already been built, just return the existing one
     if(fFringeFitter != nullptr){return fFringeFitter;}
 
-    //currently we only have two fringe-fitting options (basic or with ionosphere)
+    //currently we only have two fringe-fitting options (basic or with ionosphere fitting)
     //but if we add more types, we need to add logic to decide what type should be built
     bool do_ion = false;
     fFringeData->GetParameterStore()->Get("/config/do_ion", do_ion);
