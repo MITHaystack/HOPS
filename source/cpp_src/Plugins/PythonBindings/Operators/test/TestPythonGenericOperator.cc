0c40f62c357e6725f203f1fbe3368b3e0e3f965f
John Barrett <barrettj@mit.edu>
2024-10-15 20:51:12 -0400

    getting test to build...need to revamp the python op interaction with the builder manager

--- source/cpp_src/Plugins/PythonBindings/Operators/test/TestPythonGenericOperator.cc
+++ source/cpp_src/Plugins/PythonBindings/Operators/test/TestPythonGenericOperator.cc
@@ -130,9 +130,9 @@ int main(int argc, char** argv)
 
     MHO_FringeData fdata;
     //provide necessary objects for operation
-    MHO_ParameterStore* paramStore->= fdata.GetParameterStore(); //stores various parameters using string keys
-    MHO_ScanDataStore* scanStore->= fdata.GetScanDataStore();   //provides access to data associated with this scan
-    MHO_ContainerStore* conStore->= fdata.GetContainerStore();   //stores data containers for in-use data
+    MHO_ParameterStore* paramStore = fdata.GetParameterStore(); //stores various parameters using string keys
+    MHO_ScanDataStore* scanStore = fdata.GetScanDataStore();   //provides access to data associated with this scan
+    MHO_ContainerStore* conStore = fdata.GetContainerStore();   //stores data containers for in-use data
 
     ////////////////////////////////////////////////////////////////////////////
     //INITIALIZE SCAN DIRECTORY
@@ -152,17 +152,17 @@ int main(int argc, char** argv)
     paramStore->Set("baseline", baseline);
 
     //load baseline data
-    scanStore->LoadBaseline(baseline, &conStore->;
+    scanStore->LoadBaseline(baseline, conStore);
     //configure_data_library(&conStore->;//momentarily needed for float -> double cast
     //load and rename station data according to reference/remote
     std::string ref_station_mk4id = std::string(1, baseline[0]);
     std::string rem_station_mk4id = std::string(1, baseline[1]);
-    scanStore->LoadStation(ref_station_mk4id, &conStore->;
+    scanStore->LoadStation(ref_station_mk4id, conStore);
     conStore->RenameObject("sta", "ref_sta");
-    scanStore->LoadStation(rem_station_mk4id, &conStore->;
+    scanStore->LoadStation(rem_station_mk4id, conStore);
     conStore->RenameObject("sta", "rem_sta");
 
-    configure_data_library(&conStore->;
+    configure_data_library(conStore);
 
     station_coord_type* ref_data = conStore->GetObject< station_coord_type >(std::string("ref_sta"));
     station_coord_type* rem_data = conStore->GetObject< station_coord_type >(std::string("rem_sta"));
