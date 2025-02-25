614956afb412a22215bc18c75ea6e93ef99e6539
John Barrett <barrettj@mit.edu>
2025-02-25 11:54:55 -0500

    doh...fix stupid copypasta error

--- source/c_src/applications/aedit/src/pr_csumm.c
+++ source/c_src/applications/aedit/src/pr_csumm.c
@@ -68,10 +68,10 @@ int pr_csumm(void)
         for(i=0; i<150; i++){buf[i] = '\0'; buf2[i] = '\0';}
         for(i=0;i<19;i++) {
             sprintf(buf,"%s%c ", buf2, qclist[i]);
-            if(fsumm.qcodes[i] >= 10000) sprintf(buf2,"%s    ",buf);
-            else if(fsumm.qcodes[i] >= 1000) sprintf(buf2,"%s   ",buf);
-            else if(fsumm.qcodes[i] >= 100) sprintf(buf2,"%s  ",buf);
-            else if(fsumm.qcodes[i] >= 10) sprintf(buf2,"%s ",buf);
+            if(csumm.qcodes[i] >= 10000) sprintf(buf2,"%s    ",buf);
+            else if(csumm.qcodes[i] >= 1000) sprintf(buf2,"%s   ",buf);
+            else if(csumm.qcodes[i] >= 100) sprintf(buf2,"%s  ",buf);
+            else if(csumm.qcodes[i] >= 10) sprintf(buf2,"%s ",buf);
             else sprintf(buf2,"%s",buf);
         }
         printf("\t%s\n",buf2);
