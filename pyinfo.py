84a5c330fef363072eba2e20926b3366c29a156d
John Barrett <barrettj@mit.edu>
2025-05-13 15:35:14 -0400

    modify pyinfo.py so that we just check directly for setuptools/pip/distutils rather than switching off of the python version

--- pyinfo.py
+++ pyinfo.py
@@ -5,28 +5,25 @@ version_str = str(sys.version_info.major) + "." + str(sys.version_info.minor)
 pyinstall_path = os.path.join("lib", "python" + version_str, "site-packages")
 
 #determine if distutils or setuptools/pip is what we are going to use 
-setup_option=""
+setup_option="distutils.core" #default for python <3.10
 need_pip="false"
-if sys.version_info.major == 3 and sys.version_info.minor >= 10:
-   try:
-       from setuptools import setup
-       setup_option="setuptools"
-       need_pip="true"
-   except ImportError:
-       #setuptools not installed on this system
-       from distutils.core import setup
-       setup_option="distutils.core"
-       need_pip="false"
-else:
-    setup_option="distutils.core"
+
+try:
+   from setuptools import setup
+   setup_option="setuptools"
+   need_pip="true"
+except ImportError:
+   #setuptools not installed on this system
+   from distutils.core import setup
+   setup_option="distutils.core"
+   need_pip="false"
 
 pip_installed="false"
-if need_pip == "true":
-    try:
-        import pip 
-        pip_installed="true"
-    except ImportError:
-        pip_installed="false"
+try:
+    import pip 
+    pip_installed="true"
+except ImportError:
+    pip_installed="false"
 
 info_str = version_str + ";" + pyinstall_path + ";" + setup_option + ";" + need_pip + ";" + pip_installed
 #info_str = version_str + ";" + pyinstall_path + ";" + "distutils.core" + ";" + "false" + ";" + pip_installed
