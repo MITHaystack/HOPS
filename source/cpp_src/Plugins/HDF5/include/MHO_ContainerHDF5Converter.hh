ccd234ba5bf36363cd750362a4573d76643ef753
John Barrett <barrettj@mit.edu>
2025-05-18 16:22:29 -0400

    clean up attribute writing

--- source/cpp_src/Plugins/HDF5/include/MHO_ContainerHDF5Converter.hh
+++ source/cpp_src/Plugins/HDF5/include/MHO_ContainerHDF5Converter.hh
@@ -32,6 +32,41 @@ namespace hops
  */
 
 
+//generic  - write a scalar attribute
+template < typename XValueType >
+inline void write_attribute(const std::string& key, XValueType value, hid_t dataset_id)
+{
+    hid_t attr_space = H5Screate(H5S_SCALAR);
+    hid_t TYPE_CODE = MHO_HDF5TypeCode<XValueType>();
+    //create and write the attribute
+    hid_t attr_id = H5Acreate(dataset_id, key.c_str(), TYPE_CODE, attr_space, H5P_DEFAULT, H5P_DEFAULT);
+    H5Awrite(attr_id, TYPE_CODE, &value);
+    //clean up
+    H5Aclose(attr_id);
+    H5Sclose(attr_space);
+}
+
+//specialization for std::string
+template<>
+inline void write_attribute< std::string >(const std::string& key, std::string value, hid_t dataset_id)
+{
+    hid_t attr_space = H5Screate(H5S_SCALAR);
+    hid_t TYPE_CODE = H5Tcopy(H5T_C_S1);
+    H5Tset_size(TYPE_CODE, strlen(value.c_str() ));
+    H5Tset_strpad(TYPE_CODE, H5T_STR_NULLTERM);
+    std::cout<<"string = "<<value<<std::endl;
+    //create and write the attribute
+    hid_t attr_id = H5Acreate(dataset_id, key.c_str(), TYPE_CODE, attr_space, H5P_DEFAULT, H5P_DEFAULT);
+    H5Awrite(attr_id, TYPE_CODE, value.c_str());
+    //clean up
+    H5Aclose(attr_id);
+    H5Sclose(attr_space);
+    H5Tclose(TYPE_CODE);
+    return;
+}
+
+
+
 inline void make_attribute(const std::string& key, const mho_json& value, hid_t dataset_id)
 {
     std::cout<<"key = "<<key<<std::endl;
@@ -42,54 +77,34 @@ inline void make_attribute(const std::string& key, const mho_json& value, hid_t 
 
     if (value.is_string()) 
     {
-        std::string str = value.get<std::string>();
-        TYPE_CODE = H5Tcopy(H5T_C_S1);
-        H5Tset_size(TYPE_CODE, str.size());
-        H5Tset_strpad(TYPE_CODE, H5T_STR_NULLTERM);
-        attr_data = (void*)str.c_str();
-
+        std::string strv = value.get<std::string>();
+        write_attribute(key, strv, dataset_id);
     } 
     else if (value.is_number_integer()) 
     {
         int v = value.get<int>();
-        TYPE_CODE = MHO_HDF5TypeCode<int>();
-        attr_data = &v;
-
+        write_attribute(key,v,dataset_id);
     } 
     else if (value.is_number_unsigned()) 
     {
         unsigned int v = value.get<unsigned int>();
-        TYPE_CODE = MHO_HDF5TypeCode<unsigned int>();
-        attr_data = &v;
-
+        write_attribute(key,v,dataset_id);
     } 
     else if (value.is_number_float()) 
     {
         double v = value.get<double>();
-        TYPE_CODE = MHO_HDF5TypeCode<double>();
-        attr_data = &v;
-
+        write_attribute(key,v,dataset_id);
     }
     else if (value.is_boolean()) 
     {
         uint8_t v = value.get<bool>() ? 1 : 0;
-        TYPE_CODE = MHO_HDF5TypeCode<uint8_t>();
-        attr_data = &v;
+        write_attribute(key,v,dataset_id);
     } 
     else 
     {
         //for now we are skipping objects/arrays TODO FIXME
         return;
     }
-
-    //create and write the attribute
-    hid_t attr_id = H5Acreate(dataset_id, key.c_str(), TYPE_CODE, attr_space, H5P_DEFAULT, H5P_DEFAULT);
-    H5Awrite(attr_id, TYPE_CODE, attr_data);
-
-    //clean up
-    H5Aclose(attr_id);
-    H5Sclose(attr_space);
-    if (value.is_string()) H5Tclose(TYPE_CODE);
 }
 
 
