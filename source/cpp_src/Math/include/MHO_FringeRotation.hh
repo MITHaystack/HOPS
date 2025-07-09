d1c8f450cb271ba49969925fb54ea2ce0ae0f0b0
John Barrett <barrettj@mit.edu>
2025-07-09 16:24:03 -0400

    finish off comments in Math lib

--- source/cpp_src/Math/include/MHO_FringeRotation.hh
+++ source/cpp_src/Math/include/MHO_FringeRotation.hh
@@ -12,7 +12,7 @@ namespace hops
  *@class MHO_FringeRotation
  *@author J. Barrett - barrettj@mit.edu
  *@date Fri Feb 17 13:35:37 2023 -0500
- *@brief class to implement functionality originally found in vrot.c
+ *@brief class to implement the fringe rotation functionality originally found in vrot.c
  */
 
 /**
@@ -26,49 +26,48 @@ class MHO_FringeRotation
 
         //virtual function (can be pointed to different implementations)
         /**
-         * @brief Calculates fringe rotation using delay rate and frequency difference.
+         * @brief Calculates fringe rotation for given delay, delay-rate, time and frequency difference.
          * 
-         * @param time_delta Time interval between measurements
-         * @param freq Frequency of the signal
-         * @param ref_freq Reference frequency for comparison
-         * @param dr Delay rate, change of delay with time
-         * @param mbd Multi-Band Delay
+         * @param time_delta Time difference from fourfit reference time (s)
+         * @param freq Channel frequency (MHz)
+         * @param ref_freq Reference frequency (MHz)
+         * @param dr Delay rate ()
+         * @param mbd Multi-Band Delay (us)
          * @return Complex value representing fringe rotation
          * @note This is a virtual function.
          */
         virtual std::complex< double > vrot(double time_delta, double freq, double ref_freq, double dr, double mbd) const;
 
         /**
-         * @brief Setter for sbdseparation
+         * @brief Setter for SBD bin separation
          * 
-         * @param sbd_sep New separation value in time offset between two signals
+         * @param sbd_sep value of the (time) delta between two adjacent points in SBD space
          */
-        void SetSBDSeparation(double sbd_sep) { fSBDSep = sbd_sep; }; //value of the delta between points in SBD space
+        void SetSBDSeparation(double sbd_sep) { fSBDSep = sbd_sep; }; 
 
         /**
-         * @brief Setter for sbdmax
+         * @brief Setter for sbd max
          * 
-         * @param sbd_max Maximum delay time offset between two signals.
+         * @param sbd_max the single-band delay value at the fringe maximum
          */
-        void SetSBDMax(double sbd_max) { fSBDMax = sbd_max; } //sbd delay at maximum
+        void SetSBDMax(double sbd_max) { fSBDMax = sbd_max; }
 
         /**
-         * @brief Setter for sbdmax bin
+         * @brief Setter for sbd max bin
          * 
-         * @param sbd_max_bin Maximum bin value for Single Band Delay
+         * @param sbd_max_bin the bin value for the fringe maximum (Single Band Delay axis)
          */
         void SetSBDMaxBin(int sbd_max_bin) { fSBDMaxBin = sbd_max_bin; }
 
         /**
-         * @brief Setter for nsbdbins
+         * @brief Setter for N sbd bins
          * 
-         * @param n_sbd_bins Number of Single Band Delay bins
+         * @param n_sbd_bins set the number of Single Band Delay bins
          */
         void SetNSBDBins(int n_sbd_bins) { fNSBDBins = n_sbd_bins; }
 
         /**
-         * @brief Setter for sideband - pass the sideband information if optimize_closure of single-sideband correction is needed
-         * 
+         * @brief Setter for sideband - passes the sideband information if optimize_closure requires single-sideband correction         * 
          * @param sb Sideband type: 1 for USB, 0 for DSB, -1 for LSB
          */
         void SetSideband(int sb) { fSideband = sb; } //1 is USB, 0 if DSB, -1 if LSB
@@ -90,16 +89,16 @@ class MHO_FringeRotation
          * @brief Calculates and returns a phasor for fringe rotation given time delta, frequencies, delay rate, and Multi-Band Delay.
          * 
          * @param time_delta Time interval between current AP and fourfit reference time
-         * @param freq Frequency
+         * @param freq channel frequency
          * @param ref_freq Fringe reference frequency
-         * @param dr Delay rate (fringe parameter concerning change of delay with time)
+         * @param dr Delay rate
          * @param mbd Multi-Band Delay
          * @return Phasor representing fringe rotation as a complex exponential
          */
         std::complex< double > vrot_v1(double time_delta, double freq, double ref_freq, double dr, double mbd) const;
 
         /**
-         * @brief Calculates sideband correction for fringe rotation due to delay and frequency difference.
+         * @brief Calculates sideband correction for fringe rotation (if optimize_closure is true)
          * 
          * @param mbd Multi-Band Delay, input delay parameter
          * @return Sideband correction value as a double
