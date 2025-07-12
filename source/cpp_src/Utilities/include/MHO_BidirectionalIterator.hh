84f983f73afe63221000766528a5de9e876e6dce
John Barrett <barrettj@mit.edu>
2025-07-11 22:13:07 -0400

    add minor comments in Utilities up to MHO_BidirectionaStridedIter

--- source/cpp_src/Utilities/include/MHO_BidirectionalIterator.hh
+++ source/cpp_src/Utilities/include/MHO_BidirectionalIterator.hh
@@ -12,7 +12,7 @@ namespace hops
  *@class  MHO_BidirectionalIterator
  *@author  J. Barrett - barrettj@mit.edu
  *@date Sat Oct 2 04:34:18 2021 -0400
- *@brief
+ *@brief provides an iterator over a object in either increasing or decreasing index direction
  */
 
 /**
