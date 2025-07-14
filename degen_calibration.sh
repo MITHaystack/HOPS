ced7eddb326b86672df8d0125db0b18f9d89a439
John Barrett <barrettj@mit.edu>
2025-07-14 13:00:24 -0400

    remove doxydegen calling scripts

--- degen_calibration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Calibration"
-degen --llm-enhance --llm-endpoint http://localhost:11434/v1 --llm-model mistral-nemo:12b --collect-context ./source/cpp_src/Calibration --extra-context ./acronyms.txt ./source/cpp_src/Calibration 2>&1 | tee ./calibration.txt
-echo "Done"
-
--- degen_vex2json.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Vex2JSON"
-degen --llm-enhance --llm-endpoint http://localhost:11434/v1 --llm-model mistral-nemo:12b --collect-context ./source/cpp_src/Vex2JSON --extra-context ./acronyms.txt ./source/cpp_src/Vex2JSON 2>&1 | tee ./vex2json.txt
-echo "Done"
-
