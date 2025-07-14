71560ac1bd5d8e7104c3612c36aa32710336157f
John Barrett <barrettj@mit.edu>
2025-07-14 13:00:24 -0400

    remove doxydegen calling scripts

--- degen_difx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "DiFX"
-degen --llm-enhance --llm-endpoint http://localhost:11434/v1 --llm-model mistral-nemo:12b --collect-context ./source/cpp_src/DiFXInterface --extra-context ./acronyms.txt ./source/cpp_src/DiFXInterface 2>&1 | tee ./difx.txt
-echo "Done"
-
--- degen_mk4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "MK4"
-degen --llm-enhance --llm-endpoint http://localhost:11434/v1 --llm-model mistral-nemo:12b --collect-context ./source/cpp_src/MK4Interface --extra-context ./acronyms.txt ./source/cpp_src/MK4Interface 2>&1 | tee mk4.txt
-echo "Done"
-
