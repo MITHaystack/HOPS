b36878f9659b00523ef95114e0a12c9d8cc93eb9
John Barrett <barrettj@mit.edu>
2026-02-20 16:57:28 -0500

    wip, adding builders for the pol/polprod relablers

---
+++ source/cpp_src/Initialization/src/MHO_PolarizationRelabelerBuilder.cc
@@ -0,0 +1,46 @@
+#include "MHO_PolarizationRelabelerBuilder.hh"
+#include "MHO_PolarizationRelabeler.hh"
+#include "MHO_Meta.hh"
+
+namespace hops
+{
+
+bool MHO_PolarizationRelabelerBuilder::Build()
+{
+    if(IsConfigurationOk())
+    {
+        msg_debug("initialization", "building a polarization_relabel operator." << eom);
+
+        std::string op_name = fAttributes["name"].get<std::string>();
+        std::string op_category = "calibration";
+        double priority = fFormat["priority"].get<double>();
+
+        multitone_pcal_type* pcal_data = fContainerStore->GetObject<multitone_pcal_type>(std::string("pcal"));
+        if(pcal_data == nullptr)
+        {
+            msg_error("initialization", "cannot construct MHO_PolarizationRelabeler without pcal data." << eom);
+            return false;
+        }
+
+        auto pol_relabeler = new MHO_PolarizationRelabeler<multitone_pcal_type>();
+
+        std::string station_id = fAttributes["value"]["station"].get<std::string>();
+        pol_relabeler->SetStationIdentifier(station_id);
+
+        std::string pol1 = fAttributes["value"]["pol1"].get<std::string>();
+        std::string pol2 = fAttributes["value"]["pol2"].get<std::string>();
+        pol_relabeler->SetPolarizationSwapPair(pol1, pol2);
+        
+        pol_relabeler->SetArgs(pcal_data);
+        pol_relabeler->SetName(op_name);
+        pol_relabeler->SetPriority(priority);
+
+        bool replace_duplicates = false;
+        this->fOperatorToolbox->AddOperator(pol_relabeler, pol_relabeler->GetName(), op_category, replace_duplicates);
+
+        return true;
+    }
+    return false;
+}
+
+}
