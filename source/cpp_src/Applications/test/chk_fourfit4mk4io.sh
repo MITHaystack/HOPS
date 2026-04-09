bb01c0dff4b10def6c6705da41a6a3cf49ca46bc
John Barrett <barrettj@mit.edu>
2026-04-09 14:55:44 -0400

    add test for mark4 input/output to fourfit4 (-K/-k options)

---
+++ source/cpp_src/Applications/test/chk_fourfit4mk4io.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+verb=false
+[ -n "$testverb" ] && verb=true
+
+[ -d "$srcdir" ] || { echo srcdir not set; exit 1; }
+${HOPS_SETUP-'false'} || . $srcdir/chk_env.sh
+
+
+if [ ! -d "$srcdir/3764" ]; then
+  echo "Directory $srcdir/3764 does not exist, will not apply this test"
+  exit 127
+fi
+
+export DATADIR=`cd $srcdir/3764; pwd`
+RET_VAL=0
+EXP_DIR=$DATADIR
+D2M4_EXP_NUM=.
+MK4_SCAN_DIR=104-1228
+SCAN_DIR=104-1228a
+cd $EXP_DIR
+
+export HOPS_PLOT_DATA_MASK=0x83FFFFFF
+
+echo "Running: fourfit4 -m 4 -K -c ./test0.cf -b AS -P RR ./${MK4_SCAN_DIR}"
+output_file=$(fourfit4 -m 4 -K -c ./test0.cf -b AS -P RR ./${MK4_SCAN_DIR} 2>&1 | awk '{print $NF}')
+echo "fourfit4 output file: $output_file"
+
+#convert the fringe file to json
+hops2json ${output_file}
+
+#use jq (json query) to extract the plot_data element and pipe to file
+echo "jq '.[].tags.plot_data | select( . != null )' "${output_file}.json" > tee ./fdump.json"
+jq '.[].tags.plot_data | select( . != null )' "${output_file}.json" > ./fdump.json
+
+echo "Running: fourfit4 -m 4 -Kk -c ./test0.cf -b AS -P RR ./${MK4_SCAN_DIR}"
+mk4_output_file=$(fourfit4 -m 4 -Kk -c ./test0.cf -b AS -P RR ./${MK4_SCAN_DIR} 2>&1 | awk '{print $NF}')
+echo "fourfit4 mark4 output file: $mk4_output_file"
+
+#make sure the mark4 file was generated
+MK4_OUT_OK=0
+if [ -f "${mk4_output_file}" ]; then
+echo "fourfit4 mark4 output generated ok"
+  MK4_OUT_OK=1
+fi
+
+echo "Running: fourfit3 -m 4 -c ./test0.cf -b AS -P RR ./${MK4_SCAN_DIR} set plot_data_dir ./chk3 "
+time fourfit3 -m 1 -c ./test0.cf -b AS -P RR ./${MK4_SCAN_DIR} set plot_data_dir ./chk3 2>&1  | tee ./ff.out
+
+compjsonpdd.py ./fdump.json ./chk3/104-1228-AS-B-RR.*
+RET_VAL=$?
+
+[ -f "$mk4_output_file" ] || RET_VAL=1
+
+exit $RET_VAL 
