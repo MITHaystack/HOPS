e4ea41558034eba551738d765b7634b6d5b5e8dc
John Barrett <barrettj@mit.edu>
2026-04-27 16:50:54 -0400

    clean up hard-coded if statements in favor of unordered_set check

--- source/cpp_src/Initialization/src/MHO_OperatorBuilderManager.cc
+++ source/cpp_src/Initialization/src/MHO_OperatorBuilderManager.cc
@@ -1,5 +1,7 @@
 #include "MHO_OperatorBuilderManager.hh"
 
+#include <unordered_set>
+
 //builders
 #include "MHO_AdhocFlaggingBuilder.hh"
 #include "MHO_AdhocPhaseCorrectionBuilder.hh"
@@ -100,122 +102,89 @@ void MHO_OperatorBuilderManager::CreateDefaultBuilders()
 
 void MHO_OperatorBuilderManager::BuildOperatorCategory(const std::string& cat)
 {
-    //check that the category is supported (listed below)
-    bool ok = false;
+    static const std::unordered_set< std::string > kValidCategories = {"default",     "labeling", "selection", "flagging",
+                                                                        "calibration", "prefit",   "postfit",   "finalize"};
+
+    if(kValidCategories.count(cat) == 0)
+    {
+        msg_warn("initialization", "operator category: " << cat << " is not supported." << eom);
+        return;
+    }
+
+    msg_debug("initialization", "building operator category: " << cat << "." << eom);
+    //default category requires no control input
     if(cat == "default")
     {
-        ok = true;
-    }
-    if(cat == "labeling")
-    {
-        ok = true;
-    }
-    if(cat == "selection")
-    {
-        ok = true;
-    }
-    if(cat == "flagging")
-    {
-        ok = true;
-    }
-    if(cat == "calibration")
-    {
-        ok = true;
-    }
-    if(cat == "prefit")
-    {
-        ok = true;
-    }
-    if(cat == "postfit")
-    {
-        ok = true;
-    }
-    if(cat == "finalize")
-    {
-        ok = true;
-    }
-
-    if(ok)
-    {
-        msg_debug("initialization", "building operator category: " << cat << "." << eom);
-        //default category requires no control input
-        if(cat == "default")
+        auto it1 = fCategoryToBuilderMap.lower_bound(cat);
+        auto it2 = fCategoryToBuilderMap.upper_bound(cat);
+        if(it1 != fCategoryToBuilderMap.end())
         {
-            auto it1 = fCategoryToBuilderMap.lower_bound(cat);
-            auto it2 = fCategoryToBuilderMap.upper_bound(cat);
-            if(it1 != fCategoryToBuilderMap.end())
+            while(it1 != it2)
             {
-                while(it1 != it2)
-                {
-                    //no control attributes need (default builders)
-                    it1->second->Build();
-                    it1++;
-                }
-            }
-        }
-        else
-        {
-            for(auto ctrl_iter = fControl->begin(); ctrl_iter != fControl->end(); ctrl_iter++)
-            {
-                if( !(ctrl_iter->is_null()) )
-                {
-                    auto statements = &((*ctrl_iter)["statements"]);
-                    for(auto stmt_iter = statements->begin(); stmt_iter != statements->end();)
-                    {
-                        std::string name = (*stmt_iter)["name"];
-                        bool build_op = false;
-                        if(fFormat.contains(name) && fFormat[name].contains("operator_category"))
-                        {
-                            if(cat == fFormat[name]["operator_category"].get< std::string >())
-                            {
-                                build_op = true;
-                            }
-                        }
-
-                        if(build_op)
-                        {
-                            auto builder_it = fNameToBuilderMap.find(name);
-
-                            if(builder_it != fNameToBuilderMap.end())
-                            {
-                                msg_debug("initialization",
-                                          "building operator with name: " << name << " in category: " << cat << "." << eom);
-                                builder_it->second->SetConditions(*ctrl_iter);
-                                builder_it->second->SetAttributes(*stmt_iter);
-                                // std::cout<<"name = "<<name<<std::endl;
-                                // std::cout<<"control condition: "<< *ctrl_iter << std::endl;
-                                // std::cout<<"statment attributes = "<< *stmt_iter <<std::endl;
-                                bool build_status_ok = builder_it->second->Build();
-                                if(!build_status_ok)
-                                {
-                                    msg_debug("initialization", "operator with name: " << name << " in category: " << cat
-                                                                                       << ", was not built." << eom);
-                                }
-                                stmt_iter = statements->erase(stmt_iter);
-                            }
-                            else //couldn't find a builder for this operator, skip
-                            {
-                                stmt_iter++;
-                                msg_debug("initialization", "operator: " << name << " not yet supported." << eom);
-                            }
-                        }
-                        else
-                        {
-                            msg_debug("initialization", "ignoring statement with name: " << name << eom);
-                            stmt_iter++; //statement not in this category, skip
-                        }
-                    }
-                }
-                else
-                {
-                    msg_error("initialization", "null control statement encountered " << eom);
-                }
+                //no control attributes need (default builders)
+                it1->second->Build();
+                it1++;
             }
         }
     }
     else
     {
-        msg_warn("initialization", "operator category: " << cat << " is not supported." << eom);
+        for(auto ctrl_iter = fControl->begin(); ctrl_iter != fControl->end(); ctrl_iter++)
+        {
+            if( !(ctrl_iter->is_null()) )
+            {
+                auto statements = &((*ctrl_iter)["statements"]);
+                for(auto stmt_iter = statements->begin(); stmt_iter != statements->end();)
+                {
+                    std::string name = (*stmt_iter)["name"];
+                    bool build_op = false;
+                    if(fFormat.contains(name) && fFormat[name].contains("operator_category"))
+                    {
+                        if(cat == fFormat[name]["operator_category"].get< std::string >())
+                        {
+                            build_op = true;
+                        }
+                    }
+
+                    if(build_op)
+                    {
+                        auto builder_it = fNameToBuilderMap.find(name);
+
+                        if(builder_it != fNameToBuilderMap.end())
+                        {
+                            msg_debug("initialization",
+                                      "building operator with name: " << name << " in category: " << cat << "." << eom);
+                            builder_it->second->SetConditions(*ctrl_iter);
+                            builder_it->second->SetAttributes(*stmt_iter);
+                            // std::cout<<"name = "<<name<<std::endl;
+                            // std::cout<<"control condition: "<< *ctrl_iter << std::endl;
+                            // std::cout<<"statment attributes = "<< *stmt_iter <<std::endl;
+                            bool build_status_ok = builder_it->second->Build();
+                            if(!build_status_ok)
+                            {
+                                msg_debug("initialization", "operator with name: " << name << " in category: " << cat
+                                                                                   << ", was not built." << eom);
+                            }
+                            stmt_iter = statements->erase(stmt_iter);
+                        }
+                        else //couldn't find a builder for this operator, skip
+                        {
+                            stmt_iter++;
+                            msg_debug("initialization", "operator: " << name << " not yet supported." << eom);
+                        }
+                    }
+                    else
+                    {
+                        msg_debug("initialization", "ignoring statement with name: " << name << eom);
+                        stmt_iter++; //statement not in this category, skip
+                    }
+                }
+            }
+            else
+            {
+                msg_error("initialization", "null control statement encountered " << eom);
+            }
+        }
     }
 }
 
