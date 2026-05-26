247226eff898a881ea987c6ef43aa55f0a1b5f26
gcrew <gbc@mit.edu>
2026-05-26 15:59:14 -0400

    correct fix for chk_cohfit.sh

--- source/c_src/applications/test/hops3-ff-tests/chk_cohfit.sh
+++ source/c_src/applications/test/hops3-ff-tests/chk_cohfit.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #
-# $Id: chk_cohfit.sh 4517 2026-05-11 17:23:05Z gbc $
+# $Id: chk_cohfit.sh 4572 2026-05-21 20:47:03Z gbc $
 #
 # exercises cohfit
 #
@@ -20,7 +20,7 @@ time=oifhak
 
 ls -l alist-aedit.coavg
 che=`type -p cohfit`
-[ -x $che ] || { echo No cohfit executable ; exit 77; }
+[ -x "$che" ] || { echo No cohfit executable ; exit 77; }
 echo cohfit is $che
 viable=`cohfit < /dev/null 2>&1 | grep Wrote`
 [ "$viable" = "cohfit: Wrote 0 coherence-analyzed output records" ] || {
@@ -39,9 +39,28 @@ echo return status $?
 set -- `wc -l alist-aedit.cohfit alist-aedit-1.cohfit alist-aedit-2.cohfit`
 $verb && echo wc is $@
 
-echo "$1" == 6 -a "$3" == 0 -a "$5" == 90
-[ "$1" -eq 6 -a "$3" -eq 0 -a "$5" -gt 90 ]
+echo "$1" == 6 and "$3" == 0 and "$5" gt 70 and "$5" lt 115
+[ "$1" -eq 6 -a "$3" -eq 0 -a "$5" -gt 70 -a "$5" -lt 115 ] || {
+    echo output file sizes are not within expected range
+    exit 3
+}
+echo file sizes are within normal limits
 
+# acid test
+ampfits=`grep -E 'Wrote|redchi' alist-aedit-2.cohfit |\
+         sed 's/stat.*iters=[0-9]*//'`
+$verb && echo ampfits is this: && echo "$ampfits"
+set -- `echo "$ampfits" | cksum`
+$verb && echo checksum of that is ${1-'0'} of ${2-'0'} lines
+
+# so finally
+echo test that $1 == 2742801947 and $2 == 858
+
+[ $1 -eq 2742801947 ] || { echo big number fail ; exit 4; }
+[ $1 -eq 2742801947 ] && { echo big number ok; }
+[ $2 -eq 858 ] || { echo little number fail ; exit 5; }
+[ $2 -eq 858 ] && { echo little number ok; }
+exit 0
 #
 # eof
 #
