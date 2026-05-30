82f303723fc602f19e7547459fd9b91a7c908e09
gcrew <gbc@mit.edu>
2026-05-30 18:13:13 -0400

    fixed HOPS_JAVACLASSPATH and VEX2XML?.sh

--- source/java_src/applications/vex2xml/VEX2XML3.sh
+++ source/java_src/applications/vex2xml/VEX2XML3.sh
@@ -4,7 +4,7 @@
 # once installed $HOPS_JAVACLASSPATH should hold all.
 #
 [ -z "$CLASSPATH" ] && CLASSPATH=$HOPS_JAVACLASSPATH
-[ -z "$VEX2XML3JAR" ] && VEX2XMLJAR=$CLASSPATH/java3/VEX2XML3.jar
+[ -z "$VEX2XML3JAR" ] && VEX2XML3JAR=$CLASSPATH/java3/VEX2XML3.jar
 exec java -classpath $CLASSPATH/java3 -jar $VEX2XML3JAR $@
 #
 # eof
--- source/java_src/applications/vex2xml/VEX2XML4.sh
+++ source/java_src/applications/vex2xml/VEX2XML4.sh
@@ -4,7 +4,7 @@
 # once installed $HOPS_JAVACLASSPATH should hold all.
 #
 [ -z "$CLASSPATH" ] && CLASSPATH=$HOPS_JAVACLASSPATH
-[ -z "$VEX2XML4JAR" ] && VEX2XMLJAR=$CLASSPATH/java4/VEX2XML4.jar
+[ -z "$VEX2XML4JAR" ] && VEX2XML4JAR=$CLASSPATH/java4/VEX2XML4.jar
 exec java -classpath $CLASSPATH/java4 -jar $VEX2XML4JAR $@
 #
 # eof
